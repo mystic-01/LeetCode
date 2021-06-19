76634dd14faf0d73798c7273f730e313299439d2
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2021-06-19 17:47:10 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (71.70%) - LeetHub

---
+++ remove-element/remove-element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        std::vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                result.push_back(nums[i]);
+            }
+        }
+        nums = result;
+        return nums.size();
+    }
+};

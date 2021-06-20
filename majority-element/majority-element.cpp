fbec3debe156cf2b008bfec33c4ae4dd9945693f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2021-06-21 04:45:09 +0530

    Time: 44 ms (5.61%), Space: 19.8 MB (8.60%) - LeetHub

---
+++ majority-element/majority-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map <int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]]++;
+        }
+        int cMax = nums[0], cFreq  = m[nums[0]];
+        for (int i = 0; i < nums.size(); i++) {
+            cout << m[nums[i]] << endl;
+            if (m[nums[i]] > cFreq) {
+                cMax = nums[i];
+            }
+        }
+        return cMax;
+    }
+};

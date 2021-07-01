df2f44ee2735113c0cd507f61cd16663602297e1
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2021-07-01 10:09:03 +0530

    Time: 172 ms (26.95%), Space: 46.3 MB (14.53%) - LeetHub

--- find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        vector<int> ans;
+        vector<int> ans(2, 0);
         for (int i = 0; i < mat.size(); i++) {
             for (int j = 0; j < mat[0].size(); j++) {
                 int element = mat[i][j];
@@ -9,8 +9,8 @@ public:
                     if (j-1 < 0 || element > mat[i][j-1]) {
                         if (i+1 == mat.size() || element > mat[i+1][j]) {
                             if (i-1 < 0 || element > mat[i-1][j]) {
-                                ans.push_back(i);
-                                ans.push_back(j);
+                                ans[0] = i;
+                                ans[1] = j;
                                 return ans;
                             };
                         };

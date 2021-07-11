eae4da063d4383c8f70964d26843743d1b9d509c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2021-07-11 20:31:32 +0530

    Time: 0 ms (100.00%), Space: 6 MB (96.20%) - LeetHub

---
+++ goal-parser-interpretation/goal-parser-interpretation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string s;
+        auto i = 0;
+        auto len = command.length();
+        char open = '(', close = ')';
+        while (i < len) {
+            if (command[i] == 'G') {
+                s += 'G';
+                i++;
+            }
+            else if (command[i] != 'G' && command[i+1] == 'a') {
+                s += "al";
+                i += 4;
+            }
+            else {
+                s += "o";
+                i += 2;
+            };
+        };
+        return s;
+    };
+};

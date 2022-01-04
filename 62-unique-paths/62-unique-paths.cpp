2f734e6a6fa8fcf4caf9216b26bef01d9c15a5ba
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-04 23:52:43 +0530

    Time: 6 ms (8.40%), Space: 6.5 MB (21.66%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
+    int countPaths(int i, int j, int m, int n, vector<vector<int>> &dp) {
+        if (i == (m-1) && j == (n-1)) return 1;
+        if (i >= (m) || j >= (n)) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = countPaths(i + 1, j, m, n, dp) + countPaths(i, j + 1, m, n, dp);
+    };
+    
     int uniquePaths(int m, int n) {
-        int N = m + n - 2, r = m - 1;
-        long long int res = 1;
-        
-        for (int i = 1; i <= r; i++)
-            res = (res * (N - r + i)) / i;
-        return res;
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        int nums = countPaths(0, 0, m, n, dp);
+        return (m == 1 && n == 1) ? nums : dp[0][0];
     };
 };

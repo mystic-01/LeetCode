e9852f7572c29132404bdc2392efe8b4594c4867
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-07 23:46:54 +0530

    Time: 77 ms (75.81%), Space: 16.2 MB (70.96%) - LeetHub

--- 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.cpp
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
+    int lowerBound(vector<vector<int>>& points, int target) {
+        int low = 0, high = points.size() - 1;
+        if (target <= points[low][0]) return low;
+        if (target >= points[high][0]) return high;
+
+        while (low < high) {
+            int mid = (low + high) / 2, midValue = points[mid][0];
+            if (target <= midValue) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        };
+        return high;
+    };
+    
     vector<int> countPoints(vector<vector<int>>& points, vector<vector<int>>& queries) {
+        sort(points.begin(), points.end());
         vector<int> count(queries.size(), 0);
-        int q = queries.size(), p = points.size(); 
+        int q = queries.size(), p = points.size();
+        
         for (auto i = 0; i < q; i++) {
-            for (auto j = 0; j < p; j++) {
-                if (pow(queries[i][0] - points[j][0], 2) + pow(queries[i][1] - points[j][1], 2) 
-                    <= pow(queries[i][2], 2)) count[i]++;
-            };
+            int x = queries[i][0], y = queries[i][1], r = queries[i][2], index = lowerBound(points, x - r);
+            for (auto j = index; j < p && points[j][0] <= x + r; j++)
+                count[i] += pow(x - points[j][0], 2) + pow(y - points[j][1], 2) <= pow(r, 2);
         };
         return count;
     };

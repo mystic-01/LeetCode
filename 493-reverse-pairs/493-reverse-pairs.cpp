2409d96b941f4519c2c6d8b01fa1733e4abde00d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-08 15:38:18 +0530

    Time: 1216 ms (7.46%), Space: 223.3 MB (36.35%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -26,27 +26,15 @@ public:
     };
 
     int mergeSort(int l, int r, vector<int> &arr) {
-        int count = 0;
-        if (r > l) {
-            int mid = (r + l) / 2;
-            count += mergeSort(l, mid, arr);
-            count += mergeSort(mid + 1, r, arr);
-            
-            count += merge(l, r, mid + 1, arr);
-        };
+        if (l >= r) return 0;
+        int mid = (l + r) / 2;
+        int count = mergeSort(l, mid, arr);
+        count += mergeSort(mid + 1, r, arr);
+        count += merge(l, r, mid + 1, arr);
         return count;
     };
 
     int reversePairs(vector<int>& nums) {
-        int n = nums.size(), count = 0;
-        // vector<int> temp(n);
-        // for (int i = 0; i < n - 1; i++) {
-        //     for (int j = i + 1; j < n; j++) {
-        //         double a = (double)nums[i] / 2, b = nums[j];
-        //         if (a > b) count++;
-        //     };
-        // };
-        // return count;
-        return mergeSort(0, n - 1, nums);      
+        return mergeSort(0, nums.size() - 1, nums);      
     };
 };

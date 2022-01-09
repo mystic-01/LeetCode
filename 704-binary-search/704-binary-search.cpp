46d4f3aaa03c249ff3455b9bd85cb7f266d67cd5
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-10 01:49:47 +0530

    Time: 35 ms (70.83%), Space: 27.6 MB (63.09%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int binarySearch(vector<int>& nums, int target, int l, int r) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
         if (l <= r) {
             int mid = (l + r) / 2;
             if (target == nums[mid]) return mid;

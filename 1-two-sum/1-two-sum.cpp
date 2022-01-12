f70b7c28bbd058e516b68e599da1b71639a2182f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-12 23:20:16 +0530

    Time: 48 ms (40.21%), Space: 13 MB (5.92%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -3,19 +3,13 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int, int> m;
         vector<int> ans;
-        for (int i = 0; i < nums.size(); i++) {
-            m[nums[i]]++;
-        };
+        for (int i = 0; i < nums.size(); i++) m[nums[i]]++;
         for (int i = 0; i < nums.size(); i++) {
             if (ans.size() == 0) {
-                if (m[target - nums[i]] >= (target - nums[i] == nums[i] ? 2 : 1)) {
-                    ans.push_back(i);
-                };    
-            } else {
-                if (nums[i] == target - nums[ans[0]]) {
+                if (m[target - nums[i]] >= (target - nums[i] == nums[i] ? 2 : 1)) ans.push_back(i);
+            } else if (nums[i] == target - nums[ans[0]]) {
                     ans.push_back(i);
                     break;
-                }; 
             };
         };
         return ans;

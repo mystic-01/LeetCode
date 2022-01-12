7fa5ce13b7eb7998ec96c2dfb90473de68aacaf2
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-13 01:15:54 +0530

    Time: 152 ms (35.50%), Space: 85.5 MB (64.43%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-        if (original.size() != (m * n))  {
-            vector<vector<int>> ans;
-            return ans;
-        }
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);        
+        
+        if (original.size() != (m * n))  return {};
         int index = 0;
-        vector<vector<int>> ans(m);
-        for (int i = 0; i < m; i++) {
-            ans[i].resize(n);
-            for (int j = 0; j < n; j++) {
-                ans[i][j] = original[index];
-                index++;
-            };
-        };
+        vector<vector<int>> ans(m, vector<int>(n));
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                ans[i][j] = original[index++];
         return ans;
     };
 };

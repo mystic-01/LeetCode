666d52135635ebcb404d5fa3116d82dd147c273d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-13 03:28:32 +0530

    Time: 31 ms (56.48%), Space: 25 MB (50.83%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -5,22 +5,11 @@ public:
         std::cin.tie(nullptr);        
 
         int n = nums.size();
-        if (k == 0 || n == 1) return;
         k %= n;
+        if (k == 0 || n == 1) return;
 
         reverse(nums.begin(), nums.begin() + n - k);
         reverse(nums.begin() + n - k, nums.end());
         reverse(nums.begin(), nums.end());
-        
-//         if (n == 1) return;
-//         vector<int> ans(n);
-        
-//         int x = n - k, i = 0;
-
-//         while(i < n) {
-//             ans[i++] = nums[x++];
-//             x = x == n ? 0 : x;
-//         }
-//         nums = ans;
     };
 };

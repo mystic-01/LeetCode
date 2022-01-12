b2d8b1b9b50d6cd6c5abf85d4ef677f440823f92
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-13 03:27:57 +0530

    Time: 27 ms (76.86%), Space: 25 MB (50.83%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,18 +3,24 @@ public:
     void rotate(vector<int>& nums, int k) {
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);        
-        
-        int n = nums.size();
-        if (n == 1) return;
-        vector<int> ans(n);
-        
-        k = k % n;
-        int x =  k == 0 ? 0 : n - k, i = 0;
 
-        while(i < n) {
-            ans[i++] = nums[x++];
-            x = x == n ? 0 : x;
-        }
-        nums = ans;
+        int n = nums.size();
+        if (k == 0 || n == 1) return;
+        k %= n;
+
+        reverse(nums.begin(), nums.begin() + n - k);
+        reverse(nums.begin() + n - k, nums.end());
+        reverse(nums.begin(), nums.end());
+        
+//         if (n == 1) return;
+//         vector<int> ans(n);
+        
+//         int x = n - k, i = 0;
+
+//         while(i < n) {
+//             ans[i++] = nums[x++];
+//             x = x == n ? 0 : x;
+//         }
+//         nums = ans;
     };
 };

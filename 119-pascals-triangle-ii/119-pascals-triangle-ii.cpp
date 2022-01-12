2378f8aa7e6ebadff13629e98ee8535f36937240
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-13 03:59:14 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (49.63%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);        
+        
+        vector<vector<int>> ans(rowIndex + 1);
+        for (int i = 0; i <= rowIndex; i++) {
+            ans[i].resize(i + 1);
+            ans[i][0] = ans[i][i] = 1;
+            for (int j = 1; j < i; j++)
+                ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+        };
+        return ans[rowIndex];
+    };
+};

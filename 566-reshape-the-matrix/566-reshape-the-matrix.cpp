2148abd171db6e002857faf6193a14094b0386e2
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-13 01:00:21 +0530

    Time: 19 ms (15.30%), Space: 10.5 MB (94.64%) - LeetHub

---
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);        
+        
+        int m = mat.size(), n = mat[0].size(), a = 0, b = 0;
+        if (m * n != r * c) return mat;
+        vector<vector<int>> ans(r, vector<int>(c));
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                ans[i][j] = mat[a][b++];
+                if (b == n) {
+                    a = a + 1;
+                    b = 0;
+                };
+            };
+        };
+
+        return ans;
+    };
+};

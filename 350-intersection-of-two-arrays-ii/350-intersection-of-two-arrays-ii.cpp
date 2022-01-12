2b02da8651ea85b0ac1362b2a916398a7e63784a
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-13 00:04:46 +0530

    Time: 13 ms (12.84%), Space: 10.9 MB (7.41%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);        
         map<int, int> m;
         vector<int> ans;
         
         for(auto &i : nums1) m[i]++;
-        for(auto i = 0; i < nums2.size(); i++) {
+        for(auto i = 0; i < nums2.size(); i++)
             if (m[nums2[i]] > 0) {
                 ans.push_back(nums2[i]);
                 m[nums2[i]]--;
             };
-        };
         return ans;
     };
 };

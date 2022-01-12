cdf6e6deabdf004ecd383fabddcb09ac052ac8bc
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-12 23:57:25 +0530

    Time: 3 ms (58.39%), Space: 9.3 MB (28.13%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);        
+        
         if (m == 0) nums1 = nums2;
         int i = m - 1, j = n - 1, k = m + n - 1;
         

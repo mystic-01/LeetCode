68caf76491da016d913020837eccf11522d8ed65
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-13 23:33:40 +0530

    Time: 6 ms (25.23%), Space: 9.9 MB (5.53%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public: 
+    bool binarySearch(vector<int> v, int low, int high, int key) {
+        if (high >= low) {
+            int mid = (low + high) / 2;
+            return (
+                key == v[mid]
+                ? true 
+                : (v[mid] > key) 
+                   ? binarySearch(v, low, mid - 1, key) 
+                   : binarySearch(v, mid + 1, high, key)
+            );
+        }
+        return false;        
+    }   
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (target < matrix[0][0]) return false;
+        int v;
+        for (int i = 0; i < matrix.size(); i++) {
+            v = i;
+            if (target <= matrix[i][matrix[i].size() - 1]) break;
+        };
+        return binarySearch(matrix[v], 0, matrix[v].size() - 1, target);
+    };
+};

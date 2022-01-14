855d54763321e87f64760bb1e0c1713fa9f781cf
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-14 19:10:48 +0530

    Time: 22 ms (64.13%), Space: 20.5 MB (5.08%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        std::ios::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        int zero = 0, one = 0, n = nums.size() - 1;
+//     void moveZeroes(vector<int>& nums) {
+//         std::ios::sync_with_stdio(false);
+//         std::cin.tie(nullptr);
+//         int zero = 0, one = 0, n = nums.size() - 1;
         
-        while (one <= n && zero <= n && n > 0) {
-            swap(nums[one], nums[zero]);
-            while (zero <= n && nums[zero] != 0) zero++;
-            one = zero + 1;
-            while (one <= n && nums[one] == 0) one++;
-        };
-    };
-        /*void moveZeroes(vector<int>& nums) {
+//         while (one <= n && zero <= n && n > 0) {
+//             swap(nums[one], nums[zero]);
+//             while (zero <= n && nums[zero] != 0) zero++;
+//             one = zero + 1;
+//             while (one <= n && nums[one] == 0) one++;
+//         };
+//     };
+        void moveZeroes(vector<int>& nums) {
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
         int zeros = 0, i = 0, n = nums.size() - 1;
@@ -28,5 +28,5 @@ public:
         while(zeros--) ans.push_back(0);
         nums = ans;
     };
-    */
+    
 };

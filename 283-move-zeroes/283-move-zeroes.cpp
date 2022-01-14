ab9b2faf4a2e6965f4bb6eab49804314da6f01bb
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-14 19:10:12 +0530

    Time: 248 ms (5.86%), Space: 19.2 MB (83.10%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    /*
     void moveZeroes(vector<int>& nums) {
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
         int zero = 0, one = 0, n = nums.size() - 1;
         
-        while (one <= n && n > 0) {
+        while (one <= n && zero <= n && n > 0) {
             swap(nums[one], nums[zero]);
             while (zero <= n && nums[zero] != 0) zero++;
             one = zero + 1;
             while (one <= n && nums[one] == 0) one++;
         };
     };
-    */
-    void moveZeroes(vector<int>& nums) {
+        /*void moveZeroes(vector<int>& nums) {
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
         int zeros = 0, i = 0, n = nums.size() - 1;
@@ -30,4 +28,5 @@ public:
         while(zeros--) ans.push_back(0);
         nums = ans;
     };
+    */
 };

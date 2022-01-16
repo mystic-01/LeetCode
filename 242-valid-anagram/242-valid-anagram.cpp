d2771980c4bc83714d999987d3735184d334af66
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-16 19:01:36 +0530

    Time: 14 ms (38.68%), Space: 7.2 MB (77.76%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -5,12 +5,12 @@ public:
         std::cin.tie(nullptr);     
         if (s.length() != t.length()) return 0;
         map<char, int> m;
-        auto i = 0, j = 0;
+        int i = 0, j = 0;
         while (i < s.length()) {
             m[s[i++]]++;
             m[t[j++]]--;            
         };
-        for (auto &k : m) if (k.second > 0) return 0;
+        for (auto k : m) if (k.second > 0) return 0;
         return 1;
     };
 };

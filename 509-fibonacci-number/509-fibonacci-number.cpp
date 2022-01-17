beefeeb21052080cc4053c7fbe258b1e51da6872
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-18 03:45:08 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (9.71%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,26 +1,11 @@
 class Solution {
-public:
-//     int afi(int n, vector<int> &dp) {
-//         std::ios::sync_with_stdio(false);
-//         std::cin.tie(nullptr);
-  
-//         if (n == 0) return 0;
-//         if (n == 1) return 1;
-//         if (dp[n] != -1) return dp[n];
-//         return dp[n] = afi(n - 1, dp) + afi(n - 2, dp);
-//     };
-    
+public: 
     int fib(int n) {
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
         
-        int x = 0, y = 1, z = 69;
-        
-        for (int i = 2; i <= n; i++) {
-            z = x + y;
-            x = y;
-            y = z;
-        };
-        return n < 2 ? n : z;
+        vector<int> smartIfa = {0, 1};
+        for (int i = 2; i <= n; i++) smartIfa = {smartIfa[1], smartIfa[1] + smartIfa[0]};
+        return n < 2 ? n : smartIfa[1];
     };
 };

d2cca21a8823efeb8a60513f5e8009a17d5b59ce
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-18 03:18:34 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (9.71%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -14,7 +14,10 @@ public:
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
         
-        vector<int> dp(n + 1, -1);
-        return afi(n, dp);
+        vector<int> dp(n + 2, -1);
+        dp[0] = 0; dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];   
+        return dp[n];
     };
 };

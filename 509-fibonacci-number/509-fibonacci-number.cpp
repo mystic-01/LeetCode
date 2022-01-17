111b97e0028d232673389a56c65922b72296c610
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-18 03:27:25 +0530

    Time: 0 ms (100.00%), Space: 6 MB (17.66%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int afi(int n, vector<int> &dp) {
-        std::ios::sync_with_stdio(false);
-        std::cin.tie(nullptr);
+//     int afi(int n, vector<int> &dp) {
+//         std::ios::sync_with_stdio(false);
+//         std::cin.tie(nullptr);
   
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        if (dp[n] != -1) return dp[n];
-        return dp[n] = afi(n - 1, dp) + afi(n - 2, dp);
-    };
+//         if (n == 0) return 0;
+//         if (n == 1) return 1;
+//         if (dp[n] != -1) return dp[n];
+//         return dp[n] = afi(n - 1, dp) + afi(n - 2, dp);
+//     };
     
     int fib(int n) {
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
         
-        vector<int> dp(n + 2, -1);
-        dp[0] = 0; dp[1] = 1;
+        int x = 0, y = 1, z = 69;
         
-        for (int i = 2; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];   
-        return dp[n];
+        for (int i = 2; i <= n; i++) {
+            z = x + y;
+            x = y;
+            y = z;
+        };
+        return n < 2 ? n : z;
     };
 };

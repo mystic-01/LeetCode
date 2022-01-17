b5721625e6b6fbd0d2252a6fbae04e296386c021
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-18 03:13:09 +0530

    Time: 3 ms (45.25%), Space: 6.2 MB (9.71%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
+    int afi(int n, vector<int> &dp) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+  
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        if (dp[n] != -1) return dp[n];
+        return dp[n] = afi(n - 1, dp) + afi(n - 2, dp);
+    };
+    
     int fib(int n) {
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
         
-        if (n == 0) return 0;
-        if (n == 1) return 1;
-        return fib(n - 1) + fib(n  -2);
+        vector<int> dp(n + 1, -1);
+        return afi(n, dp);
     };
 };

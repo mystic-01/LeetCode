f019e472817451e56f538e74242e8a45233726f0
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-21 00:42:23 +0530

    Time: 12 ms (99.31%), Space: 23.1 MB (76.22%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        
         int i = 0, j = s.size() - 1;
         while (i < j) {
             char temp = s[i];

732dbfac1d2d7f33c241ae0f08973e6944983275
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-21 00:37:45 +0530

    Time: 4 ms (84.99%), Space: 7.5 MB (12.52%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.cpp
+++ 541-reverse-string-ii/541-reverse-string-ii.cpp
@@ -4,19 +4,15 @@ public:
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
     
-        int i = 0, j = 1, poi = 0, n = s.length();
-        for (i = 0; i < n; i++) {
+        int j = 1, pos = 0, n = s.length();
+        for (int i = 0; i < n; i++) {
             if (j % (2 * k) == 0) {
-                reverse(s.begin() + poi, s.begin() + poi + k);
-                poi = j;
+                reverse(s.begin() + pos, s.begin() + pos + k);
+                pos = j;
             };
             j++;
         };
-        if (n - poi >= k) {
-            reverse(s.begin() + poi, s.begin() + poi + k);
-        } else {
-            reverse(s.begin() + poi, s.end());            
-        }
+        reverse(s.begin() + pos, (n - pos >= k) ? s.begin() + pos + k : s.end());
         return s;
     };
 };

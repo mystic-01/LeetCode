9311e68a5be6e6184edb3062755fcf667b193c52
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-21 17:53:54 +0530

    Time: 11 ms (40.80%), Space: 14.9 MB (44.55%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,42 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-       
-
-        if (!l1 && !l2) return NULL; 
-        else if (!l1) return l2;
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        
+        if (!l1) return l2;
         else if (!l2) return l1;
 
-        ListNode * result = (l1->val > l2->val) ? l2 : l1;
-        if (l1->val > l2->val) {
-            l2 = l2->next;
-        } else {
-            l1 = l1->next;
-        }
-        ListNode * head = result;
-
-      while (l1 != NULL && l2 != NULL) {
-          if (l1->val > l2->val) {
-              result->next = l2;
-              l2 = l2->next;
-          } else {
-              result->next = l1;
-              l1 = l1->next;
-          }
-          result = result->next;
-      };  
-      if (l1) result->next = l1;
-      if (l2) result->next = l2;
-      return head;  
+        ListNode * result = new ListNode(69), * temp = result;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                temp = temp->next = l1;
+                l1 = l1->next;
+            } else {
+                temp = temp->next = l2;
+                l2 = l2->next;                
+            };            
+        };
+        if (l1) temp->next = l1;
+        if (l2) temp->next = l2;
+        return result->next;  
     };
 };

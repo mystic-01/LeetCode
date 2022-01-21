1daf22dbcbfb3dd2786362750ad9469c4dc75563
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-21 05:34:11 +0530

    Time: 4 ms (89.06%), Space: 10.4 MB (28.56%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        
         if (!head || !head->next) return head;
         ListNode* newHead = new ListNode(69), * prev = new ListNode(69);
-        prev->next = head;
         ListNode * curr = head, * newCurr = newHead, * newPrev = prev;
-        
+        prev->next = head;
         while (curr) {
             if (curr->val < x) {
                 newCurr = newCurr->next = curr;

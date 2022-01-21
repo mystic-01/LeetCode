347231ff78075b74f822c82e8d4757ea12cd8060
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-21 05:31:27 +0530

    Time: 4 ms (89.06%), Space: 10.3 MB (72.22%) - LeetHub

--- 86-partition-list/86-partition-list.cpp
+++ 86-partition-list/86-partition-list.cpp
@@ -1,56 +1,21 @@
-// class Solution {
-// public:
-    // ListNode* partition(ListNode* head, int x) {
-        // ListNode* curr = head;
-        // while (curr && curr->next) {
-          // if (curr->next->val < x) {
-              // ListNode* temp = curr->next;
-              // curr->next = curr->next->next;
-              // temp->next = head;
-              // head = temp;
-          // } else {
-              // curr = curr->next;
-          // };
-        // };
-        // return head;
-    // };
-// };
-
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        // ListNode *curr = head, * newHead, * newTail, * nHead, * nTail;
-        // while (curr) {
-        //     if (curr->val < x) {
-        //         if (nHead) nHead->next = curr;
-        //         else newHead = curr;
-        //         nHead = curr;
-        //     } else {
-        //         if (nTail) nTail->next = curr;
-        //         else newTail = curr;
-        //         nTail = curr;
-        //     }
-        //     curr = curr->next;
-        // };
-        // return newHead;
-        std::ios::sync_with_stdio(false);
-        std::cin.tie(nullptr);
+        if (!head || !head->next) return head;
+        ListNode* newHead = new ListNode(69), * prev = new ListNode(69);
+        prev->next = head;
+        ListNode * curr = head, * newCurr = newHead, * newPrev = prev;
         
-        
-        ListNode* start = new ListNode(69), * end = new ListNode(69);
-        ListNode *curr = head, *temp1 = start, *temp2 = end;
-         while(curr){
+        while (curr) {
             if (curr->val < x) {
-                temp1->next = curr;
-                temp1 = curr;
+                newCurr = newCurr->next = curr;
+                newPrev->next = curr->next;
             } else {
-                temp2->next = curr;
-                temp2 = curr;
-            }
-            curr = curr -> next;          
-        }
-        temp1->next = end->next;
-        temp2->next = nullptr;       
-        return start->next;
+                newPrev = curr;
+            };
+            curr = curr->next;
+        };
+        newCurr->next = prev->next;
+        return newHead->next;
     };
 };

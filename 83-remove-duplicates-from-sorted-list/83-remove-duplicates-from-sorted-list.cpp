796c89ed7883e3032414963439ad7565cb510683
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-21 18:07:04 +0530

    Time: 14 ms (35.89%), Space: 11.7 MB (35.76%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -13,9 +13,8 @@ public:
     ListNode* deleteDuplicates(ListNode* head) {
         ListNode * temp = head;
         while (temp && temp->next) {
-            if (temp->val == temp->next->val)  {
-                temp->next = temp->next->next;
-            } else temp = temp->next;
+            if (temp->val == temp->next->val) temp->next = temp->next->next;
+            else temp = temp->next;
         };
         return head;
     };

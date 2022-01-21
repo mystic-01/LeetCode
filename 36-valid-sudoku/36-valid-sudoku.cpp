3f32e55bf6984c116791a37d675799efa78d5331
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-21 05:46:43 +0530

    Time: 36 ms (31.07%), Space: 18.8 MB (54.09%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        vector<int> col, row;
+        for (int i = 0; i < 9; i++) {
+            col = vector<int>(10);
+            row = vector<int>(10);
+            for (int j = 0; j < 9; j++)
+                if ((board[i][j] != '.' && ++row[board[i][j] - '0'] > 1) ||
+                    (board[j][i] != '.' && ++col[board[j][i] - '0'] > 1)) return false;
+        };
+        for (int k = 0; k < 3; k++) {
+            for (int i = 0; i < 9; i++) {
+                if (i == 0 || i == 3 || i == 6) col = vector<int>(10);
+                for (int j = (3 * k); j  < 3 + (3 * k); j++) {
+                    if (board[i][j] != '.' && ++col[board[i][j] - '0'] > 1) return false;
+                };
+            };
+        };
+        return true;
+    };
+};

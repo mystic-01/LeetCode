376a3ba9e2ddd0f590439d998f1c085a51ffde77
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-21 06:59:03 +0530

    Time: 46 ms (25.61%), Space: 22.5 MB (49.02%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        
         int n = arr.size(), i = 0, j = n - 1, count = n - 1;
         if (n < 3 || arr[0] > arr[1]) return 0;
         while (i < j && count >= 0) {
@@ -8,7 +11,6 @@ public:
             if (arr[j] < arr[j-1]) j--;
             count--;
         };
-        cout << i << " " << j;
         if (j - i > 1 || i == n - 1) return 0;
         if (i == j - 1 && arr[i] >= arr[j]) return 0;
         return 1;

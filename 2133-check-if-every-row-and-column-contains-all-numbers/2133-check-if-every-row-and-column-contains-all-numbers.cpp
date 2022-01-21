1449c5c577bd80b87e52a363a639e21b921913eb
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-21 06:07:31 +0530

    Time: 145 ms (80.26%), Space: 39.6 MB (54.08%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -2,15 +2,18 @@ class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<int> row, col;
-        
+        vector<int> row;
         for (auto i = 0; i < n; i++) {
-            row = col = vector<int>(n, -1);
-            for (auto j = 0; j < n; j++) {
+            row = vector<int>(n, -1);
+            for (auto j = 0; j < n; j++)
                 row[matrix[i][j] - 1]++;
-                col[matrix[j][i] - 1]++;
-            };
-            if (find(row.begin(), row.end(), -1) != row.end() || find(col.begin(), col.end(), -1) != col.end()) return 0;
+            if (find(row.begin(), row.end(), -1) != row.end()) return 0;
+        };
+        for (auto i = 0; i < n; i++) {
+            row = vector<int>(n, -1);
+            for (auto j = 0; j < n; j++)
+                row[matrix[j][i] - 1]++;
+            if (find(row.begin(), row.end(), -1) != row.end()) return 0;
         };
         return true;
     };

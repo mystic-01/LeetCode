04fc84213c389e0cc325c9e9a545fc4949533e74
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-21 05:40:30 +0530

    Time: 4 ms (96.25%), Space: 8.1 MB (28.83%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
+    bool isVowel (char x) {
+      return x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' || x == 'A' || x == 'E' || x == 'I' 
+          || x == 'O' || x == 'U';
+    };
+    
     string reverseVowels(string s) {
         std::ios::sync_with_stdio(false);
         std::cin.tie(nullptr);
         
         int i = 0, j = s.length() - 1;
-        vector<char> v = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        vector<char> v = {};
         while (i < j) {
-            while (i < j && find(v.begin(), v.end(), s[i]) == v.end()) i++;
-            while (i < j && find(v.begin(), v.end(), s[j]) == v.end()) j--;
+            while (i < j && !isVowel(s[i])) i++;
+            while (i < j && !isVowel(s[j])) j--;
             swap(s[i++], s[j--]);
         };
         return s;

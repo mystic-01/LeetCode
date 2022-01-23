db14f1965f8d3f20e82ac930e1808b6f274e6070
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-23 06:40:45 +0530

    Time: 576 ms (12.50%), Space: 30.9 MB (62.50%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    // SPPSSSSPPS
+    int numberOfWays(string corridor) {
+        unordered_map<char, int> m, realCount;
+        long long int plantCount = 1;
+        long long int curr = 0, mod = 1000000007;
+        for (auto i : corridor) {
+            if (m['S'] == 2) {
+                if (i == 'P') curr = (curr + 1) % mod;
+                else {
+                    m['S'] = 0;
+                    plantCount = (plantCount * (curr + 1)) % mod;
+                    curr = 0;
+                };
+            }; 
+            m[i]++;
+            realCount[i]++;
+        };
+        
+        if (realCount['S'] == 2) return 1;
+        if (realCount['S'] < 2 || realCount['S'] % 2 != 0 ) return 0;
+        
+        return (int)plantCount;
+        
+    };
+};

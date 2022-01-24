20534b245c988f5a1644d589aaf5832773224732
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-25 03:48:42 +0530

    Time: 392 ms (52.34%), Space: 124.6 MB (39.77%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -22,7 +22,7 @@ public:
     int maximumUniqueSubarray(vector<int>& nums) {
         unordered_map<int, int> m;
         unordered_map<int, int> sums;
-        int n = nums.size(), sum = 0, best = 0, right = 0, left = 0, runningSum = 0;
+        int n = nums.size(), best = 0, left = 0, prefixSum = 0;
         int sumToBeRemoved = 0;
 
         for (auto right = 0; right < n; right++) {
@@ -31,14 +31,12 @@ public:
                 while (left < n && nums[left] != nums[right]) m.erase(nums[left++]);
                 ++left;
             };
-            runningSum += nums[right];
-            sums[nums[right]] = runningSum;
-            m[nums[right]] = runningSum - sumToBeRemoved;
+            prefixSum += nums[right];
+            sums[nums[right]] = prefixSum;
+            m[nums[right]] = prefixSum - sumToBeRemoved;
 
             best = max(best, m[nums[right]]);
         };
-        best = max(best, sum);
-
         return best;
     };
 };

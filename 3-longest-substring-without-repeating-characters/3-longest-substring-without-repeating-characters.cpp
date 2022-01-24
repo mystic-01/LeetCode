3d6aba70d4f317c8d3b00b7374ba0e3917eb99be
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-25 01:29:44 +0530

    Time: 32 ms (36.35%), Space: 8.4 MB (56.71%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,41 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) {
+//         std::ios::sync_with_stdio(false);
+//         std::cin.tie(nullptr);        
+        
+//         int n = s.length(), best = 0, count = 0, i = 0;
+//         unordered_map<char, int> m;
+        
+//         for (auto j = 0; j < n; j++) {
+//             m[s[j]]++;
+//             if (m.find(s[j])->second >= 2) {
+//                 best = max(best, count);
+//                 while (i < n && s[i] != s[j]) m[s[i++]]--;
+//                 m[s[i++]]--;
+//                 count = j - i;
+//             };
+//             count++;
+//         };  
+//         best = max(best, count);
+//         return best;
+//     };
+// };
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        std::ios::sync_with_stdio(false);
-        std::cin.tie(nullptr);        
+        // std::ios::sync_with_stdio(false);
+        // std::cin.tie(nullptr);        
         
-        int n = s.length(), best = 0, count = 0, i = 0;
+        int n = s.length(), best = 0, left = 0;
         unordered_map<char, int> m;
         
-        for (auto j = 0; j < n; j++) {
-            m[s[j]]++;
-            if (m.find(s[j])->second >= 2) {
-                best = max(best, count);
-                while (i < n && s[i] != s[j]) m[s[i++]]--;
-                m[s[i++]]--;
-                count = j - i;
-            };
-            count++;
-        };  
-        best = max(best, count);
+        for (auto right = 0; right < n; right++) {
+            if (m.find(s[right]) != m.end()) left = max(m[s[right]] + 1, left);
+            best = max(best, right - left + 1);
+            m[s[right]] = right;
+        };
         return best;
     };
 };

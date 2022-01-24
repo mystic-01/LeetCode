4a5a72c644e64abb379998db3b06e1b4c98c457b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-24 19:59:09 +0530

    Time: 200 ms (50.74%), Space: 109.8 MB (5.01%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -11,7 +11,7 @@ public:
             m[sum % k]++;
         };
         for (int i = 0; i < n; i++) {
-            if ((v[i] == 0 && i != 0) || (v[i] == 0 && find(v.begin() + i + 1, v.end(), v[i]) != v.end())) return 1;
+            if ((v[i] == 0) && (i != 0 || find(v.begin() + i + 1, v.end(), v[i]) != v.end())) return 1;
             if (i < n - 2 && m[v[i]] > 1 && find(v.begin() + i + 2, v.end(), v[i]) != v.end()) return 1;
         };
         return 0;

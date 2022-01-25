5e5aa43134c1ec63f1c196a0cea2d34ff1fd928b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-26 00:55:11 +0530

    Time: 10 ms (13.77%), Space: 11.1 MB (75.15%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -12,7 +12,6 @@ public:
     
     int findRotationIndex(vector<int>& nums) {
         int lo = 0, hi = nums.size() - 1;
-
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
             if (nums[hi] > nums[mid]) hi = mid;

d3ba55b0be9842bb272dbed8e2cb786db5baece2
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-25 06:46:55 +0530

    Time: 28 ms (97.15%), Space: 36.5 MB (86.28%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -5,14 +5,13 @@ public:
         int count = 0, best = 0, lastZeroAnswer = 0;
         for (int i = 0; i < nums.size(); i++) {
             if (!nums[i]) {
-                count -= lastZeroAnswer;
-                lastZeroAnswer = count;
+                lastZeroAnswer = count -= lastZeroAnswer;
             } else {
                 count++;
             };
             
             best = max(best, count);
         };
-        return best == nums.size() ? best - 1 : best;
+        return best -= (best == nums.size());
     };
 };

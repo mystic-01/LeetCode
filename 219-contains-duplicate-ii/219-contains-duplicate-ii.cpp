49db9bdebfdaaa3e92ec51d6477bcf03ed7269f8
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-25 21:01:35 +0530

    Time: 324 ms (23.39%), Space: 72.2 MB (90.14%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -1,10 +1,23 @@
+// class Solution {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         unordered_map<int, int> m;
+//         for (auto i = 0; i < nums.size(); i++) {
+//             if (m.find(nums[i]) != m.end() && i - m[nums[i]] <= k) return 1;
+//             m[nums[i]] = i; 
+//         };
+//         return 0;
+//     };
+// };
+
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> m;
+        unordered_set<int> s;
         for (auto i = 0; i < nums.size(); i++) {
-            if (m.find(nums[i]) != m.end() && i - m[nums[i]] <= k) return 1;
-            m[nums[i]] = i; 
+            if (i > k) s.erase(nums[i - k - 1]);
+            if (s.find(nums[i]) != s.end()) return 1;
+            s.insert(nums[i]);
         };
         return 0;
     };

884ec447922a40b6436e25fa5463d178c450589d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-25 20:54:32 +0530

    Time: 109 ms (5.25%), Space: 18.7 MB (21.87%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
@@ -1,31 +1,11 @@
 class Solution {
 public:
-    auto lowerBound(int target, set<int> s) {
-        auto lo = s.begin(), hi = s.end(), it = s.begin();
-        
-        if (target < *lo) return lo;
-        int count = distance(lo, hi);        
-        while (count > 0) {
-            it = lo;
-            int mid = count / 2;
-            if (*it < target) {
-                lo = ++it;
-                count -= mid + 1;
-            } else {
-                count = mid;
-            };
-        };
-        return lo;
-    };
-    
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         set<long> s;
         for (auto i = 0; i < nums.size(); i++) {
             if (i > k) s.erase(nums[i - k - 1]);
-            if (s.size()) {
-                auto pos = s.lower_bound((long)nums[i] - t);
-                if (pos != s.end() && *pos - (long)nums[i] <= t) return 1;
-            };
+            auto pos = s.lower_bound((long)nums[i] - t);
+            if (pos != s.end() && *pos - (long)nums[i] <= t) return 1;
             s.insert(nums[i]);
         };
         return 0;

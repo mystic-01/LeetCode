ef8a5027b63cd22ef200058194b9987dcb395068
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-25 20:57:16 +0530

    Time: 91 ms (7.22%), Space: 18.8 MB (21.87%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
@@ -4,8 +4,10 @@ public:
         set<long> s;
         for (auto i = 0; i < nums.size(); i++) {
             if (i > k) s.erase(nums[i - k - 1]);
-            auto pos = s.lower_bound((long)nums[i] - t);
-            if (pos != s.end() && *pos - (long)nums[i] <= t) return 1;
+            if (s.size()) {
+                auto pos = s.lower_bound((long)nums[i] - t);
+                if (pos != s.end() && *pos - (long)nums[i] <= t) return 1;
+            };
             s.insert(nums[i]);
         };
         return 0;

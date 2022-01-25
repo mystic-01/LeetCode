3f58f821f4590094704e635ce1afc1e82e353a0a
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-26 00:51:22 +0530

    Time: 5 ms (35.87%), Space: 10.8 MB (99.98%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int binarySearch(int l, int r, vector<int>& nums, int target) {
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+            if (target == nums[mid]) return mid;
+            else if (target < nums[mid]) return binarySearch(l, mid - 1, nums, target);
+            else return binarySearch(mid + 1, r, nums, target);
+        };  
+        return -1;
+    };
+    
+    int findRotationIndex(vector<int>& nums) {
+        int lo = 0, hi = nums.size() - 1;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[hi] > nums[mid]) hi = mid;
+            else lo = mid + 1;
+        };
+        return hi;
+    };
+    
+    int search(vector<int>& nums, int target) {
+        int n = nums.size(), k = findRotationIndex(nums);
+        return target <= nums[n - 1] 
+            ? binarySearch(k, n - 1, nums, target) 
+            : binarySearch(0, k - 1, nums, target);
+    };
+};

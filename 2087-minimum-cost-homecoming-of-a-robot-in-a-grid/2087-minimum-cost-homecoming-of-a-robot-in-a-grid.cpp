e7dd3eb4d69ce3e7ebce20e6ed247bb9a797b9fd
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-26 22:29:14 +0530

    Time: 277 ms (18.57%), Space: 149.5 MB (99.50%) - LeetHub

--- 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
+++ 2087-minimum-cost-homecoming-of-a-robot-in-a-grid/2087-minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int minCost(vector<int>& start, vector<int>& home, vector<int>& rowCosts, vector<int>& colCosts) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
         int cost = 0;
-        
-        if (start[0] > home[0]) {
-            for (int i = home[0]; i < start[0]; i++) cost += rowCosts[i];                
-        } else {
-            for (int i = start[0]; i < home[0]; i++) cost += rowCosts[i + 1];                
-        };
-        
-        if (start[1] > home[1]) {
-            for (int i = home[1]; i < start[1]; i++) cost += colCosts[i];                
-        } else {
-            for (int i = start[1]; i < home[1]; i++) cost += colCosts[i + 1];                
-        };
+        for (int i = home[0]; i < start[0]; i++) cost += rowCosts[i];                
+        for (int i = start[0]; i < home[0]; i++) cost += rowCosts[i + 1];                
+        for (int i = home[1]; i < start[1]; i++) cost += colCosts[i];                
+        for (int i = start[1]; i < home[1]; i++) cost += colCosts[i + 1];                
         return cost;
     };
 };

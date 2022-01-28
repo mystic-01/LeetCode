b091e5e35147c76bb544726abda436daa0ae355f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-29 00:06:36 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (22.15%) - LeetHub

--- 728-self-dividing-numbers/728-self-dividing-numbers.cpp
+++ 728-self-dividing-numbers/728-self-dividing-numbers.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     bool canDivideItself (int x) {
-        int num = x;
-        while (x) {
-            int curr = (x % 10);
-            if (curr && num % curr != 0) return 0;
-            x /= 10;
-            if (curr == 0 && x) return 0;
+        int num = x, curr = 0;
+        while (num) {
+            curr = num % 10;
+            num /= 10;
+
+            if ((curr && x % curr != 0) || (!curr && num)) return 0;
         };        
         return 1;
     };
     
     vector<int> selfDividingNumbers(int left, int right) {
         vector<int> ans;
-        for (int i = left; i <= right; i++) {
-            if (canDivideItself(i)) ans.push_back(i);            
-        };
+        for (int i = left; i <= right; i++) if (canDivideItself(i)) ans.push_back(i);            
         return ans;
     };
 };

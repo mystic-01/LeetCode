a6b12abf1bea2fa422a102aae89b1f2bfd2db29b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-30 19:58:24 +0530

    Time: 68 ms (45.55%), Space: 8.5 MB (51.52%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,13 +1,29 @@
+// class Solution {
+// public:
+//     int numberOfSubstrings(string s) {
+//         int n = s.size(), count = 0, left = 0;
+//         vector<int> v(3);
+//         for (auto i = 0; i < s.length(); i++) {
+//             v[s[i] - 'a']++;
+//             while (v[0] > 0 && v[1] > 0 && v[2] > 0) {
+//                 count += (n - i);
+//                 v[s[left++] - 'a']--;
+//             };
+//         };
+//         return count;
+//     };
+// };
+
 class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n = s.size(), count = 0, left = 0;
-        vector<int> v(3);
+        map<char, int> m;
         for (auto i = 0; i < s.length(); i++) {
-            v[s[i] - 'a']++;
-            while (v[0] > 0 && v[1] > 0 && v[2] > 0) {
+            m[s[i]]++;
+            while (m['a'] > 0 && m['b'] > 0 && m['c'] > 0) {
                 count += (n - i);
-                v[s[left++] - 'a']--;
+                m[s[left++]]--;
             };
         };
         return count;

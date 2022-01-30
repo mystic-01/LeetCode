cd4b99506b960830c00b5a7338e4dc05c75ff16a
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-30 19:59:42 +0530

    Time: 34 ms (57.16%), Space: 8.6 MB (51.52%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,34 +1,34 @@
-// class Solution {
-// public:
-//     int numberOfSubstrings(string s) {
-//         int n = s.size(), count = 0, left = 0;
-//         vector<int> v(3);
-//         for (auto i = 0; i < s.length(); i++) {
-//             v[s[i] - 'a']++;
-//             while (v[0] > 0 && v[1] > 0 && v[2] > 0) {
-//                 count += (n - i);
-//                 v[s[left++] - 'a']--;
-//             };
-//         };
-//         return count;
-//     };
-// };
-
-
-// ---> Slower
-
 class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n = s.size(), count = 0, left = 0;
-        unordered_map<char, int> m;
+        vector<int> v(3);
         for (auto i = 0; i < s.length(); i++) {
-            m[s[i]]++;
-            while (m['a'] > 0 && m['b'] > 0 && m['c'] > 0) {
+            v[s[i] - 'a']++;
+            while (v[0] > 0 && v[1] > 0 && v[2] > 0) {
                 count += (n - i);
-                m[s[left++]]--;
+                v[s[left++] - 'a']--;
             };
         };
         return count;
     };
 };
+
+
+// ---> Slower when done using map.
+
+// class Solution {
+// public:
+//     int numberOfSubstrings(string s) {
+//         int n = s.size(), count = 0, left = 0;
+//         unordered_map<char, int> m;
+//         for (auto i = 0; i < s.length(); i++) {
+//             m[s[i]]++;
+//             while (m['a'] > 0 && m['b'] > 0 && m['c'] > 0) {
+//                 count += (n - i);
+//                 m[s[left++]]--;
+//             };
+//         };
+//         return count;
+//     };
+// };

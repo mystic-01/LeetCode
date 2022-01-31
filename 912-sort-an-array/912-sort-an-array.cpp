ca2ac28cc6deb417b5cf12891dee7ed7b41472e2
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-31 20:56:11 +0530

    Time: 86 ms (52.85%), Space: 28.5 MB (53.68%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     
-    void merge (int l, int mid, int r, vector<int>& nums, int temp[]) {
-        int i = l, j = mid + 1, k = l;
+//     void merge (int l, int mid, int r, vector<int>& nums, int temp[]) {
+//         int i = l, j = mid + 1, k = l;
         
-        while (i <= mid && j <= r) temp[k++] = nums[i] < nums[j] ? nums[i++] : nums[j++];
+//         while (i <= mid && j <= r) temp[k++] = nums[i] < nums[j] ? nums[i++] : nums[j++];
 
-        while (i <= mid) temp[k++] = nums[i++];
+//         while (i <= mid) temp[k++] = nums[i++];
         
-        while (j <= r) temp[k++] = nums[j++];
+//         while (j <= r) temp[k++] = nums[j++];
         
-        for (auto i = l; i <= r; i++) nums[i] = temp[i];
+//         for (auto i = l; i <= r; i++) nums[i] = temp[i];
         
-    };
+//     };
     
-    void mergeSort (int l, int r, vector<int>& nums, int temp[]) {
-        if (r > l) {
-            int mid = l + (r - l) / 2;
-            mergeSort (l, mid, nums, temp);            
-            mergeSort (mid + 1, r, nums, temp);            
+//     void mergeSort (int l, int r, vector<int>& nums, int temp[]) {
+//         if (r > l) {
+//             int mid = l + (r - l) / 2;
+//             mergeSort (l, mid, nums, temp);            
+//             mergeSort (mid + 1, r, nums, temp);            
             
-            merge (l, mid, r, nums, temp);            
-        };
-    };
+//             merge (l, mid, r, nums, temp);            
+//         };
+//     };
     
     vector<int> sortArray(vector<int>& nums) {
         // int n = nums.size(), temp[n]; 

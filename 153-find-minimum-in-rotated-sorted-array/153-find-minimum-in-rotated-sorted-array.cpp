c2ff2d3e9e0ab36b37a53f67ab33f3a67fd31a27
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-31 21:12:54 +0530

    Time: 6 ms (31.15%), Space: 10.2 MB (22.06%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -6,7 +6,6 @@ public:
             int mid = lo + (hi - lo) / 2;
             if (nums[mid] < nums[hi]) hi = mid;
             else lo = mid + 1;
-            cout << mid << " ";
         };
         return nums[hi];
     };

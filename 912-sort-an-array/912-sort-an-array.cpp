91bacbf4e5e6b396e8b230da75a81e85dcd100c3
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-01-31 20:43:23 +0530

    Time: 95 ms (46.89%), Space: 28.7 MB (45.58%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    void merge (int l, int mid, int r, vector<int>& nums, int temp[]) {
+        int i = l, j = mid + 1, k = l;
+        
+        while (i <= mid && j <= r) temp[k++] = nums[i] < nums[j] ? nums[i++] : nums[j++];
+
+        while (i <= mid) temp[k++] = nums[i++];
+        
+        while (j <= r) temp[k++] = nums[j++];
+        
+        for (auto i = l; i <= r; i++) nums[i] = temp[i];
+        
+    };
+    
+    void mergeSort (int l, int r, vector<int>& nums, int temp[]) {
+        if (r > l) {
+            int mid = l + (r - l) / 2;
+            mergeSort (l, mid, nums, temp);            
+            mergeSort (mid + 1, r, nums, temp);            
+            
+            merge (l, mid, r, nums, temp);            
+        };
+    };
+    
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size(), temp[n]; 
+        mergeSort(0, n - 1, nums, temp);        
+        return nums;
+    };
+};

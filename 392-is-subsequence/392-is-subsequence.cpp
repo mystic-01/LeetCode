9547bec82237021ec288da3b91841a1561fada4d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-03 01:13:17 +0530

    Time: 5 ms (23.49%), Space: 6.5 MB (32.55%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,3 +1,16 @@
+// class Solution {
+// public:
+//     bool isSubsequenceRecurse(int i, string s, int n1, int j, string t, int n2) {
+//         if (i == n1) return 1;
+//         if (j == n2) return 0;
+//         return isSubsequenceRecurse((s[i] != t[j]) ? i : i + 1, s, n1, j + 1, t, n2);
+//     };            
+
+//     bool isSubsequence(string s, string t) {
+//         return isSubsequenceRecurse(0, s, s.length(), 0, t, t.length());            
+//     };
+// };
+
 class Solution {
 public:
     bool isSubsequenceRecurse(int i, string s, int n1, int j, string t, int n2) {
@@ -7,6 +20,8 @@ public:
     };            
 
     bool isSubsequence(string s, string t) {
-        return isSubsequenceRecurse(0, s, s.length(), 0, t, t.length());            
+        int i = 0, j = 0, n1 = s.length(), n2 = t.length();
+        while (j < n2) if (s[i] == t[j++]) i++;
+        return i == n1;            
     };
 };

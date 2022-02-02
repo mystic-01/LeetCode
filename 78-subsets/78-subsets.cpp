0257662e9b036c431111afeb8a5a924d7de3b3c6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-03 01:33:55 +0530

    Time: 8 ms (18.72%), Space: 6.9 MB (95.70%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,21 +1,40 @@
+// class Solution {
+// public:
+    
+//     void subsequences(int i, vector<int> nums, int n, vector<vector<int>> & ans, vector<int> ds) {
+//         if (i == n) {
+//             ans.push_back(ds);
+//             return;
+//         };
+//         ds.push_back(nums[i]);
+//         subsequences(i + 1, nums, n, ans, ds);
+//         ds.pop_back();    
+//         subsequences(i + 1, nums, n, ans, ds);
+//     };
+    
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> ans;            
+//         vector<int> ds;            
+//         subsequences(0, nums, nums.size(), ans, ds);
+//         return ans;
+//     };
+// };
+
 class Solution {
 public:
     
-    void subsequences(int i, vector<int> nums, int n, vector<vector<int>> & ans, vector<int> ds) {
-        if (i == n) {
-            ans.push_back(ds);
-            return;
-        };
-        ds.push_back(nums[i]);
-        subsequences(i + 1, nums, n, ans, ds);
-        ds.pop_back();    
-        subsequences(i + 1, nums, n, ans, ds);
-    };
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;            
-        vector<int> ds;            
-        subsequences(0, nums, nums.size(), ans, ds);
-        return ans;
+        vector<vector<int>> result;            
+        result.push_back({});
+        for (auto n : nums) {
+            int size = result.size();
+            for(auto i = 0; i < size; i++) {
+                vector<int> subset = result[i];
+                subset.push_back(n);
+                result.push_back(subset);
+            };
+        };
+        return result;
     };
 };
+

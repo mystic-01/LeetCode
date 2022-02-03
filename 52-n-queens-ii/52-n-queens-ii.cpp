1f4217260b847f8aff10114e5c970580f0e54e32
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-04 01:31:56 +0530

    Time: 149 ms (5.50%), Space: 29.2 MB (9.69%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    int recurseNQueens(int col, int n, vector<string> board, 
+    int recurseNQueens(int col, int n, 
     unordered_map<int, int> mRow, unordered_map<int, int>  mUpperDiagonal, unordered_map<int, int> mLowerDiagonal) {
         if (col == n) return 1;
         int count = 0;
         
         for (auto row = 0; row < n; row++) {
             if (!mRow[row] && !mUpperDiagonal[col - row] && !mLowerDiagonal[col + row]) {
-                board[row][col] = 'Q';
                 mRow[row] = mUpperDiagonal[col - row] = mLowerDiagonal[col + row] = 1;
-                count += recurseNQueens(col + 1, n, board, mRow, mUpperDiagonal, mLowerDiagonal);  
-                board[row][col] = '.';
+                count += recurseNQueens(col + 1, n, mRow, mUpperDiagonal, mLowerDiagonal);  
                 mRow[row] = mUpperDiagonal[col - row] = mLowerDiagonal[col + row] = 0;
             };            
         };
@@ -19,8 +17,7 @@ public:
 
     
     int totalNQueens(int n) {
-        vector<string> board(n, string(n, '.'));
         unordered_map<int, int> mRow, mUpperDiagonal, mLowerDiagonal;
-        return recurseNQueens(0, n, board, mRow, mUpperDiagonal, mLowerDiagonal);
+        return recurseNQueens(0, n, mRow, mUpperDiagonal, mLowerDiagonal);
     };
 };

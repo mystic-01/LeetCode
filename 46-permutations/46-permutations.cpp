0b9d59f0f2d9e9952b72e35d3dcbed2b09d0e590
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-03 20:31:09 +0530

    Time: 3 ms (78.87%), Space: 8 MB (29.08%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,26 +1,48 @@
+// class Solution {
+// public:
+//     void permutator(vector<int>nums, int n, vector<vector<int>>&ans, unordered_map<int,int>m,vector<int>ds) {
+//         if (ds.size() == n) {
+//             ans.push_back(ds);
+//             return;
+//         };
+//         for (auto i = 0; i < n; i++) {
+//             if (m[nums[i]] == 1) continue;
+//             ds.push_back(nums[i]);
+//             m[nums[i]]++;
+//             permutator(nums, n, ans, m, ds);
+//             ds.pop_back();
+//             m[nums[i]]--;
+//         };
+//     };    
+
+    
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         unordered_map<int, int> m;
+//         vector<vector<int>> ans;
+//         vector<int> ds;
+//         permutator(nums, nums.size(), ans, m, ds);    
+//         return ans;
+//     };
+// };
+
 class Solution {
 public:
-    void permutator(vector<int>nums, int n, vector<vector<int>>&ans, unordered_map<int,int>m,vector<int>ds) {
-        if (ds.size() == n) {
-            ans.push_back(ds);
+    void permutator(int idx, vector<int> nums, int n, vector<vector<int>>& ans) {
+        if (idx == n) {
+            ans.push_back(nums);
             return;
         };
-        for (auto i = 0; i < n; i++) {
-            if (m[nums[i]] == 1) continue;
-            ds.push_back(nums[i]);
-            m[nums[i]]++;
-            permutator(nums, n, ans, m, ds);
-            ds.pop_back();
-            m[nums[i]]--;
+        for (auto i = idx; i < n; i++) {
+            swap(nums[i], nums[idx]);
+            permutator(idx + 1, nums, n, ans);
+            swap(nums[i], nums[idx]);
         };
     };    
 
     
     vector<vector<int>> permute(vector<int>& nums) {
-        unordered_map<int, int> m;
         vector<vector<int>> ans;
-        vector<int> ds;
-        permutator(nums, nums.size(), ans, m, ds);    
+        permutator(0, nums, nums.size(), ans);    
         return ans;
     };
 };

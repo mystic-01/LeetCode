2a46a8f526a4fe1b89dd06d5e6233f1fd43e855d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-04 19:40:47 +0530

    Time: 11 ms (13.00%), Space: 8.1 MB (21.42%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,29 +1,3 @@
-// class Solution {
-// public:
-//     void permutator(vector<int>nums, int n, vector<vector<int>>&ans, unordered_map<int,int>m,vector<int>ds) {
-//         if (ds.size() == n) {
-//             ans.push_back(ds);
-//             return;
-//         };
-//         for (auto i = 0; i < n; i++) {
-//             if (m[nums[i]] == 1) continue;
-//             ds.push_back(nums[i]);
-//             m[nums[i]]++;
-//             permutator(nums, n, ans, m, ds);
-//             ds.pop_back();
-//             m[nums[i]]--;
-//         };
-//     };    
-
-    
-//     vector<vector<int>> permute(vector<int>& nums) {
-//         unordered_map<int, int> m;
-//         vector<vector<int>> ans;
-//         vector<int> ds;
-//         permutator(nums, nums.size(), ans, m, ds);    
-//         return ans;
-//     };
-// };
 
 class Solution {
 public:

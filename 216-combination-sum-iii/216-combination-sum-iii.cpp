ee3861ceb0a575779c19e155aa7ef39ce803d950
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-05 06:24:10 +0530

    Time: 7 ms (12.58%), Space: 13.4 MB (6.79%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    void findCombination(int i, int t, int k, vector<int> nums, int n, vector<vector<int>> &ans, vector<int> ds) {
-        if (t == 0) {
-            if (ds.size() == k) ans.push_back(ds);
-            return;
-        };
+    void findCombination(int i, int t, int k, vector<vector<int>> &ans, vector<int> ds) {
+        if (t == 0 && ds.size() == k) ans.push_back(ds);
+       
+        if (i == 10 || i > t || ds.size() > k) return;
         
-        if (i == n || nums[i] > t || ds.size() > k) return;
-        
-        ds.push_back(nums[i]);
-        findCombination(i + 1, t - nums[i], k, nums, n, ans, ds);
+        ds.push_back(i);
+        findCombination(i + 1, t - i, k, ans, ds);
         ds.pop_back();
-        findCombination(i + 1, t, k, nums, n, ans, ds);        
+        findCombination(i + 1, t, k, ans, ds);        
     };
     
-    vector<vector<int>> combinationSum3 (int k, int n) {
-        vector<int> nums = {1,2,3,4,5,6,7,8,9};
+    vector<vector<int>> combinationSum3 (int k, int target) {
         vector<vector<int>> ans;
         vector<int> ds;        
-        findCombination(0, n, k, nums, nums.size(), ans, ds);                
+        findCombination(1, target, k, ans, ds);                
         return ans;
     };
 };

9ca81796ebf27edb17d12ae92b88686739f9ca25
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-07 08:40:23 +0530

    Time: 2169 ms (5.01%), Space: 318.8 MB (5.01%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    bool isValid(char c, int row, int col, vector<vector<char>> board, int size) {
-        int x = sqrt(size); 
-        for (auto i = 0; i < size; i++) {
-            if (board[row][i] == c) return 0;               
-            if (board[i][col] == c) return 0;               
-            if (board[x * (row / x) + (i / x)][x * (col / 3) + (i % x)] == c) return 0;               
+    
+    bool isValid(char c, int row, int col, vector<vector<char>> board) {
+        for (auto i = 0; i < 9; i++)
+            if (board[row][i] == c || board[i][col] == c || 
+                board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == c) return 0;               
+        return 1;
+    };
+    
+    bool solve(vector<vector<char>> & board) {
+        for (auto i = 0; i < 9; i++)  {
+            for (auto j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (auto c = '1'; c <= '9'; c++)
+                        if (isValid(c, i, j, board)) {
+                            board[i][j] = c;
+                            if (solve(board)) return 1;
+                            board[i][j] = '.'; 
+                        }; 
+                    return 0;
+                };              
+            };        
         };
         return 1;
     };
     
-    bool solve(vector<vector<char>> & board, int n) {
-        for (auto i = 0; i < n; i++)  {
-            for (auto j = 0; j < n; j++) {
-                if (board[i][j] == '.') {
-                    for (auto c = '1'; c <= '9'; c++) {
-                        if (isValid(c, i, j, board, n)) {
-                            board[i][j] = c;
-                            if (solve(board, n)) return true;
-                            board[i][j] = '.'; }; 
-                    }; 
-                    return false;
-                };              
-            };        
-        };
-        return true;
-    };
-    
     void solveSudoku(vector<vector<char>>& board) {
-        bool ans = solve(board, board.size());        
+        solve(board);        
     };
 };

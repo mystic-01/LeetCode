cd184fba8c1f07e97dd506e83b9f1bfd1f8357fe
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-07 11:46:19 +0530

    Time: 9 ms (12.47%), Space: 6.5 MB (87.58%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int n = s.length();
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
-        for (auto i = 0; i < n; i++) if (t[i] != s[i]) return t[i];
-        return t[n];
+        for (auto i = 0; i < s.length(); i++) if (t[i] != s[i]) return t[i];
+        return t[s.length()];
     };
 };

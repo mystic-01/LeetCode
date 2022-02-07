d517949e037e685def59ae2d7d38d16444aaa917
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-08 02:19:22 +0530

    Time: 244 ms (81.37%), Space: 123.4 MB (93.25%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int n = nums.size(), lo = 0, hi = n - 1, i = 0, j = n - 1;  
-        // Fill constructor supremacy        
+        int n = nums.size(), lo = 0, hi = n - 1;
         vector<int> ans(n, pivot);
-        while (i < n) {
+        for (int i = 0; i < n; i++) {
             if (nums[i] < pivot) ans[lo++] = nums[i];            
-            if (nums[j] > pivot) ans[hi--] = nums[j];            
-            i++;            
-            j--;
+            if (nums[n - i - 1] > pivot) ans[hi--] = nums[n - i - 1];            
         };
         return ans;
     };

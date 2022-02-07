5fd5c14a21f498cd7ea31f860228af2691dd4e0f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-07 07:00:10 +0530

    Time: 243 ms (5.08%), Space: 35 MB (18.97%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    void recurseNQueens (int col, int n, vector<string> board, vector<vector<string>> & ans, 
+    
+    void nQueens(int col, int n, vector<vector<string>>& ans, vector<string> board, 
     unordered_map<int, int> mRow, unordered_map<int, int> mUpperDiagonal, unordered_map<int, int> mLowerDiagonal) {
         if (col == n) {
-            ans.push_back(board);
-            return;                        
-        };        
-        
+            ans.push_back(board); return; 
+        };
         for (auto row = 0; row < n; row++) {
-            if (!mRow[row] && !mUpperDiagonal[col - row] && !mLowerDiagonal[col + row]) {
+            if (!mRow[row] && !mLowerDiagonal[col + row] && !mUpperDiagonal[col - row]) {
                 board[row][col] = 'Q';
-                mRow[row] = mUpperDiagonal[col - row] = mLowerDiagonal[col + row] = 1;
-                
-                recurseNQueens(col + 1, n, board, ans, mRow, mUpperDiagonal, mLowerDiagonal);
-                
+                mRow[row] = mLowerDiagonal[col + row] = mUpperDiagonal[col - row] = 1;
+                nQueens(col + 1, n, ans, board, mRow, mUpperDiagonal, mLowerDiagonal);
                 board[row][col] = '.';
-                mRow[row] = mUpperDiagonal[col - row] = mLowerDiagonal[col + row] = 0;
+                mRow[row] = mLowerDiagonal[col + row] = mUpperDiagonal[col - row] = 0;
             };            
         };
     };
+
     
     vector<vector<string>> solveNQueens(int n) {
-        unordered_map<int, int> mRow, mUpperDiagonal, mLowerDiagonal;
-        vector<string> board(n, string(n, '.'));
         vector<vector<string>> ans;
-        recurseNQueens (0, n, board, ans, mRow, mUpperDiagonal, mLowerDiagonal);
+        vector<string> board(n, string(n, '.'));
+        unordered_map<int, int> mRow, mUpperDiagonal, mLowerDiagonal;
+        nQueens(0, n, ans, board, mRow, mUpperDiagonal, mLowerDiagonal);
         return ans;
     };
 };

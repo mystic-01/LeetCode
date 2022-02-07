7a4605dc8087279d324d61c10ddaad0fdb930f12
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-07 07:16:38 +0530

    Time: 23 ms (30.96%), Space: 15.1 MB (22.79%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,46 +1,18 @@
-class Solution {
-public:
-    
-    void nQueens(int col, int n, vector<vector<string>>& ans, vector<string> board, 
-    vector<int> mRow, vector<int> mUpperDiagonal, vector<int> mLowerDiagonal) {
-        if (col == n) {
-            ans.push_back(board); return; 
-        };
-        for (auto row = 0; row < n; row++) {
-            if (!mRow[row] && !mLowerDiagonal[col + row] && !mUpperDiagonal[n + col - row]) {
-                board[row][col] = 'Q';
-                mRow[row] = mLowerDiagonal[col + row] = mUpperDiagonal[n + col - row] = 1;
-                nQueens(col + 1, n, ans, board, mRow, mUpperDiagonal, mLowerDiagonal);
-                board[row][col] = '.';
-                mRow[row] = mLowerDiagonal[col + row] = mUpperDiagonal[n + col - row] = 0;
-            };            
-        };
-    };
-
-    
-    vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> board(n, string(n, '.'));
-        vector<int> mRow(n, 0), mUpperDiagonal(2 * n, 0), mLowerDiagonal(2 * n, 0);
-        nQueens(0, n, ans, board, mRow, mUpperDiagonal, mLowerDiagonal);
-        return ans;
-    };
-};
-
 // class Solution {
 // public:
     
-//     void nQueens(int col, int n, vector<vector<string>>& ans, vector<string> board, vector<int> v) {
+//     void nQueens(int col, int n, vector<vector<string>>& ans, vector<string> board, 
+//     vector<int> mRow, vector<int> mUpperDiagonal, vector<int> mLowerDiagonal) {
 //         if (col == n) {
 //             ans.push_back(board); return; 
 //         };
 //         for (auto row = 0; row < n; row++) {
-//             if (!v[row] && !v[n + col + row] && !v[4 * n + col - row]) {
+//             if (!mRow[row] && !mLowerDiagonal[col + row] && !mUpperDiagonal[n + col - row]) {
 //                 board[row][col] = 'Q';
-//                 v[row] = v[n + col + row] = v[4 * n + col - row] = 1;
-//                 nQueens(col + 1, n, ans, board, v);
+//                 mRow[row] = mLowerDiagonal[col + row] = mUpperDiagonal[n + col - row] = 1;
+//                 nQueens(col + 1, n, ans, board, mRow, mUpperDiagonal, mLowerDiagonal);
 //                 board[row][col] = '.';
-//                 v[row] = v[n + col + row] = v[4 * n + col - row] = 0;
+//                 mRow[row] = mLowerDiagonal[col + row] = mUpperDiagonal[n + col - row] = 0;
 //             };            
 //         };
 //     };
@@ -49,8 +21,36 @@ public:
 //     vector<vector<string>> solveNQueens(int n) {
 //         vector<vector<string>> ans;
 //         vector<string> board(n, string(n, '.'));
-//         vector<int> v(5 * n, 0);
-//         nQueens(0, n, ans, board, v);
+//         vector<int> mRow(n, 0), mUpperDiagonal(2 * n, 0), mLowerDiagonal(2 * n, 0);
+//         nQueens(0, n, ans, board, mRow, mUpperDiagonal, mLowerDiagonal);
 //         return ans;
 //     };
 // };
+
+class Solution {
+public:
+    
+    void nQueens(int col, int n, vector<vector<string>>& ans, vector<string> board, vector<int> v) {
+        if (col == n) {
+            ans.push_back(board); return; 
+        };
+        for (auto row = 0; row < n; row++) {
+            if (!v[row] && !v[n + col + row] && !v[4 * n + col - row]) {
+                board[row][col] = 'Q';
+                v[row] = v[n + col + row] = v[4 * n + col - row] = 1;
+                nQueens(col + 1, n, ans, board, v);
+                board[row][col] = '.';
+                v[row] = v[n + col + row] = v[4 * n + col - row] = 0;
+            };            
+        };
+    };
+
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+        vector<int> v(5 * n, 0);
+        nQueens(0, n, ans, board, v);
+        return ans;
+    };
+};

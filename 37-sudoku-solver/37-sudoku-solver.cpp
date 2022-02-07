d970a1910c5667abf5ca2d59f8c37418adea105b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-07 08:34:12 +0530

    Time: 2258 ms (5.01%), Space: 318.7 MB (5.01%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(char c, int row, int col, vector<vector<char>> board, int size) {
+        int x = sqrt(size); 
+        for (auto i = 0; i < size; i++) {
+            if (board[row][i] == c) return 0;               
+            if (board[i][col] == c) return 0;               
+            if (board[x * (row / x) + (i / x)][x * (col / 3) + (i % x)] == c) return 0;               
+        };
+        return 1;
+    };
+    
+    bool solve(vector<vector<char>> & board, int n, int row, int col) {
+        for (auto i = row; i < n; i++)  {
+            for (auto j = col; j < n; j++) {
+                if (board[i][j] == '.') {
+                    for (auto c = '1'; c <= '9'; c++) {
+                        if (isValid(c, i, j, board, n)) {
+                            board[i][j] = c;
+                            if (solve(board, n, row, col)) return true;
+                            board[i][j] = '.'; }; 
+                    }; 
+                    return false;
+                };              
+            };        
+        };
+        return true;
+    };
+    
+    void solveSudoku(vector<vector<char>>& board) {
+        bool ans = solve(board, board.size(), 0 , 0);
+    };
+};

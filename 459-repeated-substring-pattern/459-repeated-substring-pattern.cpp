16f46f3ec756bc71f6b95f3c1f52fc12720310ad
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-08 10:37:03 +0530

    Time: 34 ms (39.49%), Space: 13.5 MB (37.50%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -37,9 +37,6 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        int n = s.length();
-        string s2 = s + s;
-        for (auto i = 1; i < n; i++) if (s == s2.substr(i, n)) return 1;
-        return 0;
+        return (s + s).substr(1, 2 * s.length() - 2).find(s) != -1;
     };
 };

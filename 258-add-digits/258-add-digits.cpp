aaa4ba719ba4025a82b981062bc8a5668766705f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-08 15:09:25 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     int addDigits(int num) {
         int ans = 0;
-        while (num) {
-            ans += num % 10;
-            num /= 10;
+        while (1) {
+            ans = 0;
+            while (num) {
+                ans += num % 10;
+                num /= 10;
+            };
+            if (ans < 10) break;
+            num = ans;
         };
-        if (ans < 10) return ans; 
-        return addDigits(ans);
+        return ans;
     };
 };

d62484d5d81c6e2517cae6bd7b6334bd9476971d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-08 10:37:35 +0530

    Time: 48 ms (27.11%), Space: 14.2 MB (32.96%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -1,42 +1,42 @@
-// class Solution {
-// public:
-//     bool repeatedSubstringPattern(string s) {
-//         int n = s.length(), size = sqrt(n), flag = 1;
-//         string test = "";
-//         for (auto i = 2; i <= size; i++) {
-//             if (n % i == 0) {
-//                 flag = 1;
-//                 test = s.substr(0, i);
-//                 for (auto j = i; j <= n - i; j += i) {
-//                    if (test != s.substr(j, i)) {
-//                        flag = 0;
-//                        break;
-//                    };
-//                 };
-//                 if (flag) return n - 1;
-                
-//                 int nByI = n / i;
-//                 flag = 1;
-//                 test = s.substr(0, nByI);
-//                 for (auto j = nByI; j <= n - nByI; j += nByI) {
-//                    if (test != s.substr(j, nByI)) {
-//                        flag = 0;
-//                        break;
-//                    };
-//                 };
-//                 if (flag) return n - 1;
-//             };
-//         };
-//         flag = 1;
-//         test = s.substr(0, 1);
-//         for (auto j = 1; j <= n - 1; j += 1) if (test != s.substr(j, 1)) flag = 0;
-//         if (flag) return n - 1;
-//         return 0;
-//     };
-// };
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        return (s + s).substr(1, 2 * s.length() - 2).find(s) != -1;
+        int n = s.length(), size = sqrt(n), flag = 1;
+        string test = "";
+        for (auto i = 2; i <= size; i++) {
+            if (n % i == 0) {
+                flag = 1;
+                test = s.substr(0, i);
+                for (auto j = i; j <= n - i; j += i) {
+                   if (test != s.substr(j, i)) {
+                       flag = 0;
+                       break;
+                   };
+                };
+                if (flag) return n - 1;
+                
+                int nByI = n / i;
+                flag = 1;
+                test = s.substr(0, nByI);
+                for (auto j = nByI; j <= n - nByI; j += nByI) {
+                   if (test != s.substr(j, nByI)) {
+                       flag = 0;
+                       break;
+                   };
+                };
+                if (flag) return n - 1;
+            };
+        };
+        flag = 1;
+        test = s.substr(0, 1);
+        for (auto j = 1; j <= n - 1; j += 1) if (test != s.substr(j, 1)) flag = 0;
+        if (flag) return n - 1;
+        return 0;
     };
 };
+// class Solution {
+// public:
+//     bool repeatedSubstringPattern(string s) {
+//         return (s + s).substr(1, 2 * s.length() - 2).find(s) != -1;
+//     };
+// };

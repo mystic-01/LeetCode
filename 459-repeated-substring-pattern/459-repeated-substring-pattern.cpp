6c8fc05de91137c03d82294d6dd89b42093e8fb4
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-08 10:16:21 +0530

    Time: 34 ms (39.49%), Space: 14.5 MB (32.90%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -1,21 +1,42 @@
 class Solution {
 public:
-    bool pattern (string s, int i, int n) {
-        bool flag = 1;
-        string test = s.substr(0, i);
-        for (auto j = i; j <= n - i; j += i) {
-           if (test != s.substr(j, i)) {
+    
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length(), size = sqrt(n), flag = 1;
+        string test = "";
+        for (auto i = 2; i <= size; i++) {
+            if (n % i == 0) {
+                flag = 1;
+                test = s.substr(0, i);
+                for (auto j = i; j <= n - i; j += i) {
+                   if (test != s.substr(j, i)) {
+                       flag = 0;
+                       break;
+                   };
+                };
+                if (flag) return n - 1;
+                
+                int nByI = n / i;
+                flag = 1;
+                test = s.substr(0, nByI);
+                for (auto j = nByI; j <= n - nByI; j += nByI) {
+                   if (test != s.substr(j, nByI)) {
+                       flag = 0;
+                       break;
+                   };
+                };
+                if (flag) return n - 1;
+            };
+        };
+        flag = 1;
+        test = s.substr(0, 1);
+        for (auto j = 1; j <= n - 1; j += 1) {
+           if (test != s.substr(j, 1)) {
                flag = 0;
                break;
            };
-        }; 
-        return flag;
-    };
-    
-    bool repeatedSubstringPattern(string s) {
-        int n = s.length(), size = sqrt(n);
-        for (auto i = 2; i <= size; i++) if (n % i == 0 && (pattern(s, i, n) || pattern(s, n / i, n))) return n - 1;
-        if (pattern(s, 1 , n)) return n - 1;        
+        };
+        if (flag) return n - 1;
         return 0;
     };
 };

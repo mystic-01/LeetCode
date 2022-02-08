651d060c95dd444c5df0182b29d8fa4db39465c6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-08 13:58:38 +0530

    Time: 3 ms (81.56%), Space: 8.9 MB (50.34%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -4,8 +4,9 @@ public:
         int n = nums.size(), lo = 0, hi = n - 1;
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-            if (nums[mid] > nums[mid + 1]) hi = mid;
-            else lo = mid + 1;
+            if (mid + 1 < n && nums[mid] < nums[mid + 1]) lo = mid + 1;
+            else if (mid - 1 > -1 && nums[mid] < nums[mid - 1]) hi = mid - 1;
+            else return mid;
         };
         return hi;
     };

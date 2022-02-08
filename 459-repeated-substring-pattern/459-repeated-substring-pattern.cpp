82b4e584445d902227ab51a66714085768a52606
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-08 10:41:55 +0530

    Time: 54 ms (23.63%), Space: 14.5 MB (32.90%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -7,29 +7,22 @@ public:
             if (n % i == 0) {
                 flag = 1;
                 test = s.substr(0, i);
-                for (auto j = i; j <= n - i; j += i) {
-                   if (test != s.substr(j, i)) {
-                       flag = 0;
-                       break;
-                   };
-                };
+                for (auto j = i; j <= n - i; j += i)
+                   if (test != s.substr(j, i)) { flag = 0; break; };
                 if (flag) return n - 1;
                 
                 int nByI = n / i;
                 flag = 1;
                 test = s.substr(0, nByI);
-                for (auto j = nByI; j <= n - nByI; j += nByI) {
-                   if (test != s.substr(j, nByI)) {
-                       flag = 0;
-                       break;
-                   };
-                };
+                for (auto j = nByI; j <= n - nByI; j += nByI)
+                   if (test != s.substr(j, nByI)) { flag = 0; break; };
                 if (flag) return n - 1;
             };
         };
         flag = 1;
         test = s.substr(0, 1);
-        for (auto j = 1; j <= n - 1; j += 1) if (test != s.substr(j, 1)) flag = 0;
+        for (auto j = 1; j <= n - 1; j++) 
+            if (test != s.substr(j, 1)) { flag = 0; break; };
         if (flag) return n - 1;
         return 0;
     };

c16156256056d89a42d78799267491397e2a0371
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-08 10:37:56 +0530

    Time: 46 ms (27.92%), Space: 14.3 MB (32.96%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.cpp
@@ -34,9 +34,3 @@ public:
         return 0;
     };
 };
-// class Solution {
-// public:
-//     bool repeatedSubstringPattern(string s) {
-//         return (s + s).substr(1, 2 * s.length() - 2).find(s) != -1;
-//     };
-// };

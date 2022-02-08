325cdaed18f8bd57b465b897ec7cc4ffaf5c605b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-08 12:09:02 +0530

    Time: 14 ms (43.49%), Space: 11.6 MB (48.50%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& nums) {
+        int n = nums.size();        
+        for (auto i = 0; i + 1 < n; i++) {
+            if (nums[i] > nums[i + 1]) return i;            
+        };
+        return n - 1;
+    };
+};

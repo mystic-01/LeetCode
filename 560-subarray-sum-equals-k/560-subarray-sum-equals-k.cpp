9bfe770b304629bfe61904a178eb3f4d412e55c4
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-10 08:48:10 +0530

    Time: 164 ms (21.13%), Space: 42.1 MB (13.98%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -5,8 +5,7 @@ public:
         unordered_map<int, int> m = {{0, 1}};
         
         for (auto i = 0; i < n; i++) {
-            sum += nums[i];
-            if (m.find(sum - k) != m.end()) count += m[sum - k];
+            sum += nums[i];count += m[sum - k];
             m[sum]++;
         };
         return count;

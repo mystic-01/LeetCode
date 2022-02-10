0b995f209164f02a731673d5808319b89be56c30
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-11 04:43:31 +0530

    Time: 83 ms (51.22%), Space: 44.1 MB (21.35%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -3,18 +3,18 @@ public:
     int maxScore(vector<int>& nums, int k) {        
         int n = nums.size(), m = n + 1, count = 0, best = 0;
 
-        vector<int> left(n + 1, 0), right(n + 1, 0);
+        vector<int> left(k + 1, 0), right(k + 1, 0);
         
-        int i = 1, j = n - 1;
+        int i = 1, j = k - 1;
         while (i <= k) {
             left[i] += (left[i - 1] + nums[i - 1]);           
-            right[j] += (right[j + 1] + nums[j]);
+            right[j] += (right[j + 1] + nums[n - i]);
             i++, j--;
         };
        
-        i = k, j = n;
-        while (i >= 0) {
-            count = left[i--] + right[j--];
+        i = 0;
+        while (i <= k) {
+            count = left[i] + right[i]; i++;
             best = max(best, count);
         };
         return best;

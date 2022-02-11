3a0f74d7b65102d9f1befe7d6bc04d3518430758
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-11 10:38:07 +0530

    Time: 179 ms (18.29%), Space: 46.5 MB (94.51%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std::ios::sync_with_stdio(false);
-        std::cin.tie(nullptr);        
         sort(nums.begin(), nums.end());
-        for (auto i = 0; i < nums.size() - 1; i++)
-            if (nums[i] == nums[i + 1]) return true;
-        return false;
+        for (auto i = 1; i < nums.size(); i++) if (nums[i] == nums[i - 1]) return 1;
+        return 0;
     };
 };

2a2949afca1d5150c582153031a86c95d16710bd
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-11 10:39:30 +0530

    Time: 146 ms (36.96%), Space: 51.3 MB (64.45%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (auto i = 1; i < nums.size(); i++) if (nums[i] == nums[i - 1]) return 1;
+        unordered_map<int, int> m;
+        for (auto i = 0; i < nums.size(); i++) if (++m[nums[i]] > 1) return 1;
         return 0;
     };
 };

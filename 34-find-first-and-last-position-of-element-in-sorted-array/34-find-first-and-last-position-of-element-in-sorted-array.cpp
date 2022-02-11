2eb0bdda452b1879c499f3dfce200517a32c1c76
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-11 22:59:00 +0530

    Time: 4 ms (96.33%), Space: 13.7 MB (18.53%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    int binarySearch (int l, int r, vector<int> nums, int target) {
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size(), found = -1, first = 0, last = 0;
+        int l = 0, r = n - 1;
         while (r >= l) {
             int mid = l + (r - l) / 2;
-            if (nums[mid] == target) return mid;
+            if (nums[mid] == target) {found = mid; break;}
             else if (nums[mid] > target) r = mid - 1;
             else l = mid + 1;
         };
-        return - 1;
-    };
-    
-    int lowerBound (vector<int> nums, int target) {
-        int lo = 0, hi = nums.size() - 1; 
+        if (found == -1) return {-1, -1};
+        
+        int lo = 0, hi = n - 1; 
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-             if (nums[mid] >= target) hi = mid;
-             else lo = mid + 1;
+            if (nums[mid] >= target) hi = mid;
+            else lo = mid + 1;
         };
-        return hi;
-    };
-    
-    int upperBound (vector<int> nums, int target) {
-        int lo = 0, hi = nums.size() - 1; 
+        first = hi;
+        
+        lo = 0, hi = n - 1;
         while (lo < hi) {
             int mid = lo + (hi - lo) / 2;
-             if (nums[mid] > target) hi = mid;
-             else lo = mid + 1;
+            if (nums[mid] > target) hi = mid;
+            else lo = mid + 1;
         };
-        return hi;
-    };
-    
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size(), foundTarget = binarySearch (0, n - 1, nums, target);
-        if (foundTarget == -1) return {-1, -1};
-        int i1 = lowerBound(nums, target), i2 = upperBound(nums, target);
-        i2 -= nums[i2] != target ? 1 : 0; 
-        return {i1, i2};
+        if (nums[hi] != target) hi--;
+        last = hi; 
+
+        return {first, last};
     };
 };

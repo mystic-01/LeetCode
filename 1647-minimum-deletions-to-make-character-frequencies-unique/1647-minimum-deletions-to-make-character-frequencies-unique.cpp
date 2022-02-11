04062f2a77c6ce3279241d0df880067d58dcf50c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-11 06:12:23 +0530

    Time: 61 ms (77.19%), Space: 17.2 MB (84.22%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minDeletions(string s) {
-        vector<int> v(26, 0);
+        int v[26] = {0};
         unordered_map<int, int> m;
         int n = s.length(), count = 0;
         

156b252303ee6d106802093fbbde6cd1f5f88314
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-13 00:44:17 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (40.93%) - LeetHub

--- 274-h-index/274-h-index.cpp
+++ 274-h-index/274-h-index.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int hIndex(vector<int>& nums) {
-        int n = nums.size(), h = 0, lo = 0, hi = n - 1;
+        int n = nums.size(), curr = 0, h = 0;
         sort(nums.begin(), nums.end());
         
-        while(lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (nums[mid] > n - mid) {
-                hi = mid - 1;
-                h = max(h, n - mid);
-            } else if (nums[mid] < n - mid) {
-                lo = mid + 1;
-                h = max(h, nums[mid]);
-            } else return nums[mid];
+        for (auto i = 0; i < n; i++) {
+            curr = min(nums[i], n - i);
+            h = max(curr, h);
         };
         return h;
     };

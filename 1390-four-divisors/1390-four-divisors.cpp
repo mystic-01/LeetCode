85357480850787677fcc00654360622c93ccf72c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-13 00:26:44 +0530

    Time: 32 ms (86.06%), Space: 12.4 MB (49.83%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -3,9 +3,9 @@ public:
     int sumFourDivisors(vector<int>& nums) {
         int n = nums.size(), sum = 0;
         for (auto x : nums) {
-            int rootI = sqrt(x), i = 2, curr = 0, count = 0;
-            if (pow(rootI, 2) != x) {
-                while (i <= rootI) {
+            int rootI = sqrt(x), curr = 0, count = 0;
+            if (pow (rootI, 2) != x) {
+                for (auto i = 2; i <= rootI; i++) {
                     if (x % i == 0) { 
                         curr += (i + (x / i));
                         count++;
@@ -14,7 +14,6 @@ public:
                         curr = 0;
                         break;
                     };
-                    i++;
                 };
             };
             if (curr) curr += (x + 1);

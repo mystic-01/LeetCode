305ff6da4a08c2e767692b11d2763f9998a18091
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-13 02:23:25 +0530

    Time: 74 ms (23.50%), Space: 15.1 MB (22.34%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
-    // int maxFreqOfSmallestchar (string s) {
-    //     vector<int> v(26);
-    //     for (auto i : s) v[i - 'a']++;                        
-    //     for (auto i : v) if (i) return i;
-    //     return 0;
-    // };
+    int maxFreqOfSmallestchar (string s) {
+        vector<int> v(26);
+        for (auto i : s) v[i - 'a']++;                        
+        for (auto i : v) if (i) return i;
+        return 0;
+    };
     
     vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
-        int q1 = queries.size(), w1 = words.size(), it = 0;
+        int q1 = queries.size(), w1 = words.size(), it1 = 0, it2 = 0, count = 0;
         vector<int> q(q1, 0), w(w1, 0);
         
-        for (auto s : queries) {
-            vector<int> v(26);
-            for (auto i : s) v[i - 'a']++;                        
-            for (auto i : v) if (i) {q[it++] = i; break;}
-        };
-        
-        it = 0;
-        for (auto s : words) {
-            vector<int> v(26);
-            for (auto i : s) v[i - 'a']++;                        
-            for (auto i : v) if (i) {w[it++] = i; break;}
-        };
+        for (auto s : queries) q[it1++] = maxFreqOfSmallestchar(s);
+        for (auto s : words) w[it2++] = maxFreqOfSmallestchar(s);
         
         for (auto &num : q) {
-            int count = 0;
+            count = 0;
             for (auto word : w) if (num < word) count++;                                                
             num = count;
         };

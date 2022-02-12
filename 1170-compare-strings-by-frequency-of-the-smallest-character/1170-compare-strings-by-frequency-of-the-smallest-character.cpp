d5bb67bb1f33b9107b7b977c91996c214c3d0d90
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-13 02:39:09 +0530

    Time: 1125 ms (5.09%), Space: 11.6 MB (54.63%) - LeetHub

--- 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
+++ 1170-compare-strings-by-frequency-of-the-smallest-character/1170-compare-strings-by-frequency-of-the-smallest-character.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int maxFreqOfSmallestchar (string s) {
+    string maxFreqOfSmallestchar (string s) {
         int count = 0;
         char small = 'z';
         for (auto i : s) if (i < small) small = i;                        
         for (auto i : s) if (i == small) count++;
-        return count;
+        return to_string(count);
     };
     
     vector<int> numSmallerByFrequency(vector<string>& queries, vector<string>& words) {
-        int q1 = queries.size(), w1 = words.size(), it1 = 0, it2 = 0, count = 0;
-        vector<int> q(q1, 0), w(w1, 0);
+        int q1 = queries.size(), it = 0, count = 0;
         
-        for (auto s : queries) q[it1++] = maxFreqOfSmallestchar(s);
-        for (auto s : words) w[it2++] = maxFreqOfSmallestchar(s);
+        vector<int> ans(q1, 0);
+        for (auto &q : queries) q = maxFreqOfSmallestchar(q);
+        for (auto &w : words) w = maxFreqOfSmallestchar(w);
         
-        for (auto &num : q) {
+        for (auto q2 : queries) {
             count = 0;
-            for (auto word : w) if (num < word) count++;                                                
-            num = count;
+            for (auto w2 : words) if (stoi(q2) < stoi(w2)) count++;
+            ans[it++] = count;
         };
-        
-        return q;
+        return ans;
     };
 };

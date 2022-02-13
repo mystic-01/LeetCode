e3513329e18cd482898067e94417f7401262b6c6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-14 05:10:49 +0530

    Time: 289 ms (58.87%), Space: 74.2 MB (95.04%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        int m = grid.size(), n = grid[0].size();
-        vector<int> nums;
+        int m = grid.size(), n = grid[0].size(), size = m * n;
+        int nums[size];
         for (auto i = 0; i < m; i++)
             for (auto j = 0; j < n; j++)
-                nums.push_back(grid[i][j]);
+                nums[(i * n) + j] = grid[i][j];
         
-        sort(nums.begin(), nums.end());
+        sort(nums, nums + size);
         
-        int average = nums[(m * n) / 2], operations = 0;
-        for (auto i = 0; i < nums.size(); i++) {
+        int average = nums[size / 2], operations = 0;
+        for (auto i = 0; i < size; i++) {
             int diff = abs(nums[i] - average);
             if (diff % x) return -1;
             operations += (diff / x);

8cafa21fc9ba8cea2afe0dc2800202f9ce39434d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-14 04:47:23 +0530

    Time: 85 ms (69.21%), Space: 24.2 MB (94.32%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.cpp
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int lowerBound(int lo, int hi, vector<int> nums, int target) {
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (nums[mid] < target) lo = mid + 1;
-            else hi = mid;
-        };
-        // cout << nums[hi] << " " << target << "\n";
-        return nums[hi] >= target ? nums[hi] : INT_MAX;  
-    };
-    
     vector<int> findRightInterval(vector<vector<int>>& nums) {
         int n = nums.size();
-        vector<int> starts(n), ans(n);
-        unordered_map<int, int> m({{INT_MAX, -1}});
+        vector<pair<int, int>> starts(n);
+        vector<int> ans(n);
         
-        for (auto i = 0; i < n; i++) m[nums[i][0]] = i, starts[i] = nums[i][0];
+        for (auto i = 0; i < n; i++) starts[i] = {nums[i][0], i};
         sort(starts.begin(), starts.end());
-        // for (auto i = 0; i < n; i++) cout << starts[i] << " ";
 
-        for (auto i = 0; i < n; i++) ans[i] = m[lowerBound(0, n - 1, starts, nums[i][1])];
+        for (auto i = 0; i < n; i++) {
+            int lo = 0, hi = n - 1, target = nums[i][1]; 
+             while (lo < hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (starts[mid].first < target) lo = mid + 1;
+                else hi = mid;
+            };
+            ans[i] = starts[hi].first >= target ? starts[hi].second : -1;
+        };
 
         return ans;
     };

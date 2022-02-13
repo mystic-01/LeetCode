f71aa4cd7512b6b1fec1b8c8b7a0ec1e603e1762
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-14 03:56:58 +0530

    Time: 60 ms (50.79%), Space: 27.6 MB (80.39%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
         sort(nums.begin(), nums.end(), greater<int>());
-        int n = nums.size();
+        auto n = nums.size();
         return max(nums[0] * nums[1] * nums[2], nums[0] * nums[n - 1] * nums[n - 2]);
     };
 };

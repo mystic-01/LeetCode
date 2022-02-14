bbe94ca295e9c80718a63e6af209c2777ae0491e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-14 09:09:38 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (85.50%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.cpp
@@ -4,10 +4,7 @@ public:
         int n = s.length(), x = 0, sum = 0;
         for (auto i = 0; i < n; i++) {
             x = (s[i] - 'a' + 1);
-            while (x) {
-                sum += x % 10;
-                x /= 10;
-            };
+            sum += ((x % 10) + (x / 10));
         };
         while (--k) {
             x = 0;

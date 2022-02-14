9c4d7eb0931b3d2ea234b80cec615d9d604df7d2
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-15 03:58:30 +0530

    Time: 5 ms (88.90%), Space: 7 MB (51.34%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -23,16 +23,14 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n = s.length(), left = 0, maxCount = 0, charWithMaxFreq = 0, len = 0, best = 0;
+        int n = s.length(), left = 0, maxFreq = 0, len = 0, best = 0;
         vector<int> v(26, 0);
+        
         for (auto i = 0; i < n; i++) {
-            maxCount = max(++v[s[i] - 'A'], maxCount);
+            maxFreq = max(++v[s[i] - 'A'], maxFreq);
             len = i - left + 1;
-            
-            if (len - maxCount > k && left < n) {
-                v[s[left++] - 'A']--;
-                len = i - left + 1;                                
-            };
+
+            if (len - maxFreq > k && left < n) v[s[left++] - 'A']--;
             best = max(best, i - left + 1);
         };
         return best;

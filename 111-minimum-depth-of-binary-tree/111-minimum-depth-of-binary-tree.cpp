8807a02efdb9666315a2b01438272b82e7314790
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-14 10:45:32 +0530

    Time: 228 ms (83.72%), Space: 144.7 MB (73.10%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -14,9 +14,8 @@ public:
     int preOrder(TreeNode* root, int count) {
         if (root == nullptr) return INT_MAX;
         if (root->left == nullptr && root->right == nullptr) return count;
-        int x = preOrder(root->left, count + 1);
-        int y = preOrder(root->right, count + 1);
-        return min(x, y);
+        
+        return min(preOrder(root->left, count + 1), preOrder(root->right, count + 1));
     };
     
     int minDepth(TreeNode* root) {

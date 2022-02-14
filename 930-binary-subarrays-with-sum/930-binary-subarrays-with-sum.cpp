db64620c87e483896c813588e5fcadbf19b6a36e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-14 09:45:17 +0530

    Time: 96 ms (29.57%), Space: 36.5 MB (21.55%) - LeetHub

--- 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.cpp
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int k) {
-        int sum = 0, count = 0;
-        unordered_map<int, int> m{{0, 1}};
+        int n = nums.size(), sum = 0, count = 0;
+        unordered_map<int, int> m;
+        m[sum]++;
         
-        for (auto i : nums) {
-            sum += i;
+        for (auto i = 0; i < n; i++) {
+            sum += nums[i];
             count += m[sum - k];
             m[sum]++;
         };

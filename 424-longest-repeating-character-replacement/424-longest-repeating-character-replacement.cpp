7360912fd1772bf75b4662c4b7208d950a165b52
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-15 04:10:22 +0530

    Time: 4 ms (97.43%), Space: 7 MB (85.53%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,25 +1,3 @@
-// class Solution {
-// public:
-//     int characterReplacement(string s, int k) {
-//         int n = s.length(), left = 0, maxFreq = 0, charWithMaxFreq = 0, len = 0, best = 0;
-//         unordered_map<int, int> m;
-//         for (auto i = 0; i < n; i++) {
-//             m[s[i]]++;
-//             if (m[s[i]] > maxFreq) maxFreq = m[s[i]], charWithMaxFreq = 1;
-//             else if (m[s[i]] == maxFreq) charWithMaxFreq++; 
-
-//             len = i - left + 1;
-//             while (len - maxFreq > k && left < n) {
-//                 m[s[i]]--;
-//                 maxFreq = max(maxFreq, m[s[i]]);
-//                 len = i - left + 1;                                
-//             };
-//             best = max(best, i - left + 1);
-//         };
-//         return best;
-//     };
-// };
-
 class Solution {
 public:
     int characterReplacement(string s, int k) {
@@ -30,12 +8,7 @@ public:
             maxFreq = max(++v[s[i] - 'A'], maxFreq);
             len = i - left + 1;
 
-            while (len - maxFreq > k && left < n) {
-                v[s[left++] - 'A']--;
-                maxFreq = *max_element(v.begin(), v.end());
-                len = i - left + 1; 
-            };
-            cout << i - left + 1 << " ";
+            if (len - maxFreq > k && left < n) v[s[left++] - 'A']--;
             best = max(best, i - left + 1);
         };
         return best;

976420235b6cad3d2e94fb5fba93f384944a4e43
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-14 06:26:06 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (24.23%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size(), ans = 0;
-        vector<int> dp(n, 0);
-        
-        for (auto i = 2; i < n; i++) nums[i] += *max_element(nums.begin(), nums.begin() + i - 1);
+        int n = nums.size();
         if (n == 1) return nums[0];
+        int prev1 = nums[0], prev2 = nums[1];
+        
+        vector<int> dp(n, 0);
+        for (auto i = 2; i < n; i++) nums[i] += prev1, prev1 = max(prev1, prev2), prev2 = nums[i];
         return max(nums[n - 1], nums[n - 2]);
     };
 };

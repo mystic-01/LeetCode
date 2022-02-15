e7110fadd15d5c926ff8807b8ab0de6938d13047
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-15 13:51:41 +0530

    Time: 4 ms (86.43%), Space: 7.8 MB (55.60%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        unordered_map<string, int> m;
+        for (auto i : banned) m[i] = -1e7;
+        
+        string curr = "", ans = "";
+        int maxCount = 0;
+        for (auto i : paragraph) {
+            if (isalpha(i)) curr += tolower(i);
+            else if (curr.length()) {
+                if (++m[curr] > maxCount) ans = curr, maxCount = m[curr];
+                curr = "";
+            };
+        };
+        if (curr.length()) {
+            if (++m[curr] > maxCount) ans = curr;
+        };
+        return ans;
+    };
+};

133f378e3388c08346ecd4f631128bffe7311137
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-15 06:52:41 +0530

    Time: 34 ms (42.73%), Space: 11.2 MB (48.46%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -3,7 +3,8 @@ public:
     void preOrder(Node* root, vector<int>& ans) {
         if (!root) return;
         ans.push_back(root->val);
-        for (auto i = 0; i < root->children.size(); i++) preOrder(root->children[i], ans);
+        for (auto i = 0; i < root->children.size(); i++) 
+            preOrder(root->children[i], ans);
     };
 
     vector<int> preorder(Node* root) {

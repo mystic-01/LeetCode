beaa550085a624bd104038ce86a6f2719e38f6f6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-15 05:51:39 +0530

    Time: 20 ms (91.33%), Space: 11.2 MB (56.53%) - LeetHub

--- 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.cpp
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.cpp
@@ -3,9 +3,7 @@ public:
     void postOrder(Node* root, vector<int>& ans) {
         if (root == nullptr) return;
         
-        for (auto i = 0; i < root->children.size(); i++) {
-            postOrder(root->children[i], ans);
-        };
+        for (auto i = 0; i < root->children.size(); i++) postOrder(root->children[i], ans);
         ans.push_back(root->val);
     };
     

c2694acd38227c08aeed774b5294b2580e504562
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-15 19:03:37 +0530

    Time: 15 ms (63.18%), Space: 5.9 MB (31.77%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     bool isPalindrome(int num) {
         if (num < 0) return 0;
-        vector<int> v;
-        while (num) {
-            v.push_back(num % 10);
-            num /= 10;
-        };
+        // vector<int> v;
+        // while (num) {
+        //     v.push_back(num % 10);
+        //     num /= 10;
+        // };
+        string v = to_string(num);
         int i = 0, j = v.size() - 1;
         while (i < j) if (v[i++] != v[j--]) return 0;
         return 1;

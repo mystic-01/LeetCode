0aaa54477b6395bb5f5a407333c691daa8055711
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-16 13:39:53 +0530

    Time: 257 ms (30.70%), Space: 140.7 MB (22.45%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public: 
-    // bool isPalindrome(string s) {
-    //     int i = 0, j = s.length() - 1;
-    //     while (i < j) {
-    //         if (s[i] != s[j]) return 0;
-    //         i++, j--;
-    //     };
-    //     return 1;
-    // };  
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s[i] != s[j]) return 0;
+            i++, j--;
+        };
+        return 1;
+    };  
         
     void solve(int idx, string s, int n, vector<vector<string>>& ans, vector<string> ds) {
         if(idx == n) ans.push_back(ds);
         
         for (auto i = idx; i < n; i++) {
-            int k = idx, j = i, flag = 1;
-            while (k < j) {
-                if (s[k] != s[j]) flag = 0;
-                k++, j--;
-            };
-            if (flag) {
-                ds.push_back(s.substr(idx, i - idx + 1));
+            string curr = s.substr(idx, i - idx + 1);
+            if (isPalindrome(curr)) {
+                ds.push_back(curr);
                 solve(i + 1, s, n, ans, ds);
                 ds.pop_back();
             };            

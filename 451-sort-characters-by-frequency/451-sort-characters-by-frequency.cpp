99054752a4e7a541a5363720ab032dbdd460f9fa
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-18 14:05:11 +0530

    Time: 12 ms (82.44%), Space: 7.9 MB (98.15%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     string frequencySort(string s) {
         vector<pair<int, char>> v(128, pair<int, char>(0, 'a'));                
-        reverse(v.begin(), v.end());
-        int n = s.length();
+        int n = s.length(), j = 127;
         
         for (auto i = 0; i < n; i++) v[s[i] - '0'].first++, v[s[i] - '0'].second = s[i];    
         sort(v.begin(), v.end());
 
-        int j = 127;
         for (auto i = 0; i < n; i++) {
             s[i] = v[j].second;
-            v[j].first--;
-            if (v[j].first == 0) j--;
+            if (--v[j].first == 0) j--;
         };
         
         return s;

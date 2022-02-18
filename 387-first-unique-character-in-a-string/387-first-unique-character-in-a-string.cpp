8a46a471564d5b72d964a9c8768ff1e6269a5cdb
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-18 12:59:34 +0530

    Time: 30 ms (77.42%), Space: 10.6 MB (90.69%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,25 +1,23 @@
-// class Solution {
-// public:
-//     int firstUniqChar(string s) {
-//         std::ios::sync_with_stdio(false);
-//         std::cin.tie(nullptr);     
-//         vector<int> v(26);
-//         for (auto i : s) v[i - 'a']++;
-//         for (auto i = 0; i < s.length(); i++) if (v[s[i] - 'a'] == 1) return i;
-//         return -1;
-//     };
-// };
-
 class Solution {
 public:
     int firstUniqChar(string s) {
-        queue<int> q;
         int v[26] = {0}, n = s.length();
-        for (auto i = 0; i < n; i++) {
-            v[s[i] - 'a']++;
-            q.push(i);
-            while (!q.empty() && v[s[q.front()] - 'a'] > 1) q.pop();
-        };
-        return q.empty() ? -1 : q.front();
+        for (auto i : s) v[i - 'a']++;
+        for (auto i = 0; i < s.length(); i++) if (v[s[i] - 'a'] == 1) return i;
+        return -1;
     };
 };
+
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         queue<int> q;
+//         int v[26] = {0}, n = s.length();
+//         for (auto i = 0; i < n; i++) {
+//             v[s[i] - 'a']++;
+//             q.push(i);
+//             while (!q.empty() && v[s[q.front()] - 'a'] > 1) q.pop();
+//         };
+//         return q.empty() ? -1 : q.front();
+//     };
+// };

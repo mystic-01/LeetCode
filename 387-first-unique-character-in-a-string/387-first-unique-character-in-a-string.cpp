7aad492b53935ec22fbc8c886fe1c0102dda7b6c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-18 13:15:02 +0530

    Time: 24 ms (90.04%), Space: 10.6 MB (69.98%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int v[26] = {0}, n = s.length();
-        for (auto i : s) v[i - 'a']++;
-        for (auto i = 0; i < s.length(); i++) if (v[s[i] - 'a'] == 1) return i;
-        return -1;
+        int n = s.length(), v[26][2] = {{0, INT_MAX}}, ans = INT_MAX;
+        
+        for (auto i = 0; i < n; i++) 
+            if (v[s[i] - 'a'][0]++ == 0) v[s[i] - 'a'][1] = i; 
+        
+        for (auto i = 0; i < 26; i++) 
+            if (v[i][0] == 1) ans = min(ans, v[i][1]);
+        
+        return ans == INT_MAX ? -1 : ans;
     };
 };
 

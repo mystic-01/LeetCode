730f0fa63e3fe9991e08e2dce50dbc78d0b4e141
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-19 06:52:18 +0530

    Time: 11 ms (64.47%), Space: 10.2 MB (5.68%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,16 +1,38 @@
+// class Solution {
+// public:
+//     int sumPaths(int i, int j, vector<vector<int>>& grid) {
+//         if (i == 0 && j == 0) return grid[0][0];
+//         if (i < 0 || j < 0) return INT_MAX;
+        
+//         return min(sumPaths(i - 1, j, grid), sumPaths(i, j - 1, grid)) + grid[i][j];
+//     };                                          
+
+    
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m = grid.size(), n = grid[0].size();
+//         return sumPaths(m - 1, n  - 1, grid);                                          
+//     };
+// };
+
 class Solution {
 public:
-    int countPathSum(int i, int j, int m, int n, vector<vector<int>>& dp, vector<vector<int>>& grid) {
-        if (i > m || j > n) return 961;
+    int sumPaths(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        if (i == 0 && j == 0) return grid[0][0];
+        if (i < 0 || j < 0) return INT_MAX;
         if (dp[i][j] != -1) return dp[i][j];
-        if (i == m && j == n) return dp[i][j] = grid[m][n];
-        return dp[i][j] = min(countPathSum(i + 1, j, m, n, dp, grid), countPathSum(i, j + 1, m, n, dp, grid)) + grid[i][j];
-    };
+        return dp[i][j] = min(sumPaths(i - 1, j, grid, dp), sumPaths(i, j - 1, grid, dp)) + grid[i][j];
+    };                                          
+
     
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size() - 1, n = grid[0].size() - 1, count = 0;
-                                          
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, - 1));
-        return countPathSum(0, 0, m, n, dp, grid);
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return sumPaths(m - 1, n  - 1, grid, dp);                                          
     };
 };
+
+
+
+
+
+

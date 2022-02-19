fa6a28e3e4a3f629084e64cac1b94dd7804b92c6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-19 11:01:50 +0530

    Time: 3 ms (98.22%), Space: 8.9 MB (47.63%) - LeetHub

--- 174-dungeon-game/174-dungeon-game.cpp
+++ 174-dungeon-game/174-dungeon-game.cpp
@@ -4,22 +4,24 @@ public:
     int calculateMinimumHP(vector<vector<int>>& dungeon) {
         int m = dungeon.size(), n = dungeon[0].size();
         
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> dp(n, 0);
         
         for (auto i = m - 1; i >= 0; i--) {
+            vector<int> curr(n, 0);        
             for (auto j = n - 1; j >= 0; j--) {
                 if (i == m - 1 && j == n - 1) {
-                    dp[i][j] = min(dungeon[i][j], 0);                
+                    curr[j] = min(dungeon[i][j], 0);                
                 } else if (j == n - 1) {
-                    dp[i][j] = min(dp[i + 1][j] + dungeon[i][j], 0);
+                    curr[j] = min(dp[j] + dungeon[i][j], 0);
                 } else if (i == m - 1) {
-                    dp[i][j] = min(dp[i][j + 1] + dungeon[i][j], 0);
+                    curr[j] = min(curr[j + 1] + dungeon[i][j], 0);
                 } else {
-                    dp[i][j] = min(max(dp[i + 1][j], dp[i][j + 1]) + dungeon[i][j], 0);
+                   curr[j] = min(max(dp[j], curr[j + 1]) + dungeon[i][j], 0);
                 };
             };
+            dp = curr;
         };
-        return dp[0][0] >= 0 ? 1 : abs(dp[0][0]) + 1;
+        return dp[0] >= 0 ? 1 : abs(dp[0]) + 1;
     };
 };
 

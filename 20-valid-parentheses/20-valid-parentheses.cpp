77eadcccbebdb95b778a5ce67eba6eaf24abaeeb
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-19 20:51:29 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (50.63%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -4,15 +4,15 @@ public:
         stack<char> paren;
         for (char& c : s) {
             switch (c) {
-                case '(': 
-                case '{': 
+                case '(': paren.push(c); break;
+                case '{': paren.push(c); break;
                 case '[': paren.push(c); break;
                 case ')': if (paren.empty() || paren.top()!='(') return false; else paren.pop(); break;
                 case '}': if (paren.empty() || paren.top()!='{') return false; else paren.pop(); break;
                 case ']': if (paren.empty() || paren.top()!='[') return false; else paren.pop(); break;
-                default: ; // pass
-            }
-        }
-        return paren.empty() ;
+                default: ;
+            };
+        };
+        return paren.empty();
     }
 };

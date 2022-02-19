f04b497602f7b19aa17bcb8e0c86f5955eb54c44
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-19 06:23:43 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (53.67%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -8,14 +8,16 @@ public:
     // };
     
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[0][0] = 1;
-        for (auto i = 0; i < m; i++) {
+        vector<int> dp(n, 1);
+        // dp[0] = 1;
+        for (auto i = 1; i < m; i++) {
+            vector<int> curr(n, 0); 
             for (auto j = 0; j < n; j++) {
-                if (i > 0) dp[i][j] += dp[i - 1][j];       
-                if (j > 0) dp[i][j] += dp[i][j - 1];
+                if (i > 0) curr[j] += dp[j];       
+                if (j > 0) curr[j] += curr[j - 1];
             };
+            dp = curr;
         };
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     };
 };

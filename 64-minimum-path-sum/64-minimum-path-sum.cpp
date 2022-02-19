e930facfbaa0f36ad0ac209da40dd9678c7ce668
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-19 07:07:15 +0530

    Time: 27 ms (6.68%), Space: 9.9 MB (59.66%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,35 +1,3 @@
-// class Solution {
-// public:
-//     int sumPaths(int i, int j, vector<vector<int>>& grid) {
-//         if (i == 0 && j == 0) return grid[0][0];
-//         if (i < 0 || j < 0) return INT_MAX;
-        
-//         return min(sumPaths(i - 1, j, grid), sumPaths(i, j - 1, grid)) + grid[i][j];
-//     };                                          
-
-    
-//     int minPathSum(vector<vector<int>>& grid) {
-//         int m = grid.size(), n = grid[0].size();
-//         return sumPaths(m - 1, n  - 1, grid);                                          
-//     };
-// };
-
-// class Solution {
-// public:
-//     int sumPaths(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-//         if (i == 0 && j == 0) return grid[0][0];
-//         if (i < 0 || j < 0) return INT_MAX;
-//         if (dp[i][j] != -1) return dp[i][j];
-//         return dp[i][j] = min(sumPaths(i - 1, j, grid, dp), sumPaths(i, j - 1, grid, dp)) + grid[i][j];
-//     };                                          
-
-    
-//     int minPathSum(vector<vector<int>>& grid) {
-//         int m = grid.size(), n = grid[0].size();
-//         vector<vector<int>> dp(m, vector<int>(n, -1));
-//         return sumPaths(m - 1, n  - 1, grid, dp);                                          
-//     };
-// };
 
 class Solution {
 public:

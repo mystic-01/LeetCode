b949e967582ce24e8d51e6ba0781f3183d5ef6df
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-23 09:40:48 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (96.49%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -21,7 +21,20 @@ public:
     
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> ans;
-        postOrder(root, ans);
+        // postOrder(root, ans);
+        if (!root) return {};
+        stack<TreeNode*> s;
+        s.push(root);
+        
+        while (!s.empty()) {
+            root = s.top();
+            s.pop();
+            ans.push_back(root->val);
+            if (root->left) s.push(root->left);
+            if (root->right) s.push(root->right);
+        };
+        
+        reverse(ans.begin(), ans.end());        
         return ans;
     };
 };

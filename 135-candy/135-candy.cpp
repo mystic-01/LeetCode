cc7200206f7f09f7f423acf5d80d02a5fc3de06d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-23 09:58:48 +0530

    Time: 20 ms (88.84%), Space: 17.7 MB (65.17%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int candy(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> candies(n, 1);
+        int ans = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) candies[i] = 1 + candies[i - 1];
+        };
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] > nums[i + 1]) candies[i] = max(candies[i], 1 + candies[i + 1]);
+            ans += candies[i];
+        };
+        ans += candies[n - 1];
+        return ans;
+    };
+};

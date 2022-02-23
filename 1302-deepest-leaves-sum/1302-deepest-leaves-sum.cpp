c831c317eef90daed74bfdd14144b7ab9a1e0690
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-23 07:57:00 +0530

    Time: 157 ms (41.31%), Space: 59.7 MB (95.27%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,22 +11,34 @@
  */
 class Solution {
 public:
-    int deepestLeavesSum(TreeNode* root) {
-        int sum = 0;
-        if (!root) return sum;
-        queue<TreeNode*> q;
-        q.push(root);
+    int dfs(TreeNode* root, int depth, int& deepest, int& sum) {
+        if (!root) return 0;
+        if (!root->left && !root->right)
+            if (depth == deepest) sum += root->val;
+            else if (depth > deepest) sum = root->val, deepest = depth;
         
-        while(!q.empty()) {
-            int size = q.size(), curr = 0;
-            for (auto i = 0; i < size; i++) {
-                if (q.front()->left) q.push(q.front()->left);                
-                if (q.front()->right) q.push(q.front()->right);   
-                curr += q.front()->val;
-                q.pop();
-            };
-            sum = curr;
-        };
+        dfs(root->left, depth + 1, deepest, sum);
+        dfs(root->right, depth + 1, deepest, sum);
         return sum;
+    };
+    
+    int deepestLeavesSum(TreeNode* root) {
+//         int sum = 0;
+//         if (!root) return sum;
+//         queue<TreeNode*> q;
+//         q.push(root);
+        
+//         while(!q.empty()) {
+//             int size = q.size(), curr = 0;
+//             for (auto i = 0; i < size; i++) {
+//                 if (q.front()->left) q.push(q.front()->left);                
+//                 if (q.front()->right) q.push(q.front()->right);   
+//                 curr += q.front()->val;
+//                 q.pop();
+//             };
+//             sum = curr;
+//         };
+        int deepest = 0, sum = 0;
+        return dfs(root, 0, deepest, sum);
     };
 };

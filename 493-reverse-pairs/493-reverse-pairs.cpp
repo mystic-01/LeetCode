285f14e5a63eca5ceb2107eda361efa5e9e4059e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-23 20:51:19 +0530

    Time: 1125 ms (27.94%), Space: 223 MB (37.81%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-    int merge(int l, int r, int mid, vector<int> &arr) {
+    int merge(int l, int r, int mid, vector<int> &nums) {
         int i = l, j = mid, count = 0;
-        while (i < mid) {
-            while (j <= r && arr[i] > 2LL * arr[j]) j++;
+        while (i <= mid - 1) {
+            while (j <= r && nums[i] > 2LL * nums[j]) j++;
             count += (j - mid);
             i++;
         };
 
         i = l, j = mid;
         vector<int> temp;
-        while ((i <= mid -1) && (j <= r))
-            temp.push_back(arr[i] < arr[j] ? arr[i++] : arr[j++]);
+        while (i <= mid - 1 && j <= r)
+            temp.push_back(nums[i] < nums[j] ? nums[i++] : nums[j++]);
         
         while (i <= mid -1)
-            temp.push_back(arr[i++]);
+            temp.push_back(nums[i++]);
         
         while (j <= r)
-            temp.push_back(arr[j++]);
+            temp.push_back(nums[j++]);
         
         for (int i = l; i <= r; i++)
-            arr[i] = temp[i - l];
+            nums[i] = temp[i - l];
         
         return count;
     };
 
     int mergeSort(int l, int r, vector<int> &arr) {
-        if (l >= r) return 0;
-        int mid = (l + r) / 2;
-        int count = mergeSort(l, mid, arr);
-        count += mergeSort(mid + 1, r, arr);
-        count += merge(l, r, mid + 1, arr);
+        int count = 0;
+        if (r > l) {
+            int mid = (l + r) / 2;
+            count += mergeSort(l, mid, arr);
+            count += mergeSort(mid + 1, r, arr);
+            
+            count += merge(l, r, mid + 1, arr);
+        }; 
         return count;
+        
     };
 
     int reversePairs(vector<int>& nums) {

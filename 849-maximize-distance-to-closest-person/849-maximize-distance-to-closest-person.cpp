2d942d94b3c6a8a9f5861ef57fcaee13e0b40c08
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-24 13:27:04 +0530

    Time: 11 ms (89.24%), Space: 17.1 MB (32.66%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -1,20 +1,49 @@
+// class Solution {
+// public:
+//     int maxDistToClosest(vector<int>& nums) {
+//         int n = nums.size(), lastOne = -1e7, best = -1e7;
+//         vector<int> v(n, -1e7);
+//         for (auto i = 0; i < n; i++) {
+//             if (nums[i]) lastOne = i;
+//             else {
+//                 if (lastOne != -1e7) v[i] = i - lastOne;
+//                 else v[i] = 1e7;
+//             };
+//         };
+//         lastOne = 1e7;
+//         for (auto i = n - 1; i >= 0; i--) {
+//             if (nums[i]) lastOne = i;
+//             else v[i] = min(v[i], lastOne - i);
+//             best = max(best, v[i]);
+//         };
+//         return best;
+//     };
+// };
+
 class Solution {
 public:
     int maxDistToClosest(vector<int>& nums) {
-        int n = nums.size(), lastOne = -1e7, best = -1e7;
-        vector<int> v(n, -1e7);
+        int n = nums.size(), zeros = 0, best = INT_MIN;
         for (auto i = 0; i < n; i++) {
-            if (nums[i]) lastOne = i;
+            if (nums[i]) zeros = 0;
             else {
-                if (lastOne != -1e7) v[i] = i - lastOne;
-                else v[i] = 1e7;
+                zeros++;
+                best = max(best, (zeros + 1) / 2);
             };
         };
-        lastOne = 1e7;
+        
+        zeros = 0;
         for (auto i = n - 1; i >= 0; i--) {
-            if (nums[i]) lastOne = i;
-            else v[i] = min(v[i], lastOne - i);
-            best = max(best, v[i]);
+            if (nums[i]) break;
+            zeros++;
+            best = max(best, zeros);
+        };
+        
+        zeros = 0;
+        for (auto i = 0; i < n; i++) {
+            if (nums[i]) break;
+            zeros++;
+            best = max(best, zeros);
         };
         return best;
     };

155cd10d7b25e0ced7e9fb4dcad3e624f25599c2
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-24 22:36:58 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (89.43%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        int n = s.length(), i = 0;
+        string ans = "";
+        while (i < s.length()) {
+            if (isdigit(s[i])) {
+                string num = "", tbr = "";
+                while (isdigit(s[i])) num += s[i++];
+                int size = stoi(num);
+                i++;
+                int openBrackets = 1, closedBrackets = 0; 
+                while (openBrackets != closedBrackets) {
+                    if (s[i] == '[') openBrackets++;
+                    else if (s[i] == ']') closedBrackets++;
+                    tbr += s[i++];
+                };
+                
+                tbr = decodeString(tbr);
+                
+                while (size--) ans += tbr;
+            } else if (isalpha(s[i])) {
+                ans += s[i++];
+            } else i++;
+        };
+        return ans;
+    };
+};

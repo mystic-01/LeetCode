eab7e6514e0eef6949a6d5612d9c655a4655531f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-24 09:57:00 +0530

    Time: 380 ms (25.96%), Space: 84.2 MB (63.49%) - LeetHub

---
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -0,0 +1,15 @@
+class StockSpanner {
+public:
+    stack<pair<int, int>> s;
+    
+    StockSpanner() {
+        
+    };
+    
+    int next(int price) {
+        int curr = 1;
+        while(!s.empty() && s.top().first <= price) curr += s.top().second, s.pop();
+        s.push({price, curr});
+        return curr;
+    };
+};

c63af32b4b5b1aac0032e2e6b57ce9145d5067e6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-25 12:55:31 +0530

    Time: 7 ms (49.13%), Space: 10.6 MB (75.54%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -1,29 +1,47 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         int count = -1;
+//         ListNode * dummy = new ListNode(69), * temp = dummy;
+//         dummy->next = head;
+//         while (temp) {
+//             temp = temp->next;
+//             count++;  
+//         };
+//         temp = dummy;
+//         while (temp) {
+//             if (count == n) temp->next = temp->next->next;
+//             temp = temp->next;
+//             count--;
+//         };
+//         return dummy->next;
+//     };
+// };
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int count = -1;
-        ListNode * dummy = new ListNode(69), * temp = dummy;
+        ListNode * dummy = new ListNode(69), * slow = dummy, * fast = dummy;
         dummy->next = head;
-        while (temp) {
-            temp = temp->next;
-            count++;  
+        int count = n;
+        while (count--) {
+            fast = fast->next;            
         };
-        temp = dummy;
-        while (temp) {
-            if (count == n) temp->next = temp->next->next;
-            count--;
-            temp = temp->next;
+        while (fast->next) {
+            slow = slow->next;
+            fast = fast->next;
         };
+        slow->next = slow->next->next;
         return dummy->next;
     };
 };
+
+
+
+
+
+
+
+
+
+
+

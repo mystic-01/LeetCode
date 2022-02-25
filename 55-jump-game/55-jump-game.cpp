5c95adaf6ee6acc516a1ea27cd5d3bb09de71879
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-26 01:05:55 +0530

    Time: 94 ms (57.93%), Space: 48.5 MB (28.44%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,24 +1,39 @@
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+//         int n = nums.size();
+//         unordered_map<int, int> m;
+//         if (n == 1) return true;
+//         for (auto i = 0; i < n; i++) {
+//             if (i == n - 1) return true;
+//             if (nums[i]) m[nums[i] + i]++;
+//             else {
+//                 bool canNotJump = true;
+//                 for (auto x : m) {
+//                     if (x.first > i) {
+//                         canNotJump = false;
+//                         x.second--;
+//                         break;
+//                     };
+//                 };
+//                 if (canNotJump) return false;
+//             };
+//         };
+//         return true;
+//     };
+// };
+
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        unordered_map<int, int> m;
-        if (n == 1) return true;
+        int n = nums.size(), reach = 0;
         for (auto i = 0; i < n; i++) {
             if (i == n - 1) return true;
-            if (nums[i]) m[nums[i] + i]++;
-            else {
-                bool canNotJump = true;
-                for (auto x : m) {
-                    if (x.first > i) {
-                        canNotJump = false;
-                        x.second--;
-                        break;
-                    };
-                };
-                if (canNotJump) return false;
-            };
+            reach = max(reach, i + nums[i]);
+            if (reach == i) return false;
         };
         return true;
     };
 };
+
+

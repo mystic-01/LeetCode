dcfa0bd93edba803542657639ba1f94b48d2c13e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-25 15:18:01 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (35.45%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        
+        priority_queue<int, vector<int>> pq;
+        for (auto i = 0; i < n; i++) {
+            pq.push(nums[i]);            
+        };
+        while (pq.size() > 1) {
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+            pq.push(a - b);
+        };
+        return pq.top();
+    };
+};

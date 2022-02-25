b3468db38ed53f4409b12267a244a4634532b5de
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-25 12:16:04 +0530

    Time: 8 ms (77.23%), Space: 14.9 MB (45.50%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,25 +1,50 @@
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+//         ListNode * result = new ListNode(69), * temp = result;
+        
+//         while (l1 && l2) {
+//             if (l1->val < l2->val) {
+//                 temp = temp->next = l1;
+//                 l1 = l1->next;
+//             } else {
+//                 temp = temp->next = l2;
+//                 l2 = l2->next;                
+//             };            
+//         };
+//         if (l1) temp->next = l1;
+//         if (l2) temp->next = l2;
+//         return result->next;  
+//     };
+// };
+
 class Solution {
 public:
+    struct comp {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;            
+        };        
+    };
+    
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        std::ios::sync_with_stdio(false);
-        std::cin.tie(nullptr);
+        priority_queue<ListNode*, vector<ListNode*>, comp> pq;
+        if (l1) pq.push(l1);
+        if (l2) pq.push(l2);
         
-//         if (!l1) return l2;
-//         else if (!l2) return l1;
-
-        ListNode * result = new ListNode(69), * temp = result;
-        
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                temp = temp->next = l1;
-                l1 = l1->next;
-            } else {
-                temp = temp->next = l2;
-                l2 = l2->next;                
-            };            
+        ListNode* dummy = new ListNode(7), * temp = dummy;
+        while (!pq.empty()) {
+            temp->next = pq.top();
+            pq.pop();
+            temp = temp->next;
+            if (temp->next) pq.push(temp->next);
         };
-        if (l1) temp->next = l1;
-        if (l2) temp->next = l2;
-        return result->next;  
+        return dummy->next;
     };
 };
+
+
+
+
+
+
+

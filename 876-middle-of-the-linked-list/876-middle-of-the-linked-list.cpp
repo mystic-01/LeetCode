7684d1021336306e45fb3374288803dfe1108da3
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-25 12:02:50 +0530

    Time: 0 ms (100.00%), Space: 7 MB (71.38%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -1,28 +1,36 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
+// class Solution {
+// public:
+//     int count = 1, middle = 0;
+//     ListNode* middleNode(ListNode* head) {
+//         ListNode *temp = head;
+//         while(temp->next != NULL) {
+//             temp = temp->next;
+//             count++;
+//         }
+//         temp = head, middle = count/2 + 1, count = 1;
+//         while(temp->next != NULL) {
+//             if (count == middle) return temp;
+//             temp = temp->next;
+//             count++;
+//         };
+//         return temp;
+//     };
+// };
+
 class Solution {
-public:
-    int count = 1, middle = 0;
+    public:
     ListNode* middleNode(ListNode* head) {
-        ListNode *temp = head;
-        while(temp->next != NULL) {
-            temp = temp->next;
-            count++;
-        }
-        temp = head, middle = count/2 + 1, count = 1;
-        while(temp->next != NULL) {
-            if (count == middle) return temp;
-            temp = temp->next;
-            count++;
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            if (fast->next) fast = fast->next->next;
         };
-        return temp;
+        return slow;
     };
 };
+
+
+
+
+

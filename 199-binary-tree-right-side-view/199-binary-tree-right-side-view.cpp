1e4b5dc518c1fa69afaed6567c2e3e6d453c66ad
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-25 09:36:09 +0530

    Time: 7 ms (36.67%), Space: 12.1 MB (46.85%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -1,14 +1,6 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Hey, future self. Hope you're not wasting your time. 
+// So, are you smart enough to do this using dfs now?
+
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {

c8a5ceae87a6be04ba09986aa4cd1771e6037706
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-27 02:10:28 +0530

    Time: 28 ms (91.65%), Space: 70.8 MB (95.17%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
-    ListNode* reverse(ListNode* curr) {
+    pair<ListNode*, int> reverse(ListNode* curr) {
+        int size = 0;
         ListNode* prev = nullptr, * newNode = nullptr;
         while (curr) {
             newNode = curr->next;
             curr->next = prev;
             prev = curr;
             curr = newNode;
+            size++;
         };
-        return prev;
+        return {prev, size};
     };
     
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* headA = reverse(l1), * headB = reverse(l2);
+        pair<ListNode*, int> pairA = reverse(l1), pairB = reverse(l2);
+        ListNode * headA = pairA.first, * headB = pairB.first;
+        bool takeFirst = pairA.second > pairB.second;
         
         ListNode* tempA = headA, * tempB = headB, * dummy = new ListNode(7), * d = dummy;
         int carry = 0;
-        while (tempA || tempB || carry) {
-            if (tempA) carry += tempA->val, tempA = tempA->next;
-            if (tempB) carry += tempB->val, tempB = tempB->next;
-            d = d->next = new ListNode(carry % 10);
-            carry /= 10;
+        if (takeFirst) {
+            while (tempA) {
+                carry += tempA->val;
+                if (tempB) carry += tempB->val, tempB = tempB->next;
+                tempA->val = carry % 10;
+                carry /= 10;
+                if (!tempA->next) break;
+                tempA = tempA->next;
+            };
+            if (carry) tempA->next = new ListNode(carry % 10);
+            return reverse(headA).first;
+            
+        } else {
+            while (tempB) {
+                carry += tempB->val;
+                if (tempA) carry += tempA->val, tempA = tempA->next;
+                tempB->val = carry % 10;
+                carry /= 10;
+                if (!tempB->next) break;
+                tempB = tempB->next;
+            };
+            if (carry) tempB->next = new ListNode(carry % 10);   
+            return reverse(headB).first;
         };
-        return reverse(dummy->next);
     };
 };
 

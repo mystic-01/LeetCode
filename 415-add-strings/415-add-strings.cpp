b9dad2f2e09a869eb82bbfee772cce5ffdbba0f3
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-26 15:32:30 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (70.23%) - LeetHub

--- 415-add-strings/415-add-strings.cpp
+++ 415-add-strings/415-add-strings.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int n1 = num1.length(), n2 = num2.length();        
-        int i = n1 - 1, j = n2 - 1, carry = 0;
-        string ans = "";
-        while (i >= 0 || j >= 0 || carry) {
-            int sum = carry;
-            if (i >= 0) sum += num1[i] - '0';
-            if (j >= 0) sum += num2[j] - '0';
-            ans = to_string(sum % 10) + ans;
-            carry = sum / 10;
-            i--, j--;
+        int n1 = num1.length(), n2 = num2.length();
+        
+        int i = n1 - 1, j = n2 - 1, k = n1 - 1, carry = 0;
+
+        if (n1 > n2) {
+            k = n1 - 1;
+            while (i >= 0) {
+                carry += num1[i] - '0';
+                carry += (j >= 0) ? num2[j] - '0' : 0;
+                num1[k--] = char((carry % 10) + '0');
+                carry = carry / 10, i--, j--;
+            };
+            
+            return carry ? char(carry + '0') + num1 : num1;
+        } else {
+            k = n2 - 1;
+            while (j >= 0) {
+                carry += num2[j] - '0';
+                carry += (i >= 0) ? num1[i] - '0' : 0;
+                
+                num2[k--] = char((carry % 10) + '0');
+                carry = carry / 10, i--, j--;
+            };
+            return carry ? char(carry + '0') + num2 : num2;
         };
-        return ans;
     };
 };

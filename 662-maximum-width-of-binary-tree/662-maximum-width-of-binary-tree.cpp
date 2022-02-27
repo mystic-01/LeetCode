c32f3211116f608d56ee015f873289589aac7230
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-28 01:51:47 +0530

    Time: 8 ms (85.49%), Space: 17.4 MB (48.23%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -8,17 +8,17 @@ public:
         int width = 1; 
         while (!q.empty()) {
             int size = q.size();
-            int minVertical = q.front().second, maxVertical = q.back().second;
+            int left = q.front().second, right = q.back().second;
             
             for (auto i = 0; i < size; i++) {
                 TreeNode *front = q.front().first;
-                int idx = q.front().second - minVertical;    
+                int idx = q.front().second - left;    
                 
                 if (front->left) q.push({front->left, (long long)2 * idx + 1}); 
                 if (front->right) q.push({front->right, (long long)2 * idx + 2}); 
                 q.pop();
             };
-            width = max(width, maxVertical - minVertical + 1);
+            width = max(width, right - left + 1);
         };
         return width;
     };

cd5e5fde2e0aa4a679bd1d337fbfd0b57512520d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-02-27 09:44:30 +0530

    Time: 565 ms (5.03%), Space: 114.1 MB (85.70%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -1,24 +1,54 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// class Solution {
+// public:
+//     // O(n) Space solution.
+//     bool isPalindrome(ListNode* head) {
+//         vector<int> v;
+//         ListNode* temp = head;
+//         while (temp) {
+//             v.push_back(temp->val);
+//             temp = temp->next;
+//         };
+//         int i = 0, j = v.size() - 1;
+//         while (i < j && v[i] == v[j]) i++, j--;
+//         return v[i] == v[j];
+//     };
+// };
+
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        vector<int> v;
-        ListNode* temp = head;
-        while (temp) {
-            v.push_back(temp->val);
-            temp = temp->next;
+    ListNode *reverse(ListNode *curr) {
+        ListNode *prev = nullptr, *newNode = nullptr;
+        while (curr) {
+            newNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = newNode;
         };
-        int i = 0, j = v.size() - 1;
-        while (i < j && v[i] == v[j]) i++, j--;
-        return v[i] == v[j];
+        return prev;
+    };
+    
+    // O(1) Space solution.
+    bool isPalindrome(ListNode *head) {
+        ListNode *slow = head, *fast = head, *dummySlow;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        };
+        if (!fast) {
+            // Even list
+            dummySlow = slow;
+        } else {
+            // Odd list
+            dummySlow = slow;
+            slow = slow->next;
+        };
+        fast = head;
+        slow = reverse(slow);
+        while (fast != dummySlow) {
+            if (fast->val != slow->val) return false;
+            fast = fast->next, slow = slow->next;
+        };
+        return true;
     };
 };
+

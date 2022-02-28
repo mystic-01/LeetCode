c29b574aef3644de21daf6cb818a9999f692c4df
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-03-01 02:37:19 +0530

    Time: 185 ms (70.14%), Space: 85.4 MB (44.45%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, vector<int>& ans) {
+        if (!root) return;
+        dfs(root->left, ans);
+        ans.push_back(root->val);
+        dfs(root->right, ans);        
+    };
+    
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> tree1, tree2;
+        dfs(root1, tree1);        
+        dfs(root2, tree2);
+        int l1 = tree1.size(), l2 = tree2.size();
+        int i = l1 - 1, j = l2 - 1, k = l1 + l2 - 1;
+        tree1.resize(l1 + l2);
+        
+        while(i >= 0 && j >= 0) tree1[k--] = (tree1[i] > tree2[j]) ? tree1[i--] : tree2[j--];            
+        while(j >= 0) tree1[k--] = tree2[j--];            
+        return tree1;
+    };
+};

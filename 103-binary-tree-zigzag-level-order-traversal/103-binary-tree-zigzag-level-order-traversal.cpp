961dfe5773d6564c9e59f36f94fbfd7e85698377
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-03-02 12:07:33 +0530

    Time: 3 ms (77.87%), Space: 12.2 MB (47.45%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -9,29 +9,64 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+//         vector<vector<int>> ans;
+//         if (!root) return ans;
+//         queue<TreeNode*> q;
+//         q.push(root);
+//         bool isReverse = false;
+//         int level = 0;
+//         while (!q.empty()) {
+//             int size = q.size();
+//             vector<int> ds(size);
+//             for (auto i = 0; i < size; i++) {
+//                 if (q.front()->left) q.push(q.front()->left);
+//                 if (q.front()->right) q.push(q.front()->right);
+                
+//                 ds[level % 2 ? size - i - 1 : i] = (q.front()->val);
+//                 q.pop();
+//             };
+//             ans.push_back(ds);
+//             isReverse = !isReverse;
+//             level++;
+//         };
+//         return ans;
+//     };
+// };
+
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ans;
         if (!root) return ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        bool isReverse = false;
-        int level = 0;
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> ds(size);
-            for (auto i = 0; i < size; i++) {
-                if (q.front()->left) q.push(q.front()->left);
-                if (q.front()->right) q.push(q.front()->right);
-                
-                ds[level % 2 ? size - i - 1 : i] = (q.front()->val);
-                q.pop();
+        stack<TreeNode*> dqL, dqR;
+        dqL.push(root);
+        
+        while (!dqL.empty() || !dqR.empty()) {
+            vector<int> ds;
+            while (!dqL.empty()) {
+                TreeNode* curr = dqL.top();
+                dqL.pop();
+                ds.push_back(curr->val);
+                if (curr->left) dqR.push(curr->left);
+                if (curr->right) dqR.push(curr->right);
             };
-            ans.push_back(ds);
-            isReverse = !isReverse;
-            level++;
+            if (!ds.empty()) ans.push_back(ds), ds.clear();
+                
+            while (!dqR.empty()) {
+                TreeNode* curr = dqR.top();
+                dqR.pop();
+                ds.push_back(curr->val);
+                if (curr->right) dqL.push(curr->right);
+                if (curr->left) dqL.push(curr->left);
+            };
+            
+            if (!ds.empty()) ans.push_back(ds), ds.clear();            
         };
         return ans;
     };
 };
+
+

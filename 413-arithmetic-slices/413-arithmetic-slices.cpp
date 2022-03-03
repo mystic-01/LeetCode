e71721cc275380b8aadce9580c6aaba027b5dd6f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-03-03 15:37:37 +0530

    Time: 3 ms (66.63%), Space: 7.4 MB (16.43%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -3,13 +3,13 @@ public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
         if (n < 3) return 0;
-        int diff = nums[1] - nums[0], count = 0, left = 0;
+        int diff = nums[1] - nums[0], left = 0, count = 0;
         
         for (auto i = 2; i < n; i++) {
-            if (nums[i] - nums[i - 1] != diff) {
-                diff = nums[i] - nums[i - 1];
-                left = i - 1;
-            };
+            int currDiff = nums[i] - nums[i - 1]; 
+            
+            if (currDiff != diff) diff = currDiff, left = i - 1;
+
             int window = i - left + 1;
             if (window >= 3) count += (window - 3  + 1);
         };

55a069175f65b88f8398c5035a137b4bfa9b4fe0
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-03-06 14:27:03 +0530

    Time: 10 ms (45.25%), Space: 12.7 MB (39.70%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -1,52 +1,42 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-// class Solution {
-// public:
-//     void toLinkedList(TreeNode* root, TreeNode*& prev) {
-//         if (!root) return;
-        
-//         toLinkedList(root->right, prev);
-//         toLinkedList(root->left, prev);
-        
-//         root->right = prev;
-//         root->left = nullptr;
-//         prev = root;
-//     };
-    
-//     void flatten(TreeNode* root) {
-//         TreeNode* prev = nullptr;
-//         toLinkedList(root, prev);
-//     };
-// };
-
 class Solution {
 public:
+    void toLinkedList(TreeNode* root, TreeNode*& prev) {
+        if (!root) return;
+        
+        toLinkedList(root->right, prev);
+        toLinkedList(root->left, prev);
+        
+        root->right = prev;
+        root->left = nullptr;
+        prev = root;
+    };
     
     void flatten(TreeNode* root) {
-        TreeNode* curr = root;
-        while (curr) {
-            if (curr->left) {
-                TreeNode* prev = curr->left;
-                while (prev->right) {
-                    prev = prev->right;    
-                };
-                prev->right = curr->right;
-                curr->right = curr->left;
-                curr->left = nullptr;
-            };    
-            curr = curr->right;
-        };
+        TreeNode* prev = nullptr;
+        toLinkedList(root, prev);
     };
 };
+
+// Using Morris Traversal O(1) Space 
+// class Solution {
+// public:
+    
+//     void flatten(TreeNode* root) {
+//         TreeNode* curr = root;
+//         while (curr) {
+//             if (curr->left) {
+//                 TreeNode* prev = curr->left;
+//                 while (prev->right) {
+//                     prev = prev->right;    
+//                 };
+//                 prev->right = curr->right;
+//                 curr->right = curr->left;
+//                 curr->left = nullptr;
+//             };    
+//             curr = curr->right;
+//         };
+//     };
+// };
 
 
 

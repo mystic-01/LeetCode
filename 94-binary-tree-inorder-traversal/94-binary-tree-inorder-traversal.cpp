12b7ca3f9a927030f867eb9ed96c763b46b6e04d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-03-06 11:44:30 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (42.90%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,40 +1,91 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+// class Solution {
+// public:
+//     void inOrder (TreeNode* root, vector<int>& ans) {
+//         if (root == nullptr) return;
+        
+//         inOrder (root->left, ans);
+//         ans.push_back(root->val);
+//         inOrder (root->right, ans);
+//     };
+    
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         // inOrder(root, ans);
+//         TreeNode* node = root;
+//         stack<TreeNode*> s;
+//         while (true) {
+//             if (node) {
+//                 s.push(node);
+//                 node = node->left;
+//             } else {
+//                 if (s.empty()) break;
+//                 ans.push_back(s.top()->val);
+//                 node = s.top()->right;
+//                 s.pop();
+//             };                        
+//         };
+//         return ans;        
+//     };
+// };
+
+
 class Solution {
 public:
-    void inOrder (TreeNode* root, vector<int>& ans) {
-        if (root == nullptr) return;
-        
-        inOrder (root->left, ans);
-        ans.push_back(root->val);
-        inOrder (root->right, ans);
-    };
     
     vector<int> inorderTraversal(TreeNode* root) {
+        // Morris Traversal
         vector<int> ans;
-        // inOrder(root, ans);
-        TreeNode* node = root;
-        stack<TreeNode*> s;
-        while (true) {
-            if (node) {
-                s.push(node);
-                node = node->left;
+        if (!root) return ans;
+        while (root) {
+            if (!root->left) {
+                ans.push_back(root->val);
+                root = root->right;
             } else {
-                if (s.empty()) break;
-                ans.push_back(s.top()->val);
-                node = s.top()->right;
-                s.pop();
-            };                        
+                TreeNode* prev = root->left;
+                while (prev->right && prev->right != root) {
+                    prev = prev->right;  
+                };
+                if (!prev->right) {
+                    prev->right = root;
+                    root = root->left;
+                } else {
+                    ans.push_back(prev->right->val);
+                    root = prev->right->right; 
+                    prev->right = nullptr;
+                
+                };
+            };                               
         };
         return ans;        
     };
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

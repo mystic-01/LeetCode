cfc66a881301c4c08fdbd943d30335319b0eed86
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-03-06 14:25:57 +0530

    Time: 9 ms (48.87%), Space: 12.7 MB (78.52%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -9,21 +9,56 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void toLinkedList(TreeNode* root, TreeNode*& prev) {
+//         if (!root) return;
+        
+//         toLinkedList(root->right, prev);
+//         toLinkedList(root->left, prev);
+        
+//         root->right = prev;
+//         root->left = nullptr;
+//         prev = root;
+//     };
+    
+//     void flatten(TreeNode* root) {
+//         TreeNode* prev = nullptr;
+//         toLinkedList(root, prev);
+//     };
+// };
+
 class Solution {
 public:
-    void toLinkedList(TreeNode* root, TreeNode*& prev) {
-        if (!root) return;
-        
-        toLinkedList(root->right, prev);
-        toLinkedList(root->left, prev);
-        
-        root->right = prev;
-        root->left = nullptr;
-        prev = root;
-    };
     
     void flatten(TreeNode* root) {
-        TreeNode* prev = nullptr;
-        toLinkedList(root, prev);
+        TreeNode* curr = root;
+        while (curr) {
+            if (curr->left) {
+                TreeNode* prev = curr->left;
+                while (prev->right) {
+                    prev = prev->right;    
+                };
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = nullptr;
+            };    
+            curr = curr->right;
+        };
     };
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

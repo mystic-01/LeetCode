249b9197c66f4ac87c5c947a88bc3aa15fa5737b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-03-09 01:13:30 +0530

    Time: 934 ms (8.36%), Space: 177.5 MB (19.28%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int n = nums.size();
-        multiset<int> s;
+        map<int, int> m;
         vector<int> ans;
         for (auto i = 0; i < n; i++) {
-            s.insert(nums[i]);
-            if (i >= k) s.erase(s.find(nums[i - k]));
-            if (i >= k - 1) ans.push_back(*s.rbegin());
+            m[nums[i]]++;
+            if (i >= k) if (!--m[nums[i - k]]) m.erase(nums[i - k]);
+            if (i >= k - 1) ans.push_back(m.rbegin()->first);
         };
         return ans;
     };

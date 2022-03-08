97565b6701b30fb61a1d40b7777e591185fff0d6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-03-08 21:10:24 +0530

    Time: 20 ms (94.86%), Space: 14.2 MB (26.51%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -2,14 +2,6 @@ class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size(), count = 0;
-        vector<vector<int>> edges(n);
-        for (auto i = 0; i < n; i++) {
-            for (auto j = 0; j < n; j++) {
-                if (j != i && isConnected[i][j]) {
-                    edges[i].push_back(j);
-                };
-            };
-        };
         vector<int> visited(n);
         for (auto i = 0; i < n; i++) {
             if (!visited[i]) {
@@ -19,8 +11,9 @@ public:
                 visited[i] = 1;
                 while(!q.empty()) {
                     int node = q.front(); q.pop();
-                    for (auto &x : edges[node]) {
-                        if (!visited[x]) q.push(x), visited[x] = 1;    
+                    for (auto x = 0; x < n; x++) {
+                        if (isConnected[x][node] && !visited[x]) 
+                            q.push(x), visited[x] = 1;    
                     };
                 };
             };

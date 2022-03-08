06eff1956d4e788398aea530951a9ba10edb41bb
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-03-08 09:21:52 +0530

    Time: 13 ms (61.51%), Space: 8.1 MB (58.87%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -1,21 +1,10 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-       
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
         ListNode* slow = head, * fast = head;
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;            
-            fast = fast->next->next;  
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
             if (slow == fast) return true;
         };
         return false;

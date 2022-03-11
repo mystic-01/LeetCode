a651fdf462a68306b7fd737d3fa49c82bb9d2b10
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-03-11 12:15:56 +0530

    Time: 5 ms (18.41%), Space: 6.4 MB (51.48%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        stack<char> paren;
-        for (char& c : s) {
-            switch (c) {
-                case '(': paren.push(c); break;
-                case '{': paren.push(c); break;
-                case '[': paren.push(c); break;
-                case ')': if (paren.empty() || paren.top()!='(') return false; else paren.pop(); break;
-                case '}': if (paren.empty() || paren.top()!='{') return false; else paren.pop(); break;
-                case ']': if (paren.empty() || paren.top()!='[') return false; else paren.pop(); break;
-                default: ;
-            };
+    bool isValid(string str) {
+        stack<char> s;
+        for (auto &x : str) {
+            switch (x) {
+                case '(': s.push(x); break;
+                case '{': s.push(x); break;
+                case '[': s.push(x); break;
+                case ')': if (s.empty() || s.top() != '(') return false; else s.pop(); break;
+                case '}': if (s.empty() || s.top() != '{') return false; else s.pop(); break;
+                case ']': if (s.empty() || s.top() != '[') return false; else s.pop(); break;
+            };    
         };
-        return paren.empty();
-    }
+        return s.empty();
+    };
 };

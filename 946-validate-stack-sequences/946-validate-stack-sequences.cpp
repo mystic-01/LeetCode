7eb335584d16ad5ac1478b200103f075f820ec75
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-03-16 13:02:48 +0530

    Time: 8 ms (83.81%), Space: 15.4 MB (15.68%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -3,14 +3,9 @@ public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         int n = pushed.size(), j = 0;
         stack<int> s;
-        for(auto i = 0; i < n; i++){
-            if(pushed[i] != popped[j]){
-                s.push(pushed[i]);
-            }else{
-                j++;
-                while(!s.empty() && popped[j] == s.top()) s.pop(), j++;
-            }
-        }
+        for(auto i = 0; i < n; i++)
+            if (pushed[i] != popped[j]) s.push(pushed[i]);
+            else while(++j && !s.empty() && popped[j] == s.top()) s.pop();
         return s.empty();
     };
 };

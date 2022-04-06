50ff4bceb0c868a60343419f0181247a9df8fece
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-04-07 03:08:05 +0530

    Time: 79 ms (68.73%), Space: 16.3 MB (5.84%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    int combinatrics(int N, int r) {
+    long long combinatrics(int N, int r) {
         long long int res = 1, mod = 1e9 + 7;
-        for (auto i = 1; i <= r; i++) {
-            res = res * (N - r + i) / i; 
-            res %= mod;
-        };
-        return (int)res;
+        for (auto i = 1; i <= r; i++)
+            res = res * (N - r + i) / i, res %= mod; 
+        return res;
     };
     
     int threeSumMulti(vector<int>& nums, int target) {
@@ -14,26 +12,21 @@ public:
         unordered_map<int, int> m;
         for (auto &it : nums) m[it]++;
         
-        int n = nums.size(), i = 0, mod = 1e9 + 7;
-        long long int count = 0;
+        int n = nums.size(), i = 0, j, k, a, b, c;
+        long long int count = 0, prod = 1;
         while (i < n - 2) {
-            int a = nums[i], j = i + 1, k = n - 1;
+            a = nums[i], j = i + 1, k = n - 1;
             while (j < k) {
-                int b = nums[j], c = nums[k];
+                b = nums[j], c = nums[k];
                 if (b + c == target - a) {
-                    long long int prod = 1;
+                    prod = 1;
                     unordered_map<int, int> triplet;
                     triplet[a]++, triplet[b]++, triplet[c]++;
-                    for (auto &x : triplet) {
-                    // ( a * b) % c = ( ( a % c ) * ( b % c ) ) % c
-                        long long int curr = combinatrics(m[x.first], x.second);
-                        prod = ((prod % mod) * (curr % mod)) % mod;
-                    };
-                    count = ((count % mod) + (prod % mod)) % mod;
+                    for (auto &x : triplet) prod *= combinatrics(m[x.first], x.second);;
+                    count += prod;
                     while (j < n && nums[j] == b) j++;
                     while (k >= 0 && nums[k] == c) k--;
-                }              
-                else if (b + c < target - a) while (j < n && nums[j] == b) j++;
+                } else if (b + c < target - a) while (j < n && nums[j] == b) j++;
                 else while (k >= 0 && nums[k] == c) k--;
             };
             while (i < n - 2 && nums[i] == a) i++;

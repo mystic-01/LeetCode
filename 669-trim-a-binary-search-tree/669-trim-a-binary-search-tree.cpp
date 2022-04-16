e8694afbefe8101eb66a3968d524f75056e7a9af
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-04-16 19:11:15 +0530

    Time: 31 ms (18.73%), Space: 23.8 MB (74.86%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -1,17 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
+    void trimmer(TreeNode* root, int low, int high) {
+        if (!root) return;
+        if (root->left) {
+            if (root->left->val < low) {
+                root->left = root->left->right;
+                trimmer(root, low, high);
+            };
+            trimmer(root->left, low, high);
+        };
+        if (root->right) {
+            if (root->right->val > high) {
+                root->right = root->right->left;
+                trimmer(root, low, high);
+            } else if (root->right->val < low) {
+                root->right = root->right->right;
+                trimmer(root, low, high);
+            };
+            trimmer(root->right, low, high);
+        };
+    };
+    
     TreeNode* trimBST(TreeNode* root, int low, int high) {
-        if(!root) return nullptr;
-        
-        if(root->val < low) {
-            return trimBST(root->right, low, high);
-        } else if (root->val > high) {
-            return trimBST(root->left, low, high);
-        }
-        
-        root->left = trimBST(root->left, low, high);
-        root->right = trimBST(root->right, low, high);
-        
-        return root;
-    }
+        TreeNode* dummy = new TreeNode(low, nullptr, root);
+        trimmer(dummy, low, high);
+        return dummy->right;
+    };
 };

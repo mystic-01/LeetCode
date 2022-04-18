0db1f5e7e19456a0092603c1544b3971451fa841
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-04-18 13:35:38 +0530

    Time: 28 ms (6.49%), Space: 23.9 MB (51.93%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -11,30 +11,14 @@
  */
 class Solution {
 public:
-    void trimmer(TreeNode* root, int low, int high) {
-        if (!root) return;
-        if (root->left) {
-            if (root->left->val < low) {
-                root->left = root->left->right;
-                trimmer(root, low, high);
-            };
-            trimmer(root->left, low, high);
-        };
-        if (root->right) {
-            if (root->right->val > high) {
-                root->right = root->right->left;
-                trimmer(root, low, high);
-            } else if (root->right->val < low) {
-                root->right = root->right->right;
-                trimmer(root, low, high);
-            };
-            trimmer(root->right, low, high);
-        };
-    };
     
     TreeNode* trimBST(TreeNode* root, int low, int high) {
-        TreeNode* dummy = new TreeNode(low, nullptr, root);
-        trimmer(dummy, low, high);
-        return dummy->right;
+        if (!root) return root;
+        if (root->val < low) return trimBST(root->right, low, high);
+        else if (root->val > high) return trimBST(root->left, low, high);
+
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
+        return root;
     };
 };

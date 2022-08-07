c609dd889ded207924255946d4523916ce24451c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-08-07 14:33:16 +0530

    Time: 126 ms (30.72%), Space: 45.2 MB (30.14%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,78 @@
+struct Node {
+    Node* links[26] = { nullptr };
+    bool flag = false;
+    
+    bool contains(char c) {
+        return links[c - 'a'];
+    };
+    
+    void put(char c) {
+        links[c - 'a'] = new Node();
+    };
+    
+};
+
+class Trie {
+private:
+    Node* root;
+    
+public:
+    Trie() {
+        root = new Node();
+    };
+    
+    void insert(string word) {
+        Node* temp = root;
+        for (auto i = 0; i < word.length(); i++) {
+            if (!temp->contains(word[i])) {
+                temp->put(word[i]);
+            };
+            temp = temp->links[word[i] - 'a'];
+        };
+        temp->flag = true;
+    };
+    
+    bool finder(string word, string fun) {
+       Node* temp = root;
+        for (auto i = 0; i < word.length(); i++) {
+            if (!temp->contains(word[i])) {
+                return false;
+            };
+            temp = temp->links[word[i] - 'a'];
+        };
+        return fun == "search" ? temp->flag : true;
+    };
+    
+    bool search(string word) {
+        return finder(word, "search");
+    };
+    
+    bool startsWith(string prefix) {
+        return finder(prefix, "startsWith");
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */

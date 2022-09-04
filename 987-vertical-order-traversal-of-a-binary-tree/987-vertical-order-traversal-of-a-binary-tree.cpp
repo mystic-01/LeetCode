7b4bd6723dd087a485f390b6438e1185ce5ac3ec
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-04 19:32:04 +0530

    Time: 4 ms (83.68%), Space: 12.8 MB (73.97%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-//     struct triplet {
-//         TreeNode* node;
-//         int x;
-//         int y;
-//     };
+    struct triplet {
+        TreeNode* node;
+        int vertical;
+        int level;
+        triplet (TreeNode* curr, int y, int x) {
+            vertical = y, level = x, node = curr;           
+        };
+    };
     
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         
         // y, x, val
         map<int, map<int, multiset<int>>> m;
-        queue<pair<TreeNode*, pair<int, int>>> q;
-        q.push({root, {0, 0}});
+        queue<triplet> q;
+        q.push(triplet(root, 0, 0));
             
         while (!q.empty()) {
             int size = q.size();
             for (auto i =  0; i < size; i++) {
-                TreeNode* curr = q.front().first;     
-                int y = q.front().second.first, x = q.front().second.second;  
+                TreeNode* curr = q.front().node;     
+                int y = q.front().vertical, x = q.front().level;  
                 q.pop();
-                if (curr->left) q.push({curr->left, {y - 1, x + 1}});
-                if (curr->right) q.push({curr->right, {y + 1, x + 1}});
+                if (curr->left) q.push(triplet(curr->left, y - 1, x + 1));
+                if (curr->right) q.push(triplet(curr->right, y + 1, x + 1));
                 m[y][x].insert(curr->val);
             };
         };

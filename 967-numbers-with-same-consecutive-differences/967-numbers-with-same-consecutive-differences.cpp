43a3a548967b723c5cc48595afc0c60583a0286c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-04 18:09:47 +0530

    Time: 3 ms (73.88%), Space: 8.9 MB (15.92%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,38 +1,53 @@
+// Think!!! You got this!
+// class Solution {
+// public:
+//     vector<int> numsSameConsecDiff(int n, int k) {
+//         // n = 9, k = 1 ->>>not working
+//         vector<int> ans;
+//         int minimum = pow(10, n - 1);
+//         for (auto i = 0; i <= 9; i++) {
+//             int num1 = 0, num2 = 0, add = 0; 
+//             for (auto j = 0; j < n; j++) {
+//                 if (!add) {
+//                     if (num1 != INT_MIN) num1 += (pow(10, j) * i);
+//                     if (num2 != INT_MIN) num2 += (pow(10, j) * i);
+//                 } else {
+//                     if (num1 != INT_MIN && i + k <= 9) num1 += (pow(10, j) * (i + k));
+//                     else num1 = INT_MIN;
+//                     if (num2 != INT_MIN && i - k >= 0) num2 += (pow(10, j) * (i - k));
+//                     else num2 = INT_MIN;
+//                 };
+//                 add = !add;
+//             };
+//             if (num1 >= minimum) ans.push_back(num1);
+//             if (num2 >= minimum) ans.push_back(num2);
+//         };
+//         return ans;
+//     };
+// };
+
 class Solution {
-    
-    vector<int> ans;
-    
-    void Solve(int len , int &k , int num, int digit)
-    {
-        // Base Case
-        if(len == 0 )
-        {
-                ans.push_back(num);
-           return ;
-        }
-        
-        // Next Possible digits will have range from 0 - 9
-        for(int i = 0 ; i<10 ; i++)
-        {
-            // Checking for given condition 
-            if(abs(i-digit) == k)
-                Solve(len-1 , k, num*10 + i, i);
-        }
-    }
-    
 public:
+    void findValidNumbers(int last, vector<int> &ans, int &diff, int power, int ds, int &n, int &bar) {
+        if (power == n) {
+            if (ds >= bar) ans.push_back(ds);
+            return;
+        };
+        int add = ds + (pow(10, power) * (last + diff));
+        int subtract = ds + (pow(10, power) * (last - diff));
+        if (last + diff < 10) findValidNumbers(last + diff, ans, diff, power + 1, add, n, bar);    
+        if (last - diff > -1 && diff != 0) findValidNumbers(last - diff, ans, diff, power + 1, subtract, n, bar);
+        return;
+    };
+    
     vector<int> numsSameConsecDiff(int n, int k) {
-        
-        
-        // First Digit will have range from  1 - 9 
-        // We have passed digit as 4th variable to have the last inserted digit
-        for(int i = 1 ; i<=9 ; i++)
-        {
-             Solve(n-1 , k, i, i);
-        }
-        
+        vector<int> ans;
+        int bar = pow(10, n - 1);
+
+        for (auto i = 0; i <= 9; i++) {
+            findValidNumbers(i, ans, k, 1, i, n, bar);    
+        };
+      
         return ans;
-        
-		// Pls Upvote If You Like It !!
-    }
+    };
 };

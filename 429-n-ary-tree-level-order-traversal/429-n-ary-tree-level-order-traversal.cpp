5496667c7846f3cc55de48af78550375189e267c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-06 02:51:19 +0530

    Time: 27 ms (82.32%), Space: 11.9 MB (34.56%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -1,20 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
 class Solution {
 public:
     vector<vector<int>> levelOrder(Node* root) {
         vector<vector<int>> ans;
         queue<Node*> q;
-        if(!root) return ans;
         q.push(root);
         
         while (!q.empty()) {
-            vector<int> ds;
+            vector<int> level;
             int size = q.size();
-            for (auto i = 0; i < size; i++) {
-                ds.push_back(q.front()->val);
-                for (auto j = 0; j < q.front()->children.size(); j++) q.push(q.front()->children[j]);
+            for (auto i = 0; i < size; i++) { 
+                Node* curr = q.front();
                 q.pop();
+                if (curr) {
+                    for (auto &node : curr->children) if (node) q.push(node);
+                    level.push_back(curr->val);
+                };
             };
-            ans.push_back(ds);
+            if (level.size()) ans.push_back(level);
         };
         return ans;
     };

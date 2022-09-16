b4a2d25a36c9954402d5d8b211c1910e80701525
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-17 01:29:22 +0530

    Time: 351 ms (6.30%), Space: 46.4 MB (22.43%) - LeetHub

---
+++ 654-maximum-binary-tree/654-maximum-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* dfs(int start, int end, map<int, int>::iterator begin, map<int, int>::iterator it, vector<int> &nums) {
+        
+        if (start > end) return nullptr;
+
+        while (it->second < start || it->second > end) {
+            it--;
+            if (it == begin) return nullptr;
+        };
+
+        int val = it->first, idx = it->second;
+        
+        TreeNode* node = new TreeNode(val);        
+        node->left = dfs(start, idx - 1, begin, it, nums);
+        node->right = dfs(idx + 1, end, begin, it, nums);
+        
+        return node;
+    };
+    
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        int n = nums.size();
+        map<int, int> m;
+        
+        for (auto i = 0; i < n; i++) m[nums[i]] = i;
+        auto it = m.end(), begin = m.begin();
+        it--, begin--;
+
+        return dfs(0, n, begin, it, nums);
+    };
+};

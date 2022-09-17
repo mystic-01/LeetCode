850d8406c52d30ae043c318dc1127a4ac18d17a6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-18 02:23:01 +0530

    Time: 3 ms (53.93%), Space: 6.3 MB (71.38%) - LeetHub

---
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool areNumbersAscending(string s) {
+        int n = s.length(), numToBeat = -1;
+        string word = "";
+        for (auto i = 0; i < n; i++) {
+            if (s[i] != ' ') word += s[i];
+            if (s[i] == ' ' || i == n - 1) {
+                int firstCharValue = word[0] - '0';  
+                if (firstCharValue >= 0 && firstCharValue <= 9) {
+                    int num = stoi(word);
+                    if (num <= numToBeat) return false;
+                    else numToBeat = num;
+                };
+                word = "";
+            };
+        };
+        return true;
+    };
+};

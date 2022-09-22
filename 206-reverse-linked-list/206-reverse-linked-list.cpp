54c3016ed9e4a7a486150264824dd34dd5c575c4
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-22 18:48:54 +0530

    Time: 14 ms (28.81%), Space: 8.4 MB (19.36%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* reverse(ListNode* head, ListNode* prev) {
@@ -24,24 +14,20 @@ public:
     
     ListNode* reverseList(ListNode* head) {
         
-//         ListNode* prev = new ListNode(69), * current = head, * next;
-// //         while (current) {
-// //             next = current->next;
-// //             current->next = prev;
-          
-// //             prev = current;
-// //             current = next;
-// //         };
-//         ListNode* ans = reverse(head, prev);
-//         return prev;
-        ListNode *prev = nullptr, *curr = head, *newNode;
-        while (curr) {
-            newNode = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = newNode;
-        };
-        return prev;
+        if (!head) return head;
+        ListNode* prev = new ListNode(69), * current = head, * next;
+        ListNode* ans = reverse(head, prev);
+        ans->next = nullptr;
+        return prev->next;
+        
+        // ListNode *prev = nullptr, *curr = head, *newNode;
+        // while (curr) {
+        //     newNode = curr->next;
+        //     curr->next = prev;
+        //     prev = curr;
+        //     curr = newNode;
+        // };
+        // return prev;
         
     };
 };

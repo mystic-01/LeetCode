0ec1b30f745624251ca0df51d1b2983dbeaa6d7e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-22 20:52:57 +0530

    Time: 3 ms (66.19%), Space: 6.1 MB (6.23%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
-        if (x <= INT_MIN) return 0;
         
         bool isNegative = x < 0;
-        int num =  isNegative ? -1 * x : x, copy = num, digits = 0;
-        long ans = 0;
-        while (copy) digits++, copy /= 10;
+        long num =  x, digits = 0, ans = 0;
+        
+        while (num) digits++, num /= 10;
 
-        copy = num;
-        while (digits--) {
-            ans += (pow(10, digits) * (copy % 10)), copy /= 10;
-        };
-        ans = isNegative ? -1 * ans : ans;
+        num = isNegative ? -1L * x : x;
+        
+        while (digits--) ans += (pow(10, digits) * (num % 10)), num /= 10;
+
+        ans = isNegative ? -1L * ans : ans;
         
         return ans > INT_MIN && ans <= INT_MAX - 1 ? ans : 0;
     };

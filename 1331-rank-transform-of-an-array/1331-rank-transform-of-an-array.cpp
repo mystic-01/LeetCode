fefd2ff8870faddbbb3c090474ece6e7b20bb88d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-24 02:13:45 +0530

    Time: 258 ms (23.54%), Space: 38.9 MB (74.87%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& nums) {
-        int rank = 1; 
+        int n = nums.size(), rank = 1; 
         map<int, int> m;
         
-        for (auto &x : nums) m[x]++;        
+        for (auto i = 0; i < n; i++) m[nums[i]]++;        
         
-        for (auto &i : m) i.second = rank++;
+        for (auto &x : m) x.second = rank++;
         
-        for (auto &num : nums) num = m[num];
+        for (auto i = 0; i < n; i++) nums[i] = m[nums[i]];
         
         return nums;
     };

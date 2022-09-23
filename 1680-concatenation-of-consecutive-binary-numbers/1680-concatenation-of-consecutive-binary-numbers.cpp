1e4de53248860e7bf5701aa777b6b32e54d96509
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-24 00:53:50 +0530

    Time: 99 ms (63.59%), Space: 6 MB (47.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -2,7 +2,8 @@ class Solution {
     
 public:
     int concatenatedBinary(int n) {
-        long ans = 0, mod = 1e9 + 7, i = 1;
+        long ans = 0;
+        int mod = 1e9 + 7, i = 1;
         
         while (i <= n) {
             int len = log2(i) + 1;

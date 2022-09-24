3a52b6a4303e9a4e5c8338fbdcd0b13c5a1d8ab1
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-24 23:37:40 +0530

    Time: 3 ms (89.76%), Space: 12.3 MB (96.49%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -12,19 +12,19 @@
 class Solution {
 public:
     
-    void addNode(TreeNode* root, vector<string> &ans, string ds) {
-        // int initialSize = ds.size();
+    void addNode(TreeNode* root, vector<string> &ans, string &ds) {
+        int initialSize = ds.size();
         ds += to_string(root->val);
 
         if (!root->left && !root->right) ans.push_back(ds);        
         
         ds += "->";
-        // int charAdded = ds.size() - initialSize;
+        int charAdded = ds.size() - initialSize;
         
         if (root->left) addNode(root->left, ans, ds);       
         if (root->right) addNode(root->right, ans, ds);   
         
-        // while (charAdded--) ds.pop_back();
+        while (charAdded--) ds.pop_back();
     };
     
     vector<string> binaryTreePaths(TreeNode* root) {

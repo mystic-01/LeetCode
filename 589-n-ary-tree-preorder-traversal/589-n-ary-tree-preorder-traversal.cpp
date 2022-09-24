00dfa4ac1a45e897e58b3282e97fe1e384dea60a
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-24 22:38:50 +0530

    Time: 64 ms (8.27%), Space: 11.3 MB (48.61%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.cpp
@@ -1,14 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
 class Solution {
 public:
-    void preOrder(Node* root, vector<int>& ans) {
-        if (!root) return;
+    void preOrder(Node* root, vector<int> &ans) {
         ans.push_back(root->val);
-        for (auto i = 0; i < root->children.size(); i++) preOrder(root->children[i], ans);
+        for (auto &node : root->children) preOrder(node, ans);
     };
-
+    
     vector<int> preorder(Node* root) {
         vector<int> ans;
-        preOrder(root, ans);
+        if (root) preOrder(root, ans);
         return ans;
     };
 };

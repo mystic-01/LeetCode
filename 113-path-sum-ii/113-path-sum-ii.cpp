c82e6c089fb7d9e52ed781ee7681b0c7c069ace4
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-24 22:24:58 +0530

    Time: 24 ms (44.02%), Space: 19.9 MB (81.15%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -11,25 +11,21 @@
  */
 class Solution {
 public:
-    void preOrder(TreeNode* root, int target, vector<vector<int>>& ans, vector<int> ds) {
-        if (!root) return;
-        if (!root->left && !root->right) {
-            ds.push_back(root->val);
-            if (target - root->val == 0) ans.push_back(ds);
-            ds.pop_back();
-            return;
-        };
+    void findSum (TreeNode* root, int targetSum, vector<vector<int>> &ans, vector<int> &ds) {
+        targetSum -= root->val;
         ds.push_back(root->val);
-        preOrder(root->left, target - root->val, ans, ds);
-        preOrder(root->right, target - root->val, ans, ds);        
+        if (!root->left && !root->right && !targetSum) ans.push_back(ds);                             
+
+        if (root->left) findSum(root->left, targetSum, ans, ds);
+        if (root->right) findSum(root->right, targetSum, ans, ds);
+        targetSum += root->val;
         ds.pop_back();
     };
-
     
-    vector<vector<int>> pathSum(TreeNode* root, int target) {
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        preOrder(root, target, ans, ds);
+        vector<int> ds;        
+        if (root) findSum(root, targetSum, ans, ds);
         return ans;
     };
 };

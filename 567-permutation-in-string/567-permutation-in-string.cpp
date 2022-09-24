71126781e35b01041bd59c153d09ca047e93e59f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-25 01:32:59 +0530

    Time: 6 ms (90.46%), Space: 7.2 MB (73.94%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -3,7 +3,7 @@ public:
     bool checkInclusion(string s1, string s2) {
         // window mein count 0 toh true else false.
         int v[26] = {0}, n1 = s1.length(), n2 = s2.length();
-        for (auto i : s1) v[i - 'a']++;
+        for (auto &i : s1) v[i - 'a']++;
         
         for (auto i = 0; i < n2; i++) {
             v[s2[i] - 'a']--;

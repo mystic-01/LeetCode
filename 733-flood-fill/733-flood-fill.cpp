08c6b824be1d3b4148f4a02a3bba084558f5b563
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-26 04:31:08 +0530

    Time: 18 ms (43.18%), Space: 14.3 MB (16.01%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        set<pair<int, int>> visited;
+        vector<pair<int, int>> offset = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        int rowSize = image.size(), colSize = image[0].size(), srcColor = image[sr][sc];
+        while (!q.empty()) {
+            int size = q.size();
+            for (auto i = 0; i < size; i++) {
+                int r = q.front().first, c = q.front().second;   
+                q.pop();
+                image[r][c] = color, visited.insert({r, c});
+                for (auto &duo : offset) {
+                    int row = r + duo.first, col = c + duo.second;
+                    if (row > - 1 && row < rowSize && col > - 1 && col < colSize 
+                        && image[row][col] == srcColor && !visited.count({row, col})) 
+                        q.push({row, col});
+                };
+            };
+        };
+        
+        return image;
+    };
+};

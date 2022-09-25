578cbcc6c3828addecdffcac517115272f1fcac4
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-26 04:32:13 +0530

    Time: 20 ms (32.23%), Space: 14.2 MB (25.91%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -4,7 +4,7 @@ public:
         queue<pair<int, int>> q;
         q.push({sr, sc});
         set<pair<int, int>> visited;
-        vector<pair<int, int>> offset = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        int offset[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         
         int rowSize = image.size(), colSize = image[0].size(), srcColor = image[sr][sc];
         while (!q.empty()) {
@@ -14,7 +14,7 @@ public:
                 q.pop();
                 image[r][c] = color, visited.insert({r, c});
                 for (auto &duo : offset) {
-                    int row = r + duo.first, col = c + duo.second;
+                    int row = r + duo[0], col = c + duo[1];
                     if (row > - 1 && row < rowSize && col > - 1 && col < colSize 
                         && image[row][col] == srcColor && !visited.count({row, col})) 
                         q.push({row, col});

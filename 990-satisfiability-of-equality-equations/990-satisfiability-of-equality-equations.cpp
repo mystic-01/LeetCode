de6875fa499c46a83b733730d9f1718acfb1b5af
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-27 01:08:02 +0530

    Time: 12 ms (44.63%), Space: 13.4 MB (6.75%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool equationsPossible(vector<string>& equations) {
+        map<int, set<int>> m;
+        
+        for (auto &str : equations) {
+            int first = str[0] - 'a' + 1, second = str[3] - 'a' + 1;
+            if (first == second) {
+                if (str[1] == '=') continue;
+                else return false;
+            };
+            if (str[1] == '=') m[first].insert(second), m[second].insert(first);        
+            else m[first].insert(-1 * second), m[second].insert(-1 * first);  
+        };
+        
+        for (auto &it : m) {
+            for (auto &x : it.second) if (x > 0) it.second.insert(m[x].begin(), m[x].end());        
+            if (it.second.count(-1 * it.first)) return false;    
+        };
+        for (auto &it : m) {
+            for (auto &x : it.second) if (x > 0) it.second.insert(m[x].begin(), m[x].end());        
+            if (it.second.count(-1 * it.first)) return false;    
+        };
+        
+        return true;
+    };
+};

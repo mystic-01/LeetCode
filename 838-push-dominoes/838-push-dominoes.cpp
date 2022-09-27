bbcd190f6de64d0ccde7817335e09f519dbb5258
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2022-09-28 03:33:19 +0530

    Time: 46 ms (85.59%), Space: 16.6 MB (67.79%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.cpp
+++ 838-push-dominoes/838-push-dominoes.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     string pushDominoes(string &dominos) {
-        int n = dominos.size(), left = 0, right = 0;
-        vector<int> leftEffect(n, 0), rightEffect(n, 0);        
+        int n = dominos.size(), effect = 0;
+        vector<int> right(n, 0);        
         
         for (auto i = 0; i < n; i++) {
-            if (dominos[i] == 'L') leftEffect[i] = 1, right = 0;     
-            else if (dominos[i] == 'R') rightEffect[i] = 1, right = 1;     
-            else if (right) rightEffect[i] = ++right;     
+            if (dominos[i] == 'L') effect = 0;     
+            else if (dominos[i] == 'R') right[i] = 1, effect = 1;     
+            else if (effect) right[i] = ++effect;     
         };
         
         for (auto i = n - 1; i >= 0; i--) {
-            if (dominos[i] == 'R') rightEffect[i] = 1, left = 0;     
-            else if (dominos[i] == 'L') leftEffect[i] = 1, left = 1;     
-            else if (left) leftEffect[i] = ++left;     
-            
-            if (rightEffect[i] < leftEffect[i]) dominos[i] = rightEffect[i] ? 'R' : 'L';
-            else if (leftEffect[i] < rightEffect[i]) dominos[i] = leftEffect[i] ? 'L' : 'R';
-            else dominos[i] = '.';
+            if (dominos[i] == 'R') effect = 0;     
+            else if (dominos[i] == 'L') effect = 1;     
+            else {
+                if (effect) {
+                    effect++;
+                    if (!right[i]) dominos[i] = 'L';
+                    else if (effect < right[i]) dominos[i] = 'L';
+                    else if (effect > right[i]) dominos[i] = 'R';
+                } else if (right[i]) dominos[i] = 'R';
+            };
         };
         
         return dominos;

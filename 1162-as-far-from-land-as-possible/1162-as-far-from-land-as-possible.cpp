858be7e80fb5c927dda5fe9e778e9bd8eec67e91
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-02-11 01:30:11 +0530

    Time: 1815 ms (5.03%), Space: 16.7 MB (95.11%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        vector<pair<int, int>> v;
+        int v[10000][2] = {{}}, it = 0;
         int n = grid.size(), best = 0;
         
         for (auto i = 0; i < n; i++) {
             for (auto j = 0; j < n; j++) {
-                if (grid[i][j]) v.push_back({i, j});
+                if (grid[i][j]) v[it][0] = i, v[it][1] = j, it++;
             };
         }; 
         
-        if (!v.size() || v.size() == (n * n)) return -1;
+        if (!it || it == (n * n)) return -1;
         
         for (auto i = 0; i < n; i++) {
             for (auto j = 0; j < n; j++) {
                 if (!grid[i][j]) {
                     int dist = INT_MAX;
-                    for (auto &loc : v) dist = min(dist, abs(loc.first - i) + abs(loc.second - j));
+                    for (auto k = 0; k < it; k++) dist = min(dist, abs(v[k][0] - i) + abs(v[k][1] - j));
                     best = max(best, dist);
                 };
             };

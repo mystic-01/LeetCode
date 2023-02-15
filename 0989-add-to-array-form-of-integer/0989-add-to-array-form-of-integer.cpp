6c10bb513575a0b9bba71eab4f2f62434c8a0882
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-02-15 23:12:20 +0530

    Time: 30 ms (73.55%), Space: 27.3 MB (66.17%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& nums, int k) {
+        int n = nums.size(), carry = 0;
+        for (auto i = n - 1; i >= 0; i--) {
+            int num = nums[i] + (k % 10) + carry;
+            k /= 10, carry = num / 10, nums[i] = num % 10;            
+        };
+        
+        if (k + carry) {
+            reverse(nums.begin(), nums.end());
+            while (k + carry) {
+                int num = (k % 10) + carry;
+                k /= 10, carry = num / 10;
+                nums.push_back(num % 10);
+            };
+            reverse(nums.begin(), nums.end());
+        };
+        
+        return nums;
+    };
+};

805c7147a3f483daff9ea8ff7072f3c31f9d41a3
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-02-21 18:50:14 +0530

    Time: 37 ms (22.29%), Space: 22.3 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,10 +5,10 @@ public:
         while (lo <= hi) {
             mid = lo + (hi - lo) / 2;
             if (mid + 1 < n && nums[mid] == nums[mid + 1]) {
-                if (((n - 1) - mid + 1) % 2) lo = mid + 1;
-                else hi = mid;
+                if (((n - 1) - mid + 1) % 2) lo = mid + 2;
+                else hi = mid - 1;
             } else if (mid - 1 >= 0 && nums[mid] == nums[mid - 1]) {
-                if ((mid - 0 + 1) % 2) hi = mid;
+                if ((mid - 0 + 1) % 2) hi = mid - 2;
                 else lo = mid + 1;
             } else return nums[mid];
         };

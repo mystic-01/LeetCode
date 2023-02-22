1740f2569ebb9e93beddd95367290d6130dd1d9e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-02-23 00:07:15 +0530

    Time: 48 ms (87.28%), Space: 26.2 MB (11.19%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-    int countDays(vector<int>& ws, int tot_cap, int cur_cap = 0, int res = 1) {
-  for (auto w : ws) {
-    cur_cap += w;
-    if (cur_cap > tot_cap) ++res, cur_cap = w;
-  }
-  return res;
-}
-int shipWithinDays(vector<int>& ws, int D) {
-  auto r = accumulate(begin(ws), end(ws), 0);
-  auto l = max(r / D, *max_element(begin(ws), end(ws)));
-  while (l < r) {
-    auto m = (l + r) / 2;
-    if (countDays(ws, m) <= D) r = m;
-    else l = m + 1;
-  }
-  return l;
-}
-//     int shipWithinDays(vector<int>& weights, int days) {
-//         int n = weights.size(), multiplier = ceil((double)n / days);
-//         cout << multiplier << "\n";
-//         int lo = (*min_element(weights.begin(), weights.end()));
-//         int hi = (*max_element(weights.begin(), weights.end())) * multiplier;
-//         int least = hi; 
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size(), multiplier = ceil((double)n / days);
+        int lo = (*max_element(weights.begin(), weights.end())),  hi = lo * multiplier;
+        int least = hi; 
         
-//         while (lo < hi) {
-//             int mid = lo + (hi - lo) / 2, capacity = mid, daysLeft = days;            
-//             for (auto i = 0; i < n; i++) {
-//                 if (capacity - weights[i] < 0) daysLeft--, capacity = mid;
-//                 capacity -= weights[i];
-//             };
-//             if (daysLeft < 0) lo = mid + 1; 
-//             else hi = mid, least = min(least, mid);
-//         };
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2, capacity = mid, daysTaken = 1;            
+            for (auto i = 0; i < n; i++) {
+                if (capacity - weights[i] < 0) daysTaken++, capacity = mid;
+                capacity -= weights[i];
+            };
+            if (daysTaken > days) lo = mid + 1; 
+            else hi = mid, least = min(least, mid);
+        };
         
-//         return least;
-//     };
+        return least;
+    };
 };

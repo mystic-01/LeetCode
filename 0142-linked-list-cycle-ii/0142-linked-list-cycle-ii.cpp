5b7bd10e2ae3413c386468f555a052e743010699
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-02-26 23:58:14 +0530

    Time: 8 ms (77.47%), Space: 7.7 MB (29.37%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,21 +9,38 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        bool isCycle = false;
-        ListNode *slow = head, *fast = head;
+        ListNode* slow = head, * fast = head;  
         while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-            if (slow == fast)  {
-                isCycle = true;
-                break;
-            };
+            slow = slow->next, fast = fast->next->next;
+            if (slow == fast) break;
         };
-        if (!isCycle) return nullptr;
-
-        slow = head;
-        while (slow != fast) slow = slow->next, fast = fast->next;
         
+        if (!(fast && fast->next)) return nullptr;
+        fast = head;
+        
+        while (slow != fast) slow = slow->next, fast = fast->next;                
         return slow;
     };
 };
+
+/* Explanation :
+
+L1 is the distance outside the cycle
+L2 is the distance from *Cycle Start* to *Meeting Point*
+C is the length of cycle
+
+d(slow) = L1 + L2
+d(fast) = L1 + L2 + n(C) 
+
+speed(fast) = 2 * speed(slow)
+L1 + L2 + nC = 2 * (L1 + L2)
+L1 + L2 = nC
+L1 = nC - L2
+
+    
+
+*/
+
+
+
+

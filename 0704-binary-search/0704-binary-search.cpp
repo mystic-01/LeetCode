78b5ca48143b97e3522157d62cbd42e580f32d25
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-02-27 01:10:03 +0530

    Time: 38 ms (75.93%), Space: 27.6 MB (18.55%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,11 +4,10 @@ public:
         int lo = 0, hi = nums.size() - 1;
         
         while (lo <= hi) {
-            if (lo == hi) return nums[lo] == target ? lo : -1;
             int mid = lo + (hi - lo) / 2;            
             if (target == nums[mid]) return mid;
             else if (target > nums[mid]) lo = mid + 1;
-            else hi = mid;
+            else hi = mid - 1;
         };
         
         return -1;

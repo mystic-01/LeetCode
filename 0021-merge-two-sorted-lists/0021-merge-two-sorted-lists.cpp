c4d9ff3b4db2a5515e8c908a38283abda8c925fe
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-02-26 23:23:34 +0530

    Time: 4 ms (92.48%), Space: 14.7 MB (85.78%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,42 +1,50 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+//         ListNode * result = new ListNode(69), * temp = result;
+        
+//         while (l1 && l2) {
+//             if (l1->val < l2->val) {
+//                 temp = temp->next = l1;
+//                 l1 = l1->next;
+//             } else {
+//                 temp = temp->next = l2;
+//                 l2 = l2->next;                
+//             };            
+//         };
+//         if (l1) temp->next = l1;
+//         if (l2) temp->next = l2;
+//         return result->next;  
+//     };
+// };
+
 class Solution {
 public:
     struct comp {
         bool operator()(ListNode* a, ListNode* b) {
-            return a->val > b->val;
-        };    
+            return a->val > b->val;            
+        };        
     };
     
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        priority_queue<ListNode*, vector<ListNode*>, comp> pq;        
-        while (list1) {
-            pq.push(list1);
-            list1 = list1->next;
-        };
-        while (list2) {
-            pq.push(list2);
-            list2 = list2->next;
-        };
-        if (!pq.empty()) {
-            list1 = pq.top(), list2 = list1;
-            pq.pop();
-        };
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        priority_queue<ListNode*, vector<ListNode*>, comp> pq;
+        if (l1) pq.push(l1);
+        if (l2) pq.push(l2);
+        
+        ListNode* dummy = new ListNode(7), * temp = dummy;
         while (!pq.empty()) {
-            list1 = list1->next = pq.top();
+            temp->next = pq.top();
             pq.pop();
+            temp = temp->next;
+            if (temp->next) pq.push(temp->next);
         };
-        if (list1) list1->next = nullptr;
-        return list2;
+        return dummy->next;
     };
 };
+
+
+
+
+
 
 

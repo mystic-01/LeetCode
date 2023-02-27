bed6acfb45855109a9f337502522cb48a813deee
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-02-27 16:37:49 +0530

    Time: 10 ms (95.65%), Space: 16.2 MB (51.81%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.cpp
+++ 0427-construct-quad-tree/0427-construct-quad-tree.cpp
@@ -1,30 +1,103 @@
-class Solution {
+/*
+// Definition for a QuadTree node.
+class Node {
 public:
-    Node* solve(vector<vector<int>>& grid, int x1, int y1, int length) {
-        // Return a leaf node if the matrix size is one.
-        if (length == 1) {
-            return new Node(grid[x1][y1], true);
-        }
-        
-        // Recursive calls to the four sub-matrices.
-        Node* topLeft = solve(grid, x1, y1, length / 2);
-        Node* topRight = solve(grid, x1, y1 + length / 2, length / 2);
-        Node* bottomLeft = solve(grid, x1 + length / 2, y1, length / 2);
-        Node* bottomRight = solve(grid, x1 + length / 2, y1 + length / 2, length / 2);
-            
-        // If the four returned nodes are leaf and have the same values
-        // Return a leaf node with the same value.
-        if (topLeft -> isLeaf && topRight -> isLeaf && bottomLeft -> isLeaf && bottomRight -> isLeaf
-           && topLeft -> val == topRight -> val && topRight -> val == bottomLeft -> val 
-           && bottomLeft -> val == bottomRight -> val) {
-            return new Node(topLeft -> val, true);
-        }
-        
-        // If the four nodes aren't identical, return non-leaf node with corresponding child pointers.
-        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+    
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
     }
     
-    Node* construct(vector<vector<int>>& grid) {
-        return solve(grid, 0, 0, grid.size());
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
     }
 };
+*/
+
+class Solution {
+public:
+    
+    Node* constructNode(int rStart, int rEnd, int cStart, int cEnd, vector<vector<int>>& grid) {
+        int val = -1;
+        for (auto i = rStart; i < rEnd; i++) {
+            for (auto j = cStart; j < cEnd; j++) {
+                if (val == -1) val = grid[i][j];
+                else if (val != grid[i][j]) {
+                    int rMid = (rEnd + rStart) / 2, cMid = (cEnd + cStart) / 2;
+
+                    Node *topLeft = constructNode(rStart, rMid, cStart, cMid, grid);
+                    Node *topRight = constructNode(rStart, rMid, cMid, cEnd, grid);
+                    Node *bottomLeft = constructNode(rMid, rEnd, cStart, cMid, grid);
+                    Node *bottomRight = constructNode(rMid, rEnd, cMid, cEnd, grid);
+
+                    return new Node(val, false, topLeft, topRight, bottomLeft, bottomRight);
+                };
+            };
+        };
+        if (val != -1) return new Node(val, true);
+        return new Node();
+    };
+    
+    Node* construct(vector<vector<int>>& grid) {
+        int start = 0, end = grid.size();
+        cout << "<<<<<<<<<<<<<<<<<<<<<\n";
+        return constructNode(start, end, start, end, grid);
+    };
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

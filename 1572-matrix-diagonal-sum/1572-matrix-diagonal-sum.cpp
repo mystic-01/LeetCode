2829797e7bd0b3509182a611940e0895b66ffff0
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-03-03 03:02:16 +0530

    Time: 14 ms (75.23%), Space: 11.3 MB (69.55%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
-        std::ios::sync_with_stdio(false);
-        std::cin.tie(nullptr);
-        
-        int sum = 0, n = mat.size();
-        for (int i = 0; i < n; i++)
-            sum += mat[i][i] + mat[i][n - i - 1];
-        return n % 2 ? sum - mat[n/2][n/2]: sum;
+        int n = mat.size(), sum = 0;
+        for (auto i = 0; i < n; i++) sum = sum + mat[i][i] + mat[i][n - i - 1];
+        return sum - (n % 2 ? mat[n / 2][n / 2] : 0);
     };
 };

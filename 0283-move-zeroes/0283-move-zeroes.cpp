0b98841b9513d6bf5e60c885771deb955608ebb5
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-03-03 04:18:54 +0530

    Time: 24 ms (70.11%), Space: 19.3 MB (17.28%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+//     void moveZeroes(vector<int>& nums) {
+//         std::ios::sync_with_stdio(false);
+//         std::cin.tie(nullptr);
+//         int zero = 0, one = 0, n = nums.size() - 1;
+        
+//         while (one <= n && zero <= n && n > 0) {
+//             swap(nums[one], nums[zero]);
+//             while (zero <= n && nums[zero] != 0) zero++;
+//             one = zero + 1;
+//             while (one <= n && nums[one] == 0) one++;
+//         };
+//     };
+
+    
+    // Don't understand why this is 5x slower. 
+    // void moveZeroes(vector<int>& nums) {
+    //     int n = nums.size(), zero = 0, nonZero = 0;
+    //     while (nonZero < n) {
+    //         while (zero < n && nums[zero] != 0) zero++;  
+    //         nonZero = zero + 1;
+    //         while (nonZero < n && nums[nonZero] == 0) nonZero++;     
+    //         if (nonZero < n) swap(nums[zero], nums[nonZero]);    
+    //     };
+    // };
+    
+     void moveZeroes(vector<int>& nums) {
+        int n = nums.size(), zero = 0, nonZero = 0;
+        while (nonZero < n) {
+            while (zero < n && nums[zero] != 0) zero++;  
+            while (nonZero < n && (nonZero <= zero || nums[nonZero] == 0)) nonZero++;     
+            if (nonZero < n) swap(nums[zero], nums[nonZero]);    
+        };
+    };
+    
+};

5c6d706fd8de26203af0dc9a45d1d065f2bf2ac3
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-03-23 18:10:06 +0530

    Time: 2 ms (39.06%), Space: 6 MB (63.97%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.cpp
+++ 2235-add-two-integers/2235-add-two-integers.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int sum(int a, int b) {
-        return a + b;
+        while (b != 0) {
+            unsigned carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        };
+        return a;
     };
 };

0807b94bdcf621aad6f0081c05cbfb28ea7af598
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-04-01 12:29:50 +0530

    Time: 49 ms (9.98%), Space: 27.4 MB (93.53%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int lo = 0, hi = nums.size() - 1;
-        
+        int lo = 0, mid = 0, hi = nums.size() - 1;
         while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;            
-            if (target == nums[mid]) return mid;
-            else if (target > nums[mid]) lo = mid + 1;
-            else hi = mid - 1;
+            mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) hi = mid - 1;
+            else if (nums[mid] < target) lo = mid + 1;
         };
-        
         return -1;
     };
 };

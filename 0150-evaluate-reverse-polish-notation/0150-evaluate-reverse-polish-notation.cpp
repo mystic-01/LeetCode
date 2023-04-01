45f52d0288b6c64575502ecd47906acb0b553ffd
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-04-01 16:56:22 +0530

    Time: 17 ms (19.08%), Space: 13.7 MB (7.21%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void evalStack(stack<string> &st) {
+        while (true) {
+            int num1 = stoi(st.top());
+            st.pop();
+            int num2 = stoi(st.top());
+            st.pop();
+            char character = st.top()[0];
+            st.pop();
+                
+            if (character == '+') num1 += num2;
+            else if (character == '-') num1 -= num2;
+            else if (character == '*') num1 *= num2;
+            else if (character == '/') num1 /= num2;
+            if (st.size()) character = st.top()[st.top().size() - 1];
+
+            st.push(to_string(num1));
+            if (st.size() <= 2 || !isdigit(character)) break;
+        };
+    };
+    
+    int evalRPN(vector<string>& tokens) {
+        stack<string> st;
+        int n = tokens.size();
+        
+        for (auto i = n - 1; i >= 0; i--) {
+            if (st.size() && 
+                isdigit(tokens[i][tokens[i].size() - 1]) && isdigit(st.top()[st.top().size() - 1])) {
+                st.push(tokens[i]);                
+                evalStack(st);
+            } else st.push(tokens[i]);
+        };
+        
+        return stoi(st.top());
+    };
+};

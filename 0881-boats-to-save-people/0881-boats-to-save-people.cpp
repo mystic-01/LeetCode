e753de9a2b95d62f79043f3d4167bd0182c54d16
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-04-03 19:19:56 +0530

    Time: 82 ms (83.76%), Space: 42 MB (59.84%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
+    // use two pointers
     int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(),people.end());        
-        int i = 0, j = people.size() - 1, cnt = 0;        
-        while(i <= j) {   
-            if(people[i] + people[j] <= limit) {
-                ++i;
-                --j;
-            } else --j;            
-            ++cnt;
-        }        
-        return cnt;
-    }
+        sort(people.begin(), people.end());      
+        int n = people.size(), i = 0, j = n - 1, currWeight = 0, boats = 0;
+        while (i <= j) {
+            int currWeight = people[j];
+            if (i < j && currWeight + people[i] <= limit) currWeight += people[i++];
+            boats++, j--;
+        };
+        return boats;
+    };
 };

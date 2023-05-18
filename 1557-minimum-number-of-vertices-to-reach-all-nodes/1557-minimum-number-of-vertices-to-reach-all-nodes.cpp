6fec42cba1dcddd175e75846636d30a339df9887
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-05-18 07:19:02 +0530

    Time: 365 ms (30.17%), Space: 111.2 MB (19.44%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void topoSort(int &node, vector<vector<int>> &adjList, vector<int> &visited, stack<int> &st, bool &isTopoSort) {
+    void topoSort(int &node, vector<vector<int>> &adjList, int *visited, stack<int> &st, bool &isTopoSort) {
         for (auto &child : adjList[node]) {
             if (!visited[child]) {
                 visited[child] = 1;
@@ -12,7 +12,12 @@ public:
     
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adjList(n);
-        vector<int> visited(n, 0), ans;
+        vector<int> ans;
+        int visited[n];
+        
+        for (auto i = 0; i < n; i++) {
+            visited[i] = 0;
+        };
         
         for (auto &edge : edges) {
             adjList[edge[0]].push_back(edge[1]);                 
@@ -29,20 +34,21 @@ public:
         
         // Getting every node that visits a new node
         
-        visited = vector<int>(n, 0);
+        for (auto i = 0; i < n; i++) {
+            visited[i] = 0;
+        };
+        
         isTopoSort = !isTopoSort;
         
         while (!st.empty()) {
             int node = st.top();
             st.pop();
 
-            // cout << node << "\n";
             if (!visited[node]) {
                 ans.push_back(node);
                 topoSort(node, adjList, visited, st, isTopoSort);     
             };
         };
-        // cout << "\n";
         return ans;  
     };
 };

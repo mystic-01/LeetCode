61370f6f599a8e39f0a5685cd55e5b65feb31e01
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-10 01:22:38 +0530

    Time: 12 ms (45.33%), Space: 11.8 MB (87.96%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
@@ -1,15 +1,8 @@
 class Solution {
-private:
-    bool isSubstr(string &str, string &sub) {
-        int i = 0;        
-        while (i < str.size() && i < sub.size() && str[i] == sub[i]) i++;
-        return i == sub.size();
-    };
-    
 public:
     int countPrefixes(vector<string>& words, string s) {
         int count = 0;
-        for (auto &word : words) count += (isSubstr(s, word));
+        for (auto &word : words) count += (s.substr(0, word.size()) == word);
         return count;
     };
 };

0f02e9f2827bbe78b59a29977814d8cb14c3701f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-10 11:33:15 +0530

    Time: 219 ms (64.19%), Space: 138.9 MB (50.91%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,7 +1,50 @@
-class Solution {
-// Use Topo Sort!
-            
+class Solution {  
+private:
+    void dfs(int idx, vector<vector<int>> &adjList, vector<int> &visited, vector<int> &curr) {
+        visited[idx] = 1;
+        
+        for (auto &child : adjList[idx]) {
+            if (!visited[child]) {
+                curr.push_back(child);
+                dfs(child, adjList, visited, curr);            
+            };            
+        };
+    };
     
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> ans(n), adjList(n);
+        
+        for (auto &edge : edges) {
+            adjList[edge[1]].push_back(edge[0]);
+        };
+        
+        for (auto i = 0; i < n; i++) {
+            vector<int> visited(n, 0);
+            vector<int> curr;
+            
+            dfs(i, adjList, visited, curr);
+
+            sort(curr.begin(), curr.end());
+            ans[i] = curr;
+        };
+
+        return ans;
+    };
+};
+
+
+
+
+
+
+
+
+
+/*
+Use Indegree ^ BFS
+
+class Solution {  
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
         vector<vector<int>> ans(n), adjList(n);
@@ -40,7 +83,7 @@ public:
         return ans;
     };
 };
-
+*/
 
 
 

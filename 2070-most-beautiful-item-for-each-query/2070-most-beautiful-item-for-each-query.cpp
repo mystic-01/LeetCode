8ec9924c49314ee58202500141292a38d71cb7ad
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-14 21:16:52 +0530

    Time: 478 ms (33.99%), Space: 88.4 MB (92.70%) - LeetHub

---
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    static bool comp(vector<int> &a, vector<int> &b) {
+        return a[0] == b[0] ? a[1] > b[1] : a[0] < b[0];            
+    };    
+    
+    int lowerBound(int target, vector<vector<int>>& items) {
+        int lo = 0, hi = items.size() - 1;
+        
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (target == items[mid][0]) return mid;
+            else if (target > items[mid][0]) lo = mid + 1;
+            else hi = mid - 1;
+        };
+        
+        return items[lo][0] <= target ? lo : lo - 1;
+    };            
+    
+public:
+    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
+        vector<int> ans;
+        sort(items.begin(), items.end(), comp);
+        
+        int last = -1;
+        
+        for (auto &vec : items) {
+            vec[1] = max(vec[1], last), last = max(last, vec[1]);
+        };
+
+        for (auto &q : queries) {
+            int idx = lowerBound(q, items);
+            ans.push_back(idx < 0 ? 0 : items[idx][1]);            
+        };
+        
+        return ans;
+    };
+};

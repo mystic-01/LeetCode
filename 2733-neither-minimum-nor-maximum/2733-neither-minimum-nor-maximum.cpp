386fd0288ea5d91e39b66f3762fb3eacc3778fcc
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-18 00:50:21 +0530

    Time: 124 ms (91.31%), Space: 92.2 MB (7.22%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.cpp
@@ -3,17 +3,13 @@ public:
     int findNonMinOrMax(vector<int>& nums) {
         int n = nums.size(), minE = nums[0], maxE = nums[0];
 
-        for (auto i = 0; i < n; i++) {
-            if (nums[i] > minE && nums[i] < maxE) return nums[i];
-            else if (nums[i] > maxE) {
-                if (maxE > minE) return maxE;
-                maxE = nums[i];
-            } else if (nums[i] < minE) {
-                if (minE < maxE) return minE;
-                minE = nums[i];
-            };
-        };
+        set<int> s;
         
-        return -1;
+        for (auto i = 0; i < n; i++) {
+            if (s.size() < 3) s.insert(nums[i]);
+            else break;
+        };
+        auto it = s.begin();
+        return s.size() == 3 ? *(++it) : -1;
     };
 };

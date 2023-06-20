cf0123a438b88ac70c1264d252c1d5237e8d0b67
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-20 16:31:12 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (26.97%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -1,3 +1,42 @@
+// Use the grid as the visited set!!! 
+
+class Solution {
+private:
+    int offsets[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};    
+public:
+    int recurse(int i, int j, int &row, int &col, int &validCells, vector<vector<int>>& grid) {
+        if (grid[i][j] == 2) return validCells == 0;
+        int ans = 0;
+        
+        for (auto k = 0; k < 4; k++) {
+            int x = i + offsets[k][0], y = j + offsets[k][1];        
+            if (x >= row || x < 0 || y >= col || y < 0 || grid[x][y] == -1) continue;        
+            if (grid[x][y] == 0) grid[x][y] = -1;
+            validCells--;
+            ans += recurse(x, y, row, col, validCells, grid);
+            validCells++;
+            if (grid[x][y] != 2) grid[x][y] = 0;
+        };
+        
+        return ans;
+    };
+    
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        int row = grid.size(), col = grid[0].size(), validCells = 0, startX, startY;
+        
+        for (auto i = 0; i < row; i++) {
+            for (auto j = 0; j < col; j++) {
+                if (grid[i][j] == 1) startX = i, startY = j; 
+                else if (grid[i][j] != -1) validCells++;
+            };        
+        };
+        
+        grid[startX][startY] = -1;
+        return recurse(startX, startY, row, col, validCells, grid);
+    };
+};
+
+/*
 class Solution {
 private:
     int offsets[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};    
@@ -36,3 +75,4 @@ public:
         return recurse(startX, startY, row, col, validCells, grid, visited);
     };
 };
+*/

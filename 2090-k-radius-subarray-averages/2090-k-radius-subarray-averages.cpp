ed595a8d09750d9a7bdf1db374882b1c6d6bdc90
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-20 14:09:26 +0530

    Time: 259 ms (56.74%), Space: 129.8 MB (94.45%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        int size = nums.size(), front = 0, rear = 0, deno = 2 * k + 1;
-        long long runningSum = 0;
+        int size = nums.size(), front = 0, rear = k, deno = 2 * k + 1;
+        long runningSum = 0;
+
         vector<int> ans(size, -1);
         if (size < k) return ans;
 
         for (auto i = 0; i < size; i++) {
-            if (i - k == 0 && size - i > k) {
-                runningSum += nums[i];                
-                rear = i + 1;
-                int addCount = k;
-                while (addCount--) runningSum += nums[rear++];
-                ans[i] = runningSum / deno;
-            } else if (i - k > 0 && size - i > k) {
-                runningSum += nums[rear++];
-                runningSum -= nums[front++];
+            if (i - k >= 0) {
+                if (size - i <= k) break;
+                if (i - k == 0) {
+                    // Add numbers from first valid number to next 'k' numbers. 
+                    for (auto j = 0; j <= k; j++) runningSum += nums[rear++];
+                } else {
+                    // Pop from front, Add from rear for rest all until (size - i < k).
+                    runningSum += nums[rear++];
+                    runningSum -= nums[front++];
+                };                
                 ans[i] = runningSum / deno;
             } else runningSum += nums[i];
         };
-
         return ans;
     };
 };

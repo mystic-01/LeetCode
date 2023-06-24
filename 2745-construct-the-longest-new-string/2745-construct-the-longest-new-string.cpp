e178563035c06ac2660b06d2c66b1a3bb10ace70
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-24 21:55:33 +0530

    Time: 237 ms (16.67%), Space: 8.2 MB (16.67%) - LeetHub

--- 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.cpp
+++ 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.cpp
@@ -1,50 +1,21 @@
 class Solution {
 public:
-    int recurse (int xLeft, int yLeft, int zLeft, int prev, int (*arr)[51][51][4]) {
-        int takeX = 0, takeY = 0, takeZ = 0;
-        if (arr[xLeft][yLeft][zLeft][prev] != -1) return arr[xLeft][yLeft][zLeft][prev];
-        
-        if (prev == 0) {
-            takeX = (xLeft == 0 ? 0 : 1 + recurse(xLeft - 1, yLeft, zLeft, 1, arr));
-            takeY = (yLeft == 0 ? 0 : 1 + recurse(xLeft, yLeft - 1, zLeft, 2, arr));
-            takeZ = (zLeft == 0 ? 0 : 1 + recurse(xLeft, yLeft, zLeft - 1, 3, arr));
-        } else if (prev == 1) {
-            takeY = (yLeft == 0 ? 0 : 1 + recurse(xLeft, yLeft - 1, zLeft, 2, arr));
-        } else {
-            takeX = (xLeft == 0 ? 0 : 1 + recurse(xLeft - 1, yLeft, zLeft, 1, arr));
-            takeZ = (zLeft == 0 ? 0 : 1 + recurse(xLeft, yLeft, zLeft - 1, 3, arr));            
-        };
-        
-        return arr[xLeft][yLeft][zLeft][prev] = max(takeY, max(takeX, takeZ));            
-    };
-    
+    int dp[51][51][51][4] = {};
+    int solve(int x, int y, int z, int last){
+        if(dp[x][y][z][last] != 0) return dp[x][y][z][last];
+        int res = 0;
+        if(x > 0 && last != 1){
+            res = max(res, 2 + solve(x-1, y, z, 1));
+        }
+        if(y > 0 && last <= 1){
+            res = max(res, 2 + solve(x, y-1, z, 2));
+        }
+        if(z > 0 && last != 1){
+            res = max(res, 2 + solve(x, y, z-1, 3));
+        }
+        return dp[x][y][z][last] = res;
+    }
     int longestString(int x, int y, int z) {
-        int arr[51][51][51][4];
-        for (auto i = 0; i < 51; i++) {
-            for (auto j = 0; j < 51; j++) {
-                for (auto k = 0; k < 51; k++) {
-                    for (auto l = 0; l < 4; l++) {
-                        arr[i][j][k][l] = -1;
-                    };
-                };
-            };
-        };
-        
-        return recurse(x, y, z, 0, arr) * 2;
-    };
+        return solve(x, y, z, 0);
+    }
 };
-
-
-/*
-
-x = "AA", 1
-y = "BB", 1
-z = "AB", 1
-
-x -> y
-y -> x | z
-z -> x | z
-
-1 + 
-
-*/

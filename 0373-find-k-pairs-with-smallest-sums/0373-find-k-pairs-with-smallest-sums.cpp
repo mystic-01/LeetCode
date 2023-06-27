38037060398d93a3c053986a7425d25025df1c86
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-27 23:21:36 +0530

    Time: 909 ms (5.81%), Space: 128.2 MB (11.13%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
- vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-      priority_queue<pair<int,pair<int,int>>> pq;
-        for(int i=0;i<nums1.size();i++)
-        {
-            for(int j=0;j<nums2.size();j++)
-            {
-                int sum=nums1[i]+nums2[j];
-                if(pq.size()<k)pq.push({sum,{nums1[i],nums2[j]}});
-                else if(sum<pq.top().first)
-                {
-                    pq.pop();
-                    pq.push({sum,{nums1[i],nums2[j]}});
-                } else break;
-            }
-        }
+    struct comp {
+        bool operator()(const vector<int> &v1, const vector<int> &v2) {
+            return v1[0] + v1[1] < v2[0] + v2[1];                     
+        };        
+    };
+    
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         vector<vector<int>> ans;
-        while(!pq.empty())
-        {
-            ans.push_back({pq.top().second.first,pq.top().second.second});
+        
+        priority_queue<vector<int>, vector<vector<int>>, comp> pq;
+        
+        for (auto &x : nums1) {
+            for (auto &y : nums2) {
+                if (pq.size() < k) pq.push({x, y});
+                else if (x + y < pq.top()[0] + pq.top()[1]) {
+                    pq.push({x, y});
+                    pq.pop();
+                } else break;
+            };        
+        };
+        
+        while (!pq.empty()) {
+            ans.push_back(pq.top());
             pq.pop();
-        }
-        reverse(ans.begin(),ans.end());
+        };
+        
         return ans;
-    }
+    };
 };

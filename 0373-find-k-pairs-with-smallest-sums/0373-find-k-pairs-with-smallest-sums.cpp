0e37abf59cf47a87cbc19cf7e366deb70a6f523f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-27 23:36:33 +0530

    Time: 352 ms (52.56%), Space: 108.9 MB (87.95%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
+    struct arrayStruct {
+        int arr[2] = {0, 0};
+        arrayStruct(int &x, int &y) {
+            arr[0] = x, arr[1] = y;
+        };
+    };
+    
     struct comp {
-        bool operator()(const pair<int, int> &v1, const pair<int, int> &v2) {
-            return v1.first + v1.second < v2.first + v2.second;                     
+        bool operator()(const arrayStruct &v1, const arrayStruct &v2) {
+            return v1.arr[0] + v1.arr[1] < v2.arr[0] + v2.arr[1];                     
         };        
     };
     
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         vector<vector<int>> ans;
         
-        priority_queue<pair<int, int>, vector<pair<int, int>>, comp> pq;
+        priority_queue<arrayStruct, vector<arrayStruct>, comp> pq;
         
         for (int &x : nums1) {
             for (int &y : nums2) {
-                if (pq.size() < k) pq.push({x, y});
-                else if (x + y < pq.top().first + pq.top().second) {
+                if (pq.size() < k) pq.push(arrayStruct(x, y));
+                else if (x + y < pq.top().arr[0] + pq.top().arr[1]) {
                     pq.pop();
                     pq.push({x, y});
                 } else break;
@@ -22,7 +29,7 @@ public:
         };
         
         while (!pq.empty()) {
-            ans.push_back({pq.top().first, pq.top().second});
+            ans.push_back({pq.top().arr[0], pq.top().arr[1]});
             pq.pop();
         };
         

b52a984afbdff6e5ae83076e7846bb9af3e7f5e1
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-28 15:35:29 +0530

    Time: 215 ms (62.04%), Space: 65.6 MB (69.53%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        vector<vector<pair<double, int>>> adjList(n);
+        
+        for (auto i = 0; i < edges.size(); i++) {
+            adjList[edges[i][0]].push_back({succProb[i], edges[i][1]});    
+            adjList[edges[i][1]].push_back({succProb[i], edges[i][0]});    
+        };
+        
+        double dist[n];
+        for (auto i = 0; i < n; i++) dist[i] = 0;
+        dist[start] = 1.0;
+
+        auto comp = [](pair<double, int> &p1, pair<double, int> &p2) {
+            return p1.first < p2.first;      
+        };
+        
+        priority_queue<pair<double, int>, vector<pair<double, int>>, decltype(comp)> pq(comp);
+        
+        pq.push({dist[start], start});
+        
+        while (!pq.empty()) {
+            double wt = pq.top().first;
+            int idx = pq.top().second;
+            pq.pop();
+            
+            for (auto &[first, second] : adjList[idx]) {
+                if (first * wt > dist[second]) {
+                    dist[second] = first * wt;
+                    pq.push({dist[second], second});                    
+                };                
+            };
+        };
+        
+        return dist[end];
+    };
+};

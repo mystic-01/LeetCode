40e8597b680766e07531f99100b09f070047658a
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-28 15:59:52 +0530

    Time: 216 ms (60.00%), Space: 65.9 MB (67.71%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -36,9 +36,6 @@ public:
                 };
             };
         };
-        
-        for (auto i = 0; i < n; i++) cout << dist[i] << " ";        
-        
         return dist[end];
     };
 };

eb4bdf2207fd6465aab9e358320a38de5cbed23e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-30 13:12:21 +0530

    Time: 383 ms (59.69%), Space: 62.6 MB (90.10%) - LeetHub

--- 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.cpp
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.cpp
@@ -17,23 +17,23 @@ public:
     
     int shortestPath(int node1, int node2) {
         int dist[n];
-        bool visited[n];   
-        for (auto i = 0; i < n; i++) visited[i] = 0, dist[i] = INT_MAX;
+        // bool visited[n];   
+        for (auto i = 0; i < n; i++) dist[i] = INT_MAX;
         dist[node1] = 0;
         pq.push({dist[node1], node1});
         
         while (!pq.empty()) {
             int wt = pq.top().first, idx = pq.top().second;
             pq.pop();
-            if (!visited[idx]) {
-                visited[idx] = 1;
+            // if (!visited[idx]) {
+                // visited[idx] = 1;
                 for (auto &[first, second] : adjList[idx]) {
                     if (wt + second < dist[first]) {
                         dist[first] = wt + second;
                         pq.push({dist[first], first});
                     };                                        
                 };
-            };
+            // };
         };
         
         return dist[node2] == INT_MAX ? -1 : dist[node2];                        

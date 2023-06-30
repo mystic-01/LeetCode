c108de9a935312bb3f1f1d77c1d87f627552af10
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-06-30 15:27:47 +0530

    Time: 16 ms (95.15%), Space: 14.4 MB (97.70%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -0,0 +1,66 @@
+struct node {
+    int idx, distance, color;
+    node(int _idx, int _distance, int _color) {
+        idx =_idx, distance = _distance, color = _color;
+    };
+};
+
+class Solution {
+public:
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+        vector<vector<pair<int, int>>> adjList(n);
+        int dist[n][2];
+        vector<int> ans(n);
+         
+        for (int i = 0; i < n; i++) {
+            dist[i][0] = INT_MAX, dist[i][1] = INT_MAX;
+        };
+        
+        for (auto &blueEdge : blueEdges) adjList[blueEdge[0]].push_back({blueEdge[1], 0});
+        for (auto &redEdge : redEdges) adjList[redEdge[0]].push_back({redEdge[1], 1});
+        
+        auto comp = [](const node &n1, const node &n2) {
+            return n1.distance > n2.distance;             
+        };
+        
+        priority_queue<node, vector<node>, decltype(comp)> pq(comp);
+        
+        dist[0][0] = 0, dist[0][1] = 0;
+        pq.emplace(node(0, 0, -1));
+        
+        while (!pq.empty()) {
+            int idx = pq.top().idx, distance = pq.top().distance, color = pq.top().color;
+            pq.pop();
+            
+            for (auto &[first, second] : adjList[idx]) {
+                if (distance + 1 < dist[first][second] && (color == -1 || second == !color)) {
+                    dist[first][second] = distance + 1;
+                    pq.emplace(node(first, distance + 1, second));
+                };
+            };
+        };
+        
+        for (int i = 0; i < n; i++) {
+            ans[i] = min(dist[i][0], dist[i][1]);
+            if (ans[i] == INT_MAX) ans[i] = -1;
+        };
+        return ans;
+    };
+};
+
+/*
+
+3
+[[0,1],[1,2]]
+[]
+3
+[[0,1]]
+[[2,1]]
+3
+[[0,1],[0,2]]
+[[1,0]]
+5
+[[0,1],[1,2],[2,3],[3,4]]
+[[1,2],[2,3],[3,1]]
+
+*/

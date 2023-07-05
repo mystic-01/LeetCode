1783540b3d33557b6208d0eb4ece42155006ace3
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-05 17:31:38 +0530

    Time: 141 ms (7.61%), Space: 5.8 MB (92.76%) - LeetHub

---
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void recurse(int idx, string &pattern, string &ans, string &ds, bool *vis) {
+        if (idx == pattern.size()) {
+            ans = ans.size() ? min(ans, ds) : ds;
+            return;
+        };
+        int lastNum = ds.back() - '0';
+        
+        for (int i = 1; i <= 9; i++, lastNum += (pattern[idx] == 'I' ? 1 : -1)) {
+            if (lastNum >= 1 && lastNum <= 9 && !vis[lastNum]) {
+                vis[lastNum] = 1;            
+                ds += to_string(lastNum);
+                recurse(idx + 1, pattern, ans, ds, vis);
+                ds.pop_back();
+                vis[lastNum] = 0;            
+            };
+        };
+    };
+    
+    string smallestNumber(string pattern) {
+        string ans = "", ds = "";
+        bool vis[10] = {0};
+        for (int i = 1; i <= 9; i++) {
+            vis[i] = 1;
+            ds += to_string(i);
+            recurse(0, pattern, ans, ds, vis);
+            ds.pop_back();
+            vis[i] = 0;
+        };
+        return ans;
+    };
+};

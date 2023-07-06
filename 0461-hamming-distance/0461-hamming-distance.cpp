fdb0e231b4f3d9c1ae5ce0a20ff2a8f3cab00194
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-06 22:26:42 +0530

    Time: 4 ms (18.63%), Space: 5.9 MB (47.00%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -2,10 +2,7 @@ class Solution {
 public:
     int hammingDistance(int x, int y) {
         int ans = 0;
-        while (x || y) {
-            if ((x & 1) != (y & 1)) ans++;
-            x /= 2, y /= 2;
-        };
+        while (x || y) ans += ((x & 1) != (y & 1)), x /= 2, y /= 2;
         return ans;
     };
 };

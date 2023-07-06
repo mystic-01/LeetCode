a46d972dd2589b6e32c55ce424a60f4dfe335fa4
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-06 23:06:27 +0530

    Time: 55 ms (71.63%), Space: 18.9 MB (73.69%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.cpp
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -4,9 +4,7 @@ public:
         int n = nums.size(), ans = 0;
         for (auto i = 0; i < 32; i++) {
             int count = 0;
-            for (auto j = 0; j < n; j++) {
-                count += (nums[j] & 1), nums[j] /= 2;    
-            };    
+            for (auto j = 0; j < n; j++) count += (nums[j] & 1), nums[j] >>= 1;    
             ans += (count * (n - count));
         };
         return ans;

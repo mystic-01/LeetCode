86809a602c8efe5a7171ac683af34de575a1e089
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-06 13:03:15 +0530

    Time: 205 ms (58.39%), Space: 60.5 MB (97.54%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        int trustReceived[n + 1];
-        bool trustGiven[n + 1];
-        for (int i = 0; i < n + 1; i++) trustReceived[i] = 0, trustGiven[i] = 0;
-        for (int i = 0; i < trust.size(); i++) trustGiven[trust[i][0]] = 1, trustReceived[trust[i][1]]++;
-        for (int i = 1; i < n + 1; i++) if (trustGiven[i] == 0 && trustReceived[i] == n - 1) return i;
+        int totalTrust[n + 1];
+        for (int i = 0; i < n + 1; i++) totalTrust[i] = 0;
+        for (int i = 0; i < trust.size(); i++) totalTrust[trust[i][0]]--, totalTrust[trust[i][1]]++;
+        for (int i = 1; i < n + 1; i++) if (totalTrust[i] == n - 1) return i;
         return -1;
     };
 };

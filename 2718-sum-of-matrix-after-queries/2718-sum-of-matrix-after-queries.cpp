9717dc6aae6d4f54f9120662932f2de0636bb0f0
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-11 17:37:12 +0530

    Time: 601 ms (70.97%), Space: 190.2 MB (99.85%) - LeetHub

---
+++ 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    long long matrixSumQueries(int n, vector<vector<int>>& q) {
+        bool row[n], col[n];
+        int freeRowCount = n, freeColCount = n;
+        
+        for (auto i = 0; i < n; i++) row[i] = 0, col[i] = 0;
+        
+        long long ans = 0;
+        int qSize = q.size();
+        
+        for (auto i = qSize - 1; i >= 0; i--) {
+            bool isRow = q[i][0];
+            int idx = q[i][1], wt = q[i][2], count = n;
+            
+            if (isRow) {
+                if (row[idx] != 0) count = 0;
+                else count = freeColCount, freeRowCount--, row[idx] = wt;
+            } else {
+                if (col[idx] != 0) count = 0;
+                else count = freeRowCount, freeColCount--, col[idx] = wt;
+            };
+            ans += (count * wt);
+        }; 
+        return ans;
+    };
+};
+
+
+/*
+Input: n = 3, q = [[0,0,1],[1,2,2],[0,2,3],[1,0,4]]
+
+sum = 0
+
+row [n] = {0, 0, 0}
+col [n] = {0, 0, 0}
+
+q:
+1
+
+sum += q[2] * n;
+if (q[0] == 0) {
+    
+};
+
+
+*/

9d3e82f29f37586e6c47a72c8cd53d33b4cec279
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-11 14:40:53 +0530

    Time: 656 ms (34.27%), Space: 149.5 MB (71.89%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void getParent(TreeNode *root, map<TreeNode*, TreeNode*> &m) {
+        if (root->left) {
+            m[root->left] = root;
+            getParent(root->left, m);
+        };
+        if (root->right) {
+            m[root->right] = root;
+            getParent(root->right, m);
+        };        
+    };
+    
+    TreeNode *findNode(TreeNode *root, int &start) {
+        if (!root) return nullptr;
+        if (root->val == start) return root;
+        TreeNode *left = findNode(root->left, start);
+        if (left) return left;
+        TreeNode *right = findNode(root->right, start);
+        return right;    
+    };    
+    
+    int amountOfTime(TreeNode *root, int val) {
+        TreeNode *start = findNode(root, val);    
+        map<TreeNode*, TreeNode*> m;
+        getParent(root, m);
+        m[root] = nullptr;
+        
+        queue<TreeNode*> q;
+        set<TreeNode*> visited;
+        
+        q.push(start);
+        visited.insert(start);
+        
+        int time = -1;
+        while (!q.empty()) {
+            time++;
+            int size = q.size();
+            for (auto i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left && !visited.count(node->left)) { 
+                    q.push(node->left);
+                    visited.insert(node->left);
+                };     
+                if (node->right && !visited.count(node->right)) { 
+                    q.push(node->right);
+                    visited.insert(node->right);
+                }; 
+                if (m[node] && !visited.count(m[node])) { 
+                    q.push(m[node]);
+                    visited.insert(m[node]);
+                }; 
+            };
+        };
+        return time;
+    };
+};
+
+
+
+
+

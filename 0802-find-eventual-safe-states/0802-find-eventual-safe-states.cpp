ff3e95fdb1ca5e899888114259eb5a80dc6b0ca0
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-12 15:09:30 +0530

    Time: 173 ms (68.34%), Space: 46.8 MB (93.58%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkIfParentFound(int node, vector<vector<int>>& graph, int *visited) {
+        visited[node] = -1;
+        
+        for (int &child : graph[node]) {
+            if (visited[child] == -1) return false;
+            else if (visited[child] == 1) continue;
+            else {
+                if (!checkIfParentFound(child, graph, visited)) return false;
+            };
+        };
+        
+        visited[node] = 1;
+        return true;
+    };    
+    
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        vector<int> ans;
+        int visited[graph.size()];
+        for (int i = 0; i < graph.size(); i++) visited[i] = 0;
+         
+        for (int i = 0; i < graph.size(); i++) {
+            if (visited[i] == 0) checkIfParentFound(i, graph, visited);    
+            if (visited[i] == 1) ans.push_back(i);
+        };
+        return ans;      
+    };
+};

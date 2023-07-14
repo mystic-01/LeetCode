b3b3773bea4277b409edf12c7627a00b4f0587f9
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-14 22:57:43 +0530

    Time: 272 ms (44.76%), Space: 80 MB (28.95%) - LeetHub

--- 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
+++ 2453-destroy-sequential-targets/2453-destroy-sequential-targets.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int destroyTargets(vector<int>& nums, int space) {
-        int n = nums.size(), mod[n], maxCount = 0, maxCountSeed = INT_MAX;
+        int n = nums.size(), maxCount = 0, maxCountSeed = INT_MAX;
         unordered_map<int, pair<int, int>> m;
         for (int i = 0; i < n; i++) {
-            mod[i] = nums[i] % space;
-            m[mod[i]].second = (m.find(mod[i]) != m.end()) ? min(m[mod[i]].second, nums[i]) : nums[i];
-            m[mod[i]].first += 1;
+            int mod = nums[i] % space;
+            m[mod].second = (m.find(mod) != m.end()) ? min(m[mod].second, nums[i]) : nums[i];
+            m[mod].first += 1;
 
-            if ((m[mod[i]].first > maxCount) || (m[mod[i]].first == maxCount && m[mod[i]].second < maxCountSeed)) {
-                maxCount = m[mod[i]].first, maxCountSeed = m[mod[i]].second;
+            if ((m[mod].first > maxCount) || (m[mod].first == maxCount && m[mod].second < maxCountSeed)) {
+                maxCount = m[mod].first, maxCountSeed = m[mod].second;
             };
         };
         return maxCountSeed;        

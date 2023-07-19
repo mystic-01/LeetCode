f9114bacc939a54f2d3492cfe8bc010f46805a3b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-19 19:23:31 +0530

    Time: 496 ms (54.22%), Space: 89.8 MB (58.83%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
-        int n = intervals.size(), currEnd = intervals[0][1], sum = 0;
+        int n = intervals.size(), currEnd = intervals[0][1], merges = 0;
         
         for (auto i = 1; i < n; i++) {
             int start = intervals[i][0], end = intervals[i][1];
-            if (start < currEnd) sum++, currEnd = min(end, currEnd);
+            if (start < currEnd) merges++, currEnd = min(end, currEnd);
             else currEnd = end;
         };
         
-        return sum;
+        return merges;
     };
 };

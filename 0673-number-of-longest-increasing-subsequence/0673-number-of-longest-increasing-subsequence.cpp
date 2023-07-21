6a01c4e7fd603a3e3d6dedca521ba6eba2aac62c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-21 18:19:01 +0530

    Time: 159 ms (84.28%), Space: 13.2 MB (25.03%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -9,19 +9,17 @@ class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size(), count = n, maxLen = 1;
-        // lenAndCount dp[n];
         vector<lenAndCount> dp(n, lenAndCount(1, 1));
-        // for (int i = 0; i < n; i++) dp[i] = lenAndCount(1, 1);
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     int newLen = dp[j].len + 1; 
                     
-                    if (newLen > dp[i].len) dp[i].count = dp[j].count, dp[i].len = newLen;
+                    if (newLen > dp[i].len) dp[i].len = newLen, dp[i].count = dp[j].count;
                     else if (newLen == dp[i].len) dp[i].count += dp[j].count;                    
     
-                    if (newLen > maxLen) count = dp[j].count, maxLen = newLen;
+                    if (newLen > maxLen) maxLen = newLen, count = dp[j].count;
                     else if (newLen == maxLen) count += dp[j].count;
                 };
             };    

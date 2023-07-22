9980dd83010af2bf59a276d097c0a2bfa9e6851c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-23 01:52:40 +0530

    Time: 2517 ms (14.29%), Space: 6.7 MB (71.43%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,35 +1,42 @@
+int mod = 1e9 + 7, powers[301][6] = {0};
+
 class Solution {
-private:
-    int mod = 1e9 + 7;
 public:    
-    int recurse(int idx, int value, int &target, int *powers, int(*dp)[301]) {
-        if (value == target) return 1;
+    int recurse(int idx, int value, int &x, int &target, int (*powers)[6], int(*dp)[301]) {
+        if (value >= target) return value == target;
         int ans = 0;
     
         if (dp[idx][value] != -1) return dp[idx][value];
         
-        for (int i = idx + 1; i <= target && value + powers[i] <= target; i++) {
-            ans = (ans + recurse(i, value + powers[i], target, powers, dp)) % mod;
+        for (int i = idx + 1; i <= target && value + powers[i][x] <= target; i++) {
+            ans = (ans + recurse(i, value + powers[i][x], x, target, powers, dp)) % mod;
         };
         
         return dp[idx][value] = ans;
     };
     
     int numberOfWays(int target, int x) {
-        int ans = 0,  powers[301] = {0}, dp[301][301];
+        int ans = 0, dp[301][301];
         
         for (int i = 1; i < 301; i++) {
             for (int j = 1; j < 301; j++) {
                 dp[i][j] = -1;
             };        
         };
+
+        if (powers[0][0] == 0) {
+            for (int p = 1; p < 6; p++) {
+                powers[0][p] = 1;
+                for (int i = 1; i <= 300 && powers[i - 1][p] <= 300; i++) {
+                    powers[i][p] = pow(i, p);     
+                };
+            };
+        };
         
-        for (int i = 1; i <= target && powers[i - 1] <= target; i++) powers[i] = pow(i, x);
-        
-        for (int i = 1; i <= target && powers[i] <= target; i++) {
-            ans = (ans + recurse(i, powers[i], target, powers, dp)) % mod;
+        for (int i = 1; i <= target && powers[i][x] <= target; i++) {
+            ans = (ans + recurse(i, powers[i][x], x, target, powers, dp)) % mod;
         };
         
         return ans;
-    };
+    };    
 };

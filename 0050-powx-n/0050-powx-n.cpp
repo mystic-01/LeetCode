b2129a09718d147c28650490858f4b9a9338c572
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-24 11:50:09 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (92.36%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = 1;
+        bool isNeg = n < 0;
+        long long power = isNeg ? -1LL * n : n;
+        while (power) {
+            if (power % 2) ans *= x, power--;
+            else x *= x, power /= 2;
+        };
+        return isNeg ? 1.0 / ans : ans;
+    };
+};

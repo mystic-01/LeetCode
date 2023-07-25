410f57fbf8bf41de91ee55b2c56d61c9469f8f53
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-25 10:54:31 +0530

    Time: 141 ms (9.33%), Space: 59.7 MB (40.39%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,13 +4,9 @@ public:
         int lo = 0, hi = nums.size() - 1;
         while (lo <= hi) {
             int mid = (hi - lo) / 2 + lo;
-            if (nums[mid] > nums[mid + 1]) {
-                if (nums[mid] > nums[mid - 1]) return mid;
-                else hi = mid - 1;
-            } else if (nums[mid] < nums[mid + 1]) {
-                if (nums[mid + 1] > nums[mid + 2]) return mid + 1;
-                else lo = mid + 1;
-            } else lo = mid + 1;
+            if (nums[mid] < nums[mid + 1]) lo = mid + 1;
+            else if (nums[mid] < nums[mid - 1]) hi = mid - 1;
+            else return mid;
         };
         return -1;
     };

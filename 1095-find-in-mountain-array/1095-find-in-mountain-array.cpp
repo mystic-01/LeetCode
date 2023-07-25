58eda667707f17a220e99d469f88e3a1293a18ad
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-25 11:36:42 +0530

    Time: 4 ms (86.93%), Space: 6.9 MB (80.85%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -1,5 +1,55 @@
 class Solution {
 public:
+    int binarySearch(int &lo, int &hi, int &target, MountainArray &mountainArr, bool incresingArray) {
+        while (lo <= hi) {
+            int mid = (hi - lo) / 2 + lo, val = mountainArr.get(mid);    
+            
+            if (val < target) {
+                if (incresingArray) lo = mid + 1; 
+                else hi = mid - 1;;
+            } else if (val > target) {
+                if (incresingArray) hi = mid - 1; 
+                else lo = mid + 1;
+            } else return mid;
+        };
+        return -1;
+    };
+    
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int n = mountainArr.length(), peak = 0, lo = 0, hi = n - 1;
+        
+        while (lo <= hi) {
+            int mid = (hi - lo) / 2 + lo, val = mountainArr.get(mid), valNext = mountainArr.get(mid + 1);    
+                
+            if (val < valNext) lo = mid + 1;
+            else {
+                int valPrev = mountainArr.get(mid - 1);    
+                if (val < valPrev) hi = mid - 1;
+                else {
+                    peak = mid;
+                    break;
+                };
+            };
+        };
+
+        lo = 0, hi = peak;
+        int ans = binarySearch(lo, hi, target, mountainArr, true);
+        if (ans != -1) return ans;
+
+        lo = peak + 1, hi = n - 1;        
+        ans = binarySearch(lo, hi, target, mountainArr, false);        
+        return ans;
+    };
+};
+
+
+
+/*
+
+With cache 'm'
+
+class Solution {
+public:
     int binarySearch(int &lo, int &hi, int &target, MountainArray &mountainArr, map<int, int> &m, bool incresingArray) {
         while (lo <= hi) {
             int mid = (hi - lo) / 2 + lo;
@@ -46,3 +96,4 @@ public:
     };
 };
 
+*/

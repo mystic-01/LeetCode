6c297cfc4a6aee00d4317125ae51a1003dcfaaa3
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-27 00:50:01 +0530

    Time: 177 ms (5.52%), Space: 75.9 MB (93.57%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        int n = pref.size(), prev = pref[0];
+        
+        for (int i = 1; i < n; i++) {
+            int curr = prev, target = pref[i], num = 0, bit = 0;
+            while (curr || target) {
+                if ((curr & 1) != (target & 1)) {
+                    num = num | (1 << bit);
+                };
+                curr /= 2, target /= 2, bit++;
+            };
+            prev = pref[i];
+            pref[i] = num;  
+        };
+        return pref;
+    };
+};
+/*
+
+1 ^ 0 -> 1
+1 ^ 1 -> 0
+0 ^ 0 -> 0
+0 ^ 1 -> 1
+
+5 ^ x = 2
+    
+    101
+    // 111
+    010
+    // 010
+    000    
+    011
+    0 0 -> 0
+    1 0 -> 1
+    001
+*/

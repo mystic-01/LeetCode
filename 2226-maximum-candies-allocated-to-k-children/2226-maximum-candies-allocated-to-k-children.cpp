6b0bbd3da653718f036939b72846aa59607bf086
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-26 21:03:57 +0530

    Time: 332 ms (20.86%), Space: 84.4 MB (39.57%) - LeetHub

---
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& nums, long long k) {
+        int n = nums.size(), ans = 0, lo = 0, hi = 1e7;
+        
+        sort(nums.begin(), nums.end(), greater<int>());
+        
+        while (lo <= hi) {
+            int mid = (hi - lo) / 2 + lo, i = 0;
+            long long kDupe = k;
+            // cout << mid << " " << lo << " " << hi << "\n";
+            
+            while (kDupe > 0 && i < n) {
+                if (nums[i] >= mid && mid) {
+                    kDupe -= (nums[i] / mid), i++;
+                } else break;
+            };
+            if (kDupe <= 0) ans = mid, lo = mid + 1;
+            else hi = mid - 1;
+        };
+        
+        return ans; 
+    };
+};

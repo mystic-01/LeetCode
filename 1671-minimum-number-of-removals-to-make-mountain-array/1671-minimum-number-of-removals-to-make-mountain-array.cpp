dcfaa641beab65e801c5ce6d72f70e1fca51ad2a
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-26 14:05:42 +0530

    Time: 20 ms (96.50%), Space: 12.1 MB (73.02%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -4,6 +4,42 @@ public:
     
     int minimumMountainRemovals(vector<int>& nums) {
         int n = nums.size(), best = 0;
+        vector<int> lis, len(n);
+            
+        // Find max no of increasing elements (more formally, lis subsequence) on the 'left' of the element. 
+        for (int i = 0; i < n; i++) {
+            if (lis.empty() || nums[i] > lis.back()) lis.push_back(nums[i]);
+            else {
+                auto it = lower_bound(lis.begin(), lis.end(), nums[i]);
+                *it = nums[i];
+            };
+            len[i] = lis.size();
+        };
+        
+        lis.clear();
+        
+        // Find max no of increasing elements on the 'right' of the element. 
+        for (int i = n - 1; i >= 0; i--) {
+            if (lis.empty() || nums[i] > lis.back()) {
+                lis.push_back(nums[i]);
+            } else {
+                auto it = lower_bound(lis.begin(), lis.end(), nums[i]);
+                *it = nums[i];
+            };
+            if (len[i] > 1  && lis.size() > 1) best = max(best, len[i] + (int)lis.size() - 1);
+        };
+        
+        return n - best;
+    };
+};
+
+/*
+class Solution {
+public:
+    // Lomgest valid mountain subsequence
+    
+    int minimumMountainRemovals(vector<int>& nums) {
+        int n = nums.size(), best = 0;
         int left[n], right[n];
         
         for (int i = 0; i < n; i++) left[i] = 0, right[i] = 0;
@@ -31,3 +67,4 @@ public:
         return n - best;
     };
 };
+*/

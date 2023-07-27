2212e65081c7dadfb1efcaa74c5172d4881536af
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-07-27 20:40:58 +0530

    Time: 128 ms (99.64%), Space: 55.7 MB (98.56%) - LeetHub

---
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+ long long maxRunTime(int n, vector<int>& A) {
+        sort(A.begin(), A.end());
+        long long sum = accumulate(A.begin(), A.end(), 0L);
+        int k = 0, na = A.size();
+        while (A[na - 1 - k] > sum / (n - k))
+            sum -= A[na - 1 - k++];
+        return sum / (n - k);
+    }
+};
+
+/*
+
+3
+[3,3,3] - 4
+[2,3,4] - 4
+[1,1,3,5] - 4
+
+last k
+min - 1
+sum - 9
+
+extra - 6
+more extra - 5
+
+*/

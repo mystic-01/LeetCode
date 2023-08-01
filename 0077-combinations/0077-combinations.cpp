517a780158d3bc9ecd3b0eb3887ef4dfe50de7d2
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-01 17:41:57 +0530

    Time: 25 ms (79.09%), Space: 17.8 MB (38.71%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void permute(int idx, int n, int k, vector<vector<int>> & ans, vector<int> ds) {
+    void recurse(int idx, int &n, int &k, vector<vector<int>> &ans, vector<int> &ds) {
         if (ds.size() == k) {
             ans.push_back(ds);
             return;
         };
+        if (idx > n) return;
         
-        for (auto i = idx; i <= n; i++) {
-            ds.push_back(i);
-            permute(i + 1, n, k, ans, ds);
-            ds.pop_back();                
-        };
+        ds.push_back(idx);
+        recurse(idx + 1, n, k, ans, ds);
+        ds.pop_back();
+        recurse(idx + 1, n, k, ans, ds);        
     };
     
+    
     vector<vector<int>> combine(int n, int k) {
-        vector<vector<int>> ans;        
+        vector<vector<int>> ans;
         vector<int> ds;
-        permute(1, n, k, ans, ds);
+        recurse(1, n, k, ans, ds);
         return ans;
     };
 };

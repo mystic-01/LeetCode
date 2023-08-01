b32c309a4dc17b6e95627546c1b70575d0914cb0
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-01 17:38:59 +0530

    Time: 28 ms (69.10%), Space: 18 MB (31.99%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void recurse(int idx, int &n, int &k, vector<vector<int>> &ans, vector<int> &ds) {
+        if (ds.size() == k) {
+            ans.push_back(ds);
+            return;
+        };
+        if (idx > n) return;
+        
+        ds.push_back(idx);
+        recurse(idx + 1, n, k, ans, ds);
+        ds.pop_back();
+        recurse(idx + 1, n, k, ans, ds);        
+    };
+    
+    
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        
+        recurse(1, n, k, ans, ds);
+        
+        return ans;
+    };
+};

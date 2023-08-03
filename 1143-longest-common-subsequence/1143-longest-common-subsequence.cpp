5ba6901a0be0a77ba5908bb5f71c50aaa562b5bb
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-03 17:49:34 +0530

    Time: 50 ms (79.48%), Space: 10.7 MB (81.53%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
-    int recurse(int idx1, int idx2, string &s1, string &s2, int (*dp)[1001]) {
-        if (idx1 == s1.size() || idx2 == s2.size()) return 0;
+//     int recurse(int idx1, int idx2, string &s1, string &s2, int (*dp)[1001]) {
+//         if (idx1 == s1.size() || idx2 == s2.size()) return 0;
         
-        if (dp[idx1][idx2] != -1) return dp[idx1][idx2];
+//         if (dp[idx1][idx2] != -1) return dp[idx1][idx2];
         
-        int take = 0;
-        if (s1[idx1] == s2[idx2]) take = 1 + recurse(idx1 + 1, idx2 + 1, s1, s2, dp);
-        int notTake1 = recurse(idx1 + 1, idx2, s1, s2, dp);
-        int notTake2 = recurse(idx1, idx2 + 1, s1, s2, dp);
+//         int take = 0;
+//         if (s1[idx1] == s2[idx2]) take = 1 + recurse(idx1 + 1, idx2 + 1, s1, s2, dp);
+//         int notTake1 = recurse(idx1 + 1, idx2, s1, s2, dp);
+//         int notTake2 = recurse(idx1, idx2 + 1, s1, s2, dp);
     
-        return dp[idx1][idx2] = max(take, max(notTake1, notTake2));
-    };
+//         return dp[idx1][idx2] = max(take, max(notTake1, notTake2));
+//     };
     
     int longestCommonSubsequence(string &s1, string &s2) {
         int dp[1001][1001];
         
         for (int i = 0; i < 1001; i++) {
             for (int j = 0; j < 1001; j++) {
-                dp[i][j] = -1;
+                dp[i][j] = 0;
             };
         };
         
-        return recurse(0, 0, s1, s2, dp);
+        for (int idx1 = s1.size() - 1; idx1 >= 0; idx1--) {
+            for (int idx2 = s2.size() - 1; idx2 >= 0; idx2--) {
+                int take = 0;
+                if (s1[idx1] == s2[idx2]) take = 1 + dp[idx1 + 1][idx2 + 1];
+                int notTake1 = dp[idx1 + 1][idx2];
+                int notTake2 = dp[idx1][idx2 + 1];
+                
+                dp[idx1][idx2] = max(take, max(notTake1, notTake2));
+            };        
+        };
+        return dp[0][0];
     };
 };

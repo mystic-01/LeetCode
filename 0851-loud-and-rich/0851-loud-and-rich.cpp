91e3b5c240a205262dce1081045941822ecdb6a1
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-04 17:49:15 +0530

    Time: 90 ms (95.61%), Space: 42.3 MB (91.30%) - LeetHub

---
+++ 0851-loud-and-rich/0851-loud-and-rich.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int dfs(int node, vector<vector<int>> &adjList, vector<int> &ans, vector<int> &quiet) {
+        if (ans[node] != -1) return ans[node];
+        
+        int curr = node;
+        for (auto &child : adjList[node]) {
+            int childAns = dfs(child, adjList, ans, quiet);
+            if (quiet[childAns] < quiet[curr]) curr = childAns;            
+        };
+        return ans[node] = curr;
+    };
+
+    
+    vector<int> loudAndRich(vector<vector<int>> &richer, vector<int> &quiet) {
+        int n = quiet.size();
+        vector<int> ans(n, -1);
+        vector<vector<int>> adjList(n);
+        
+        for (auto &vec : richer) adjList[vec[1]].push_back(vec[0]);
+        
+        for (int i = 0; i < n; i++) dfs(i, adjList, ans, quiet);
+                
+        return ans;
+    };
+};
+
+/*
+
+1 : 0
+2 : 1
+3 : 1, 7
+4 : 3
+5 : 3
+6 : 3
+
+0 : 1
+1 : 2, 3
+7 : 3
+3 : 4, 5, 6
+
+
+
+*/

efe02fb53d077e7523ca99bffef7bcd1e24cfd42
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-07 13:17:24 +0530

    Time: 23 ms (66.67%), Space: 17.8 MB (66.67%) - LeetHub

---
+++ 2811-check-if-it-is-possible-to-split-array/2811-check-if-it-is-possible-to-split-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool recurse(int i, int j, int *pre, vector<int>& nums, int &m, int (*dp)[101]) {
+        if (j - i <= 1) return dp[i][j] = true;
+        
+        int totSum = pre[j] - pre[i] + nums[i];
+        if (totSum < m) return dp[i][j] = false;
+        
+        if (dp[i][j] != -1) return dp[i][j];
+        
+        int takeOutLeft = totSum - nums[i];
+        int takeOutRight = totSum - nums[j];
+        
+        bool left = takeOutLeft >= m ? recurse(i + 1, j, pre, nums, m, dp) : false;
+        bool right = takeOutRight >= m ? recurse(i, j - 1, pre, nums, m, dp) : false;
+        
+        return dp[i][j] = left || right;
+    };
+    
+    bool canSplitArray(vector<int>& nums, int m) {
+        int n = nums.size(), pre[n];
+        pre[0] = nums[0];
+        for (int i = 1; i < n; i++) pre[i] = pre[i - 1] + nums[i];
+        
+        int dp[101][101];
+        for (int j = 0; j < 100 + 1; j++) {
+            for (int k = 0; k < 100 + 1; k++) {
+                dp[j][k] = -1;
+            };
+        };
+        
+        // vector<vector<vector<int>>> dp(totSum + 1, vector<vector<int>>(101, vector<int>(101, -1)));
+        return recurse(0, n - 1, pre, nums, m, dp);
+        return 0;
+    };
+};

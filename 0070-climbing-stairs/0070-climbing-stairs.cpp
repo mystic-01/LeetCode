ae103fe800d90ddc99d826249a8fec825e962a6b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-11 21:53:44 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int recurse(int pos, int &n, int *dp) {
-        if (pos == n) return 1;
-        if (pos > n) return 0;
-        
-        if (dp[pos] != -1) return dp[pos];
-        
-        int takeOne = recurse(pos + 1, n, dp);                
-        int takeTwo = recurse(pos + 2, n, dp);                
-
-        return dp[pos] = takeOne + takeTwo;            
-    };
-    
     int climbStairs(int n) {
         int dp[n + 2];
-        for (int i = 0; i < n + 2; i++) dp[i] = -1;    
-        return recurse(0, n, dp);
+        for (int i = 0; i < n + 2; i++) dp[i] = 0;    
+
+        dp[n] = 1, dp[n + 1] = 0;
+        
+        for (int pos = n - 1; pos >= 0; pos--) {
+            int takeOne = dp[pos + 1];
+            int takeTwo = dp[pos + 2];
+            dp[pos] = takeOne + takeTwo;                                                    
+        };
+        
+        return dp[0];
     };
 };

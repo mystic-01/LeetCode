36c82e433b590da24711135d931315af808f5d08
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-11 22:01:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int dp[n + 2];
-        for (int i = 0; i < n + 2; i++) dp[i] = 0;    
-
-        dp[n] = 1, dp[n + 1] = 0;
+        int dp = 0, prev1 = 1, prev2 = 0;
         
         for (int pos = n - 1; pos >= 0; pos--) {
-            int takeOne = dp[pos + 1];
-            int takeTwo = dp[pos + 2];
-            dp[pos] = takeOne + takeTwo;                                                    
+            dp = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = dp;
         };
         
-        return dp[0];
+        return dp;
     };
 };

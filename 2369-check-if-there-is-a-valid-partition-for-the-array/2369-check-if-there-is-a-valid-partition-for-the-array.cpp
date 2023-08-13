5595ae74965080669aec1d4dacca9db9041b3104
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-13 09:54:42 +0530

    Time: 130 ms (88.00%), Space: 84.1 MB (84.31%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     bool validPartition(vector<int>& nums) {
         int n = nums.size(), dp[n + 1];        
-        for (int i = 0; i < n + 1; i++) dp[i] = -1;
+        for (int i = 0; i < n + 1; i++) dp[i] = 0;
         dp[n] = 1;
         
         for (int idx = n - 1; idx >= 0; idx--) {
-            bool ans = 0;
             int startVal = nums[idx], twoSame = 1, threeSame = 1, threeIncrease = 1;
         
             for (int i = idx + 1; i < nums.size() && i < idx + 3; i++) {
@@ -14,11 +13,9 @@ public:
                 if (nums[i] == nums[i - 1] + 1) threeIncrease++;
             };
         
-            if (twoSame >= 2) ans = ans || dp[idx + 2];
-            if (threeSame == 3) ans = ans || dp[idx + 3];
-            if (threeIncrease == 3) ans = ans || dp[idx + 3];
-            
-            dp[idx] = ans;                                    
+            if (twoSame >= 2) dp[idx] = dp[idx] || dp[idx + 2];
+            if (threeSame == 3) dp[idx] = dp[idx] || dp[idx + 3];
+            if (threeIncrease == 3) dp[idx] = dp[idx] || dp[idx + 3];
         };
         
         return dp[0];

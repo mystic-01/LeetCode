06f8b5d1e3eb95c1439291bba9dec9f812176433
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-13 21:36:05 +0530

    Time: 304 ms (80.00%), Space: 133.9 MB (10.00%) - LeetHub

--- 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.cpp
+++ 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.cpp
@@ -3,7 +3,7 @@ public:
     int minAbsoluteDifference(vector<int>& nums, int x) {
         set<int> ms;
         unordered_map<int, int> m;
-        unordered_set<int> seen;
+        // unordered_set<int> seen;
         
         int n = nums.size(), ans = INT_MAX, xDupe = x;
         
@@ -12,15 +12,15 @@ public:
         for (int i = 0; i < n - x; i++) {
             int value = nums[i];
 
-            if (!seen.count(value)) {
-                seen.insert(value);
+            // if (!seen.count(value)) {
+                // seen.insert(value);
                 auto ub = ms.upper_bound(value);
                 auto lb = ub;
             
                 if (lb != ms.begin()) lb--;
             
                 ans = min(ans, min(abs(value - *lb), abs(value - *ub)));
-            };
+            // };
 
             if (--m[nums[xDupe]] == 0) ms.erase(nums[xDupe]);
             xDupe++;

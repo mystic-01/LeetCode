3a494c76ae0805e90289762638b80dc315305a3c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-17 15:17:32 +0530

    Time: 70 ms (79.83%), Space: 30 MB (90.12%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -5,7 +5,6 @@ public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int row = mat.size(), col = mat[0].size();
         int offsets[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        vector<vector<int>> ans(row, vector<int>(col, INT_MAX));
 
         queue<pair<int, int>> q;
         
@@ -13,8 +12,8 @@ public:
             for (int j = 0; j < col; j++) {
                 if (!mat[i][j]) {
                     q.push({i, j});
-                    ans[i][j] = 0;
-                };                
+                    mat[i][j] = 0;
+                } else mat[i][j] = INT_MAX;                
             };                
         };
         
@@ -23,13 +22,13 @@ public:
             q.pop();
             for (int i = 0; i < 4; i++) {
                 int a = x + offsets[i][0], b = y + offsets[i][1];                
-                if (a >= 0 && a < row && b >= 0 && b < col && ans[x][y] + 1 < ans[a][b]) {
-                    ans[a][b] = ans[x][y] + 1;
+                if (a >= 0 && a < row && b >= 0 && b < col && mat[x][y] + 1 < mat[a][b]) {
+                    mat[a][b] = mat[x][y] + 1;
                     q.push({a, b});                    
                 };
             };  
         };
         
-        return ans;
+        return mat;
     };
 };

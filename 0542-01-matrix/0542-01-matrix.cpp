d35ca81a0cb8010c1cb6874287013b29c936d930
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-17 13:00:25 +0530

    Time: 60 ms (93.14%), Space: 30.2 MB (86.75%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int row = mat.size(), col = mat[0].size();
+        int offsets[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        vector<vector<int>> ans(row, vector<int>(col, INT_MAX));
+
+        queue<pair<int, int>> q;
+        
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (!mat[i][j]) {
+                    q.push({i, j});
+                    ans[i][j] = 0;
+                };                
+            };                
+        };
+        
+        while (!q.empty()) {
+            int x = q.front().first, y = q.front().second;    
+            // cout << x << " " << y << " " << "\n";
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int a = x + offsets[i][0], b = y + offsets[i][1];                
+                if (a >= 0 && a < row && b >= 0 && b < col && ans[x][y] + 1 < ans[a][b]) {
+                    ans[a][b] = ans[x][y] + 1;
+                    q.push({a, b});                    
+                };
+            };  
+        };
+        
+        return ans;
+    };
+};

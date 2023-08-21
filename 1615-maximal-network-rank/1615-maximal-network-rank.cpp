6c20ecce098a0b177db2c1ac61762483b35b8bbb
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-21 14:43:29 +0530

    Time: 433 ms (5.00%), Space: 28.1 MB (92.01%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,34 +1,60 @@
 class Solution {
 public:
-    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<set<int>> adjList(n);
-        
-        int maxRank = INT_MIN;
-        
-        for (auto &vec : roads) {
-            adjList[vec[0]].insert(vec[1]);
-            adjList[vec[1]].insert(vec[0]);
+    void updateMaxRank(vector<int> &pair, int *degree, int &maxRank, vector<vector<int>>& roads) {
+        auto findAB = find(roads.begin(), roads.end(), pair);
+        bool pairFound = findAB != roads.end();
+
+        if (!pairFound) {
+            swap(pair[0], pair[1]);
+            auto findBA = find(roads.begin(), roads.end(), pair);
+            pairFound = findBA != roads.end();
         };
+                    
+        int rank = degree[pair[0]] + degree[pair[1]] + (pairFound ? -1 : 0);
+        maxRank = max(maxRank, rank);
+    };
+    
+    void updateList(int &idx, int &curr, int &best, int &secondBest, vector<int> &bestList, vector<int> &secondBestList) {
+        if (curr >= best) {
+            if (curr > best) {
+                secondBest = best, secondBestList = bestList;
+                best = curr, bestList = vector<int>(); 
+            };
+            bestList.push_back(idx);  
+        } else if (curr >= secondBest) {
+            if (curr > secondBest) {
+                secondBest = curr, secondBestList = vector<int>(); 
+            };
+            secondBestList.push_back(idx);              
+        };
+    };
+    
+    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
+        int maxRank = INT_MIN, degree[n], best = INT_MIN, secondBest = INT_MIN;
+        vector<int> bestList, secondBestList;
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j != i) {
-                    int rank = adjList[i].size() + adjList[j].size() + (adjList[i].find(j) == adjList[i].end() ? 0 : -1); 
-                    maxRank = max(maxRank, rank);    
-                };
-            };        
+        for (int i = 0; i < n; i++) degree[i] = 0;
+        for (auto &vec : roads) ++degree[vec[0]], ++degree[vec[1]];
+        for (int i = 0; i < n; i++) updateList(i, degree[i], best, secondBest, bestList, secondBestList);
+            
+
+        if (bestList.size() > 1) {
+            for (int i = 0; i < bestList.size(); i++) {
+                for (int j = i + 1; j < bestList.size(); j++) {
+                    vector<int> pair = {bestList[i], bestList[j]};
+                    updateMaxRank(pair, degree, maxRank, roads);
+                };            
+            };
+        } else {
+            for (int j = 0; j < secondBestList.size(); j++) {
+                vector<int> pair = {bestList[0], secondBestList[j]};
+                updateMaxRank(pair, degree, maxRank, roads);
+            };     
         };
         
         return maxRank;
     };
 };
 
-/*
 
-0->1->2->3
-      |
-      4
 
-7<-5->6
-
-*/

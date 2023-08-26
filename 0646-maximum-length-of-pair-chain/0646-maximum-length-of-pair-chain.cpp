39929f4e1eccdf6706c80c6fbb5d474e0122407c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-08-26 15:28:11 +0530

    Time: 55 ms (89.73%), Space: 22.5 MB (76.92%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int recurse(int idx, int end, vector<vector<int>> &nums, int (*dp)[2002]) {
-        if (idx == nums.size()) return 0;
-        
-        if (dp[idx][end + 1001] != -1) return dp[idx][end + 1001];
-        
-        int take = 0, notTake = 0;
-        if (nums[idx][0] > end) take = 1 + recurse(idx + 1, nums[idx][1], nums, dp);
-        notTake = recurse(idx + 1, end, nums, dp);
-        
-        return dp[idx][end + 1001] = max(take, notTake);
-    };
     
     int findLongestChain(vector<vector<int>>& nums) {
-        sort(nums.begin(), nums.end());        
-        int dp[1001][2002];
-        
-        for (int i = 0; i < 1001; i++) {
-            for (int j = 0; j < 2002; j++) {
-                dp[i][j] = -1;
-            };
+        auto comp = [](const vector<int> &a, const vector<int> &b) {
+            return a[1] < b[1];
         };
         
-        return recurse(0, -1001, nums, dp);
+        sort(nums.begin(), nums.end(), comp);        
+        
+        int end = INT_MIN, count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i][0] > end) end = nums[i][1], count++;
+        };
+        return count;
     };
 };

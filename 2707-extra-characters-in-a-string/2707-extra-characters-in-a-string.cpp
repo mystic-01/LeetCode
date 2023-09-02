c4fb574673da52fadf30ee22c752f1153f664aa8
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-09-02 09:58:56 +0530

    Time: 296 ms (28.23%), Space: 89.8 MB (39.32%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,14 +1,16 @@
 class Solution {
+int outOfBound = 1e5;
+    
 public:
     int recurse(int idx, int len, string &s, set<string> &st, int (*dp)[52]) {
         if (idx == s.size()) return 0;
-        if (idx + len > s.size()) return 1e7;
+        if (idx + len > s.size()) return outOfBound;
         
         if (dp[idx][len] != -1) return dp[idx][len]; 
         
-        int take = 1e7, takeExtraLen = 1e7, notTake = 1e7;
+        int take = outOfBound, takeExtraLen = outOfBound, notTake = outOfBound;
             
-        if (idx + len <= s.size() && st.count(s.substr(idx, len))) take = recurse(idx + len, 1, s, st, dp);
+        if (st.count(s.substr(idx, len))) take = recurse(idx + len, 1, s, st, dp);
         takeExtraLen = recurse(idx, len + 1, s, st, dp);
         notTake = 1 + recurse(idx + 1, 1, s, st, dp);
         

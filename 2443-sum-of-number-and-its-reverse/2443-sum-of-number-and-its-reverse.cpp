61e4f9ef0e59146fcc3d9da8faf3aec9798b68c3
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-09-03 20:49:16 +0530

    Time: 2469 ms (5.13%), Space: 6.6 MB (14.99%) - LeetHub

--- 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
@@ -1,23 +1,26 @@
 // Solution 3 : On demand iterations
 
+unordered_set<int> s;
+
 class Solution {
 public:
+    Solution() {
+        int digits = 1;
+        for (int i = 0; i <= 1e5; i++) {
+            if (i == 10 || i == 100 || i == 1000 || i == 10000 || i == 100000) digits++; 
+            s.insert(i + reverse(i, digits));            
+        };          
+    };
+    
     int reverse(int num, int digits) {
         int ans = 0, mul = 1;
-        
         while (num) {
             ans += ((num % 10) * pow(10, digits - 1)), num /= 10, digits--;
         };
-        
         return ans;
     };
     
     bool sumOfNumberAndReverse(int num) {
-        int digits = 1;
-        for (int i = 0; i <= num; i++) {
-            if (i == 10 || i == 100 || i == 1000 || i == 10000 || i == 100000) digits++; 
-            if (i + reverse(i, digits) == num) return true;            
-        };                
-        return false;
+        return s.find(num) != s.end();
     };
 };

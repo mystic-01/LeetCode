92349d7b35fb4951b9e9e40797cfbecac823c25b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-09-11 14:58:01 +0530

    Time: 39 ms (84.84%), Space: 33.6 MB (96.50%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,20 +1,39 @@
+// class Solution {
+// public:
+//     vector<int> findDisappearedNumbers(vector<int>& nums) {
+//         int n = nums.size(), start = 0;
+//         vector<int> ans(n);            
+//         for (int i = 0; i < n; i++) {
+//             ans[i] = i + 1;
+//         };
+        
+//         for (int i = 0; i < n; i++) {
+//             ans[nums[i] - 1] = -1;                                
+//         };
+        
+//         for (int i = 0; i < n; i++) {
+//             if (ans[i] != -1) ans[start++] = ans[i];
+//         };
+//         ans.erase(ans.begin() + start, ans.end());
+//         return ans;
+//     };
+// };
+
+// True O(1) Extra Space
+
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         int n = nums.size(), start = 0;
-        vector<int> ans(n);            
+        vector<int> ans;            
         for (int i = 0; i < n; i++) {
-            ans[i] = i + 1;
+            int idx = abs(nums[i]) - 1;
+            nums[idx] = -1 * abs(nums[idx]);
         };
         
         for (int i = 0; i < n; i++) {
-            ans[nums[i] - 1] = -1;                                
+            if (nums[i] > 0) ans.push_back(i + 1);                                
         };
-        
-        for (int i = 0; i < n; i++) {
-            if (ans[i] != -1) ans[start++] = ans[i];
-        };
-        ans.erase(ans.begin() + start, ans.end());
         return ans;
     };
 };

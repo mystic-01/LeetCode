3924fc03adf2d6f35f464e1fc69f66e98fcf23ab
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-09-11 12:56:09 +0530

    Time: 11 ms (57.70%), Space: 13.5 MB (31.66%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& group) {
-        vector<vector<int>> ans;
-        map<int, int> m;
+        vector<vector<int>> ans, temp(group.size() + 1);
         
         for (int i = 0; i < group.size(); i++) {
-            if (m.find(group[i]) == m.end()) {
-                ans.push_back({i});
-                m[group[i]] = ans.size() - 1;
-            } else {
-                if (ans[m[group[i]]].size() < group[i]) {
-                    ans[m[group[i]]].push_back(i);
-                } else {
-                    ans.push_back({i});
-                    m[group[i]] = ans.size() - 1;
-                };
-            };                
+            temp[group[i]].push_back(i);
+            if (temp[group[i]].size() == group[i]) {
+                ans.push_back({});
+                swap(ans.back(), temp[group[i]]);
+            };
         };
         
         return ans;

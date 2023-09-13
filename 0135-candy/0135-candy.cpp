01814c55e99dcecb009d304b726e698112afdab9
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-09-13 12:49:26 +0530

    Time: 14 ms (83.40%), Space: 17.1 MB (98.60%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size(), candies[n];
+
+        for (int i = 0; i < n; i++) candies[i] = 1;
+        
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;                            
+            };
+        };
+        
+        int tots = candies[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);                            
+            };
+            tots += candies[i];
+        };
+
+        return tots;
+    };
+};
+
+/*
+
+[1,2,2,2,3,4,5,6,5,5]
+ 1 2 1 1 2 3 4 5 2 1
+ 
+ 10 3 2 1 1 1 0 0 9
+ 4  3 2 1 1 2 1 1 2 
+ 
+
+*/

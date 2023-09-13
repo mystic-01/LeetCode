f48c08f08f8048020d47d7e25f8ba51d8dc9e1fa
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-09-13 12:52:38 +0530

    Time: 20 ms (40.38%), Space: 17.2 MB (84.84%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int n = ratings.size(), candies[n];
-
-        for (int i = 0; i < n; i++) candies[i] = 1;
-        
+        int n = ratings.size(), candies[n], tots = 0;
+        for (int i = 0; i < n; i++) {
+            candies[i] = 1;
+        };
         for (int i = 1; i < n; i++) {
             if (ratings[i] > ratings[i - 1]) {
                 candies[i] = candies[i - 1] + 1;                            
             };
         };
-        
-        int tots = candies[n - 1];
         for (int i = n - 2; i >= 0; i--) {
             if (ratings[i] > ratings[i + 1]) {
                 candies[i] = max(candies[i], candies[i + 1] + 1);                            
             };
+        };
+        for (int i = n - 1; i >= 0; i--) {
             tots += candies[i];
         };
-
         return tots;
     };
 };

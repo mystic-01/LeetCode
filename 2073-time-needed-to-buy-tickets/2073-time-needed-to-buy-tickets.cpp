f34645449a7f9f063ffa72f6e1f7811ad0ba3c72
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-09-18 08:29:21 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (30.80%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -3,8 +3,7 @@ public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int n = tickets.size(), time = 0;
         for (int i = 0; i < n; i++) {
-            if (i <= k) time += min(tickets[k], tickets[i]);
-            else time += min(tickets[k] - 1, tickets[i]);            
+            time += min(tickets[k] - ((i <= k) ? 0 : 1), tickets[i]);
         };
         return time;
     };

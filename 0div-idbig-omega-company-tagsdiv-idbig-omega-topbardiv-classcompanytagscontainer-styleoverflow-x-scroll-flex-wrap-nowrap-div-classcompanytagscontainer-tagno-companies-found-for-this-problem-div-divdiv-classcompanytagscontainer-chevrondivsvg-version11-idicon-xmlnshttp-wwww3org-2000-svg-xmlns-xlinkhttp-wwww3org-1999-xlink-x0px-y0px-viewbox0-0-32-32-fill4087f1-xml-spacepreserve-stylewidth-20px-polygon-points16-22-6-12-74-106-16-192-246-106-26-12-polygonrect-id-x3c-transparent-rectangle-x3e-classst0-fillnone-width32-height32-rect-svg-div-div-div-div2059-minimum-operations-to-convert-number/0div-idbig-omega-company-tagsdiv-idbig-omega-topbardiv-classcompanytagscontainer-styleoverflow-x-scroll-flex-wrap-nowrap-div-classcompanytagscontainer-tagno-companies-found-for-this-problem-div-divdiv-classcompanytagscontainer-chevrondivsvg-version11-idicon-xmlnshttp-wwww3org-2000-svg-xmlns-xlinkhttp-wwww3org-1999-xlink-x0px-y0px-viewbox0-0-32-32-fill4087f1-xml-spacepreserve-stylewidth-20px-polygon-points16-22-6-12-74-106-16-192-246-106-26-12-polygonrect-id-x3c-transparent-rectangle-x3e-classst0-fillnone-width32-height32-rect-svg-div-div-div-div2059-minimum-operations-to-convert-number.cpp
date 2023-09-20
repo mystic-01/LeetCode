9a23760ab5c2728c4076b3979f0c29df9eb1d3bc
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-09-20 12:49:47 +0530

    Time: 361 ms (41.07%), Space: 134.4 MB (23.21%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2059-minimum-operations-to-convert-number/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2059-minimum-operations-to-convert-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums, int start, int goal) {
+        bool visited[1001] = {false};
+        int n = nums.size(), steps = 0;
+        
+        queue<int> q;
+        q.push(start);
+        
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int curr = q.front();
+                q.pop();
+                if (curr == goal) return steps;
+                if (curr < 0 || curr > 1000 || visited[curr]) continue;
+                for (int j = 0; j < n; j++) {
+                    for (int child : {curr + nums[j], curr - nums[j], curr ^ nums[j]}) {
+                        q.push(child);
+                    };
+                };
+                visited[curr] = true;
+            };
+            steps++;
+        };
+        return -1;
+    };
+};

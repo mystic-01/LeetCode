67295c117e9b23c2d3825f3323d04f7e78780689
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-10-01 00:00:42 +0530

    Time: 4 ms (39.62%), Space: 7.6 MB (64.92%) - LeetHub

--- 2451-odd-string-difference/2451-odd-string-difference.cpp
+++ 2451-odd-string-difference/2451-odd-string-difference.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
-string oddString(vector<string>& ws) {
-    for (int j = 0; j + 1 < ws[0].size(); ++j) {
-        unordered_map<int, vector<int>> m;
-        for (int i = 0; i < ws.size(); ++i)
-            m[ws[i][j + 1] - ws[i][j]].push_back(i);
-        if (m.size() == 2)
-            return ws[begin(m)->second.size() == 1 ? begin(m)->second.back() : next(begin(m))->second.back()];
-    }
-    return ws[0];
-}
+    string oddString(vector<string>& words) {
+        int n = words[0].size();
+        for (int i = 1; i < n; ++i) {
+            int val1 = 2147483647, val2 = 2147483647;
+            string str1 = "", str2 = ""; 
+            bool isVal1Final = false;
+            for (string &str : words) {
+                int val = int(str[i] - str[i - 1]);
+                if (str1 == "") val1 = val, str1 = str;
+                else {
+                    if (str2 != "") {
+                        if (val == val1) return str2;
+                        else return str1;
+                    } else {
+                        if (isVal1Final) {
+                            if (val != val1) return str;                                                    
+                        } else {
+                            if (val != val1) val2 = val, str2 = str;
+                            else isVal1Final = true;    
+                        };
+                    };
+                };
+            };
+        };
+        return "";
+    };
 };

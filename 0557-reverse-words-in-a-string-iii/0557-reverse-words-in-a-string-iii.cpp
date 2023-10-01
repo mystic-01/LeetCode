4d70c55303beb8832db33f11ee03e0df5aea38d4
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-10-01 09:27:37 +0530

    Time: 13 ms (74.16%), Space: 10.1 MB (55.04%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,34 @@
+// class Solution {
+// public:
+//     string reverseWords(string s) {
+//         std::ios::sync_with_stdio(false);
+//         std::cin.tie(nullptr);
+        
+//         string ans = "", temp = "";
+//         for (auto i = 0; i < s.length(); i++) {
+//             if (s[i] == ' ') {
+//                 ans += temp + " ";
+//                 temp = "";
+//             } else temp = s[i] + temp;
+//         };
+//         return ans + temp;
+//     };
+// };
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+
+        int j = 0;
+        for (auto i = 0; i < s.length(); i++) {
+            if (s[i] == ' ') {
+                reverse(s.begin() + j, s.begin() + i);
+                j = i + 1;
+            };
+        };
+        reverse(s.begin() + j, s.end());
+        return s;
+    };
+};

8ab1ae11376e74cd215475c6d0ee3684d9ca0524
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-10-02 12:46:04 +0530

    Time: 29 ms (88.92%), Space: 13.6 MB (23.75%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     bool winnerOfGame(string nums) {
-        int n = nums.size(), diffBetweenAliceAndBob = 0;
+        int n = nums.size(), canAliceRemove = 0;
         for (int i = 1; i < n - 1; ++i) {
             if (nums[i] == nums[i - 1] && nums[i] == nums[i + 1]) {
-                diffBetweenAliceAndBob += (nums[i] == 'A' ? 1 : -1);
+                canAliceRemove += (nums[i] == 'A' ? 1 : -1);
             };
         };
-        return diffBetweenAliceAndBob > 0;
+        return canAliceRemove > 0;
     };
 };
-
-
-/*
-
-ABBBAAA
-
-*/

533a755f07c662fb09c7b6ad90b63760154baab3
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-10-07 20:11:43 +0530

    Time: 112 ms (30.88%), Space: 7.5 MB (64.52%) - LeetHub

---
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    int mod = 1e9 + 7;
+public:
+int dp[101][51][51] = {};
+int dfs(int n, int m, int k, int i, int max_val) {
+    if (n == i)
+        return k == 0;
+    if (k < 0)
+        return 0;
+    if (dp[max_val][i][k] == 0) {        
+        dp[max_val][i][k] = 1;
+        for (auto val = 1; val <= m; ++val) {
+            dp[max_val][i][k] += dfs(n, m, k - (max_val < val), i + 1, max(max_val, val));
+            dp[max_val][i][k] %= 1000000007;
+        }
+    }
+    return dp[max_val][i][k] - 1;
+}
+int numOfArrays(int n, int m, int k) {
+    return dfs(n, m, k, 0, 0);
+}
+};
+
+
+/*
+
+7c2
+
+7 6 5 4 3 2 1
+2 1    5 4 3 2 1
+
+
+
+*/

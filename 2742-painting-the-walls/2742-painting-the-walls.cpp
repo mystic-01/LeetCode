541a6ec13dde142d2ab62fbc4118e5ca931cf88d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-10-14 11:15:43 +0530

    Time: 455 ms (12.02%), Space: 90.3 MB (73.95%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int recurse(int idx, int walls, vector<int>& cost, vector<int>& time, int (*dp)[501]) {
+    int unmasker = 501;
+    
+    int recurse(int mask, vector<int>& cost, vector<int>& time, int *dp) {
+        int idx = mask % unmasker, walls = mask / unmasker;
         if (walls <= 0) return 0;
         if (idx >= cost.size()) return 1e9;
 
-        if (dp[idx][walls] != -1) return dp[idx][walls];
+        if (dp[mask] != -1) return dp[mask];
+        int take = cost[idx] + recurse(idx + 1 + ((walls - time[idx] - 1) * unmasker), cost, time, dp);
+        int notTake = recurse(idx + 1 + (walls * unmasker), cost, time, dp);        
         
-        int take = cost[idx] + recurse(idx + 1, walls - time[idx] - 1, cost, time, dp);
-        int notTake = recurse(idx + 1, walls, cost, time, dp);        
-        
-        return dp[idx][walls] = min(take, notTake);
+        return dp[mask] = min(take, notTake);
     };
     
     int paintWalls(vector<int>& cost, vector<int>& time) {
-        int dp[501][501];
-        for(int i = 0; i < 501; ++i) {
-            for(int j = 0; j < 501; ++j) {
-                dp[i][j] = -1;
-            };
+        int dp[251001];
+        
+        for(int i = 0; i < 251001; ++i) {
+            dp[i] = -1;
         };
-        return recurse(0, cost.size(), cost, time, dp);        
+        return recurse(cost.size() * unmasker, cost, time, dp);        
     };
 };
 

71c30166caaa87d7494e56f7fe449d9845a54385
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-10-22 09:44:33 +0530

    Time: 239 ms (30.77%), Space: 127.7 MB (61.54%) - LeetHub

--- 2910-minimum-number-of-groups-to-create-a-valid-assignment/2910-minimum-number-of-groups-to-create-a-valid-assignment.cpp
+++ 2910-minimum-number-of-groups-to-create-a-valid-assignment/2910-minimum-number-of-groups-to-create-a-valid-assignment.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int minGroupsForValidAssignment(vector<int>& nums) {
-        int n = nums.size(), minSize = INT_MAX, best = 2147364847;
-        map<int, int> m;
-        for (int i = 0; i < n; ++i) {
-            m[nums[i]]++;
+        int n = nums.size(), minSize = INT_MAX, size = 1;
+        sort(begin(nums), end(nums));
+        multiset<int> ms;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] == nums[i - 1]) {
+                size++;
+            } else {
+                ms.insert(size);
+                size = 1;
+            };
         };
-
-        for (auto &it : m) {
-            minSize = min(minSize, (int)it.second);
-        };
+        ms.insert(size);
+        minSize = *ms.begin();
         
         for (int mid = minSize; mid >= 1; --mid) {
             int midPlusOne = mid + 1, ans = 0;
-            for (auto &it : m) {
-                int size = it.second, curr = 0;
+            for (auto &size : ms) {
+                int curr = 0;
                 for (int i = 0; !curr && mid * i <= size; ++i) {
                     for (int j = 0; !curr && midPlusOne * j <= size; ++j) {
                         if (mid * i + midPlusOne * j == size) {
@@ -31,10 +35,9 @@ public:
             };
             if (ans != INT_MAX) {
                 return ans;
-                // best = min(best, ans);
             };
         };
-        return best;
+        return -1;
     };
 };
 /*

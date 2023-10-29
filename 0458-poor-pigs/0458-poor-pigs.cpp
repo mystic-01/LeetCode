6b100001b11aeaeb159f1d7979f6d883c573247d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-10-29 19:14:17 +0530

    Time: 2 ms (53.09%), Space: 6.5 MB (25.93%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.cpp
+++ 0458-poor-pigs/0458-poor-pigs.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-    return ceil(log2(buckets)/log2(int(minutesToTest/minutesToDie)+1));
+        int lo = 0, hi = buckets, ans = 1, turns = minutesToTest / minutesToDie;
+        
+        while (lo <= hi) {
+            int mid = (hi - lo) / 2 + lo; // curr pigs
+            if (pow(turns + 1, mid) >= buckets) {
+                ans = mid, hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            };
+        };
+        
+        return ans;
     };
 };

a783abb3c83db1dfa4d189f7971a7e9f50df392b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-11-10 23:58:56 +0530

    Time: 369 ms (75.76%), Space: 83.4 MB (100.00%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -2,28 +2,38 @@ class Solution {
 public:
     vector<int> restoreArray(vector<vector<int>>& pairs) {
         int n = pairs.size() + 1, curr = 2147483647;
-        map<int, vector<int>> m;
+        map<int, pair<int, int>> m;
         vector<int> ans(n);
         for (auto &vec : pairs) {
-            m[vec[0]].push_back(vec[1]);
-            m[vec[1]].push_back(vec[0]);
+            if (m.find(vec[0]) == m.end()) {
+                m[vec[0]].first = vec[1];
+                m[vec[0]].second = INT_MIN;
+            } else {
+                m[vec[0]].second = vec[1];
+            };
+            if (m.find(vec[1]) == m.end()) {
+                m[vec[1]].first = vec[0];
+                m[vec[1]].second = INT_MIN;
+            } else {
+                m[vec[1]].second = vec[0];
+            };
         };
         for (auto &it : m) {
-            if (it.second.size() == 1) {
+            if (it.second.second == INT_MIN) {
                 curr = it.first;
                 break;
             };
         };
         ans[0] = curr;
         for (int i = 1, parent = curr; i < n; ++i) {
-            for (auto &it2 : m[curr]) {
-                if (it2 != parent) {
-                    parent = curr;
-                    curr = it2;
-                    ans[i] = curr;
-                    break;
-                };
-            };                
+            if (m[curr].first != parent) {
+                parent = curr;
+                curr = m[curr].first;
+            } else {
+                parent = curr;
+                curr = m[curr].second;
+            };
+            ans[i] = curr;
         };
         return ans;
     };

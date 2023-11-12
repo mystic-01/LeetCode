f2660c645d82b7d3558c7ce04494923d9270ecc6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-11-12 12:47:28 +0530

    Time: 620 ms (37.60%), Space: 57.6 MB (78.18%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.cpp
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    int numBusesToDestination(vector<vector<int>>& routes, int S, int T) {
-        unordered_map<int, vector<int>> to_routes;
-        for (int i = 0; i < routes.size(); ++i)
-            for (int j : routes[i])
-                to_routes[j].push_back(i);
-        queue<pair<int, int>> bfs;
-        bfs.push({S, 0});
-        unordered_set<int> seen = {S};
-        while (!bfs.empty()) {
-            int stop = bfs.front().first, bus = bfs.front().second;
-            bfs.pop();
-            if (stop == T)
-                return bus;
-            for (int i : to_routes[stop]) {
-                for (int j : routes[i]) {
-                    if (seen.find(j) == seen.end()) {
-                        seen.insert(j);
-                        bfs.push({j, bus + 1});
-                    }
-                }
-                routes[i].clear();
-            }
-        }
+    int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
+        unordered_map<int, vector<int>> m;
+        for (int i = 0; i < routes.size(); ++i) {
+            for (int &x : routes[i]) {
+                m[x].push_back(i);    
+            };
+        };
+        int dist = 0;
+        queue<int> q;
+        unordered_set<int> vis;
+        q.push(source);
+        
+        while (!q.empty()) {
+            for (int i = 0, size = q.size(); i < size; ++i) {
+                int node = q.front();
+                q.pop();
+                if (node == target) {
+                    return dist;    
+                };
+                for (auto &val : m[node]) {
+                    // 'val' is the ith row of routes we need to process now
+                    if (!vis.count(val)) {
+                        for (auto &num : routes[val]) {
+                            q.push(num);
+                        };
+                        vis.insert(val);
+                    };                
+                };
+            };
+            ++dist;
+        };
+        
         return -1;
-    }
+    };
 };

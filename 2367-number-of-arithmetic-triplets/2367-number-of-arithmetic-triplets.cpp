7b0fdf43a326378f28e36323ff8f81268ea93edb
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-11-19 02:36:37 +0530

    Time: 2 ms (85.00%), Space: 8.9 MB (57.43%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        unordered_map<int, int> next;        
         int triplets = 0;
         for (auto &it : nums) {
-            if (find(begin(nums), end(nums), it + diff) != end(nums)) {
-                next[it + diff] += (next.find(it) == next.end() ? 2 : next[it] + 1);    
-                next.erase(it);
+            if (it >= 2 * diff) {
+                triplets += (find(begin(nums), end(nums), (it - diff)) != end(nums) 
+                                  && find(begin(nums), end(nums), (it - 2 *diff)) != end(nums));
             };            
-        };
-        for (auto &it2 : next) {
-            triplets += max(0, it2.second - 2);
         };
         return triplets;
     };

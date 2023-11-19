2448915eb0c6c1ae521ff66e91183da1aa6f1ce6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-11-19 15:26:46 +0530

    Time: 2 ms (43.90%), Space: 6.3 MB (72.73%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        int matches = 0;
-        while (n != 1) {
-            matches += n / 2, n = n / 2 + n % 2;    
-        };     
-        return matches;
+        // int matches = 0;
+        // while (n != 1) {
+            // matches += n / 2, n = n / 2 + n % 2;    
+        // };     
+        return n - 1;
     };
 };

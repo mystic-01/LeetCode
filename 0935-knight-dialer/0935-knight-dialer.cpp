39cac96c071dc32ae0c916bb6d6aec9d1ea58d70
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-11-27 20:25:11 +0530

    Time: 59 ms (83.48%), Space: 6.6 MB (88.16%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -5,22 +5,23 @@ public:
     int knightDialer(int n) {
         vector<vector<int>> nextAllowed = {{4, 6}, {6, 8}, {7, 9}, {4, 8}, {0, 3, 9}, {}, {0, 1, 7}, 
                                            {2, 6}, {1, 3}, {2, 4}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}};
-        vector<vector<int>> dp(n + 1, vector<int>(11, -1));
+        vector<int> dp(11, 0), curr(11, 0);
         
         for (int prev = 0; prev < 11; ++prev) {
-            dp[n][prev] = 1;
+            dp[prev] = 1;
         };
         
         for (int idx = n - 1; idx >= 0; --idx) {
             for (int prev = 0; prev < 11; ++prev) {
                 int totalNumbers = 0;
                 for (int &x : nextAllowed[prev]) {
-                    totalNumbers = (totalNumbers + dp[idx + 1][x]) % mod;
+                    totalNumbers = (totalNumbers + dp[x]) % mod;
                 };             
-                dp[idx][prev] = totalNumbers;
+                curr[prev] = totalNumbers;
             };             
+            dp = curr;
         };
         
-        return dp[0][10];
+        return dp[10];
     };
 };

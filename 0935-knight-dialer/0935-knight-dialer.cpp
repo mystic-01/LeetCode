9bc8095037041c786e7a9ac7d75df165df249f84
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-11-27 20:22:00 +0530

    Time: 141 ms (55.41%), Space: 37.8 MB (41.67%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -1,25 +1,26 @@
 static int mod = 1e9 + 7;
 
 class Solution {
-    
 public:
-    
-    int recurse(int idx, int prev, int &n, vector<vector<int>> &nextAllowed, vector<vector<int>> &dp) {
-        if (idx == n) return dp[idx][prev] = 1;
-        if (dp[idx][prev] != -1) return dp[idx][prev];
-        
-        int totalNumbers = 0;
-        for (int &x : nextAllowed[prev]) {
-            totalNumbers = (totalNumbers + recurse(idx + 1, x, n, nextAllowed, dp)) % mod;
-            
-        };        
-        return dp[idx][prev] = totalNumbers;
-    };
-    
     int knightDialer(int n) {
         vector<vector<int>> nextAllowed = {{4, 6}, {6, 8}, {7, 9}, {4, 8}, {0, 3, 9}, {}, {0, 1, 7}, 
                                            {2, 6}, {1, 3}, {2, 4}, {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}};
         vector<vector<int>> dp(n + 1, vector<int>(11, -1));
-        return recurse(0, 10, n, nextAllowed, dp);
+        
+        for (int prev = 0; prev < 11; ++prev) {
+            dp[n][prev] = 1;
+        };
+        
+        for (int idx = n - 1; idx >= 0; --idx) {
+            for (int prev = 0; prev < 11; ++prev) {
+                int totalNumbers = 0;
+                for (int &x : nextAllowed[prev]) {
+                    totalNumbers = (totalNumbers + dp[idx + 1][x]) % mod;
+                };             
+                dp[idx][prev] = totalNumbers;
+            };             
+        };
+        
+        return dp[0][10];
     };
 };

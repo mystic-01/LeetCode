72f1ac5840dfa198f150857a02c895a04623dea0
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-12-01 15:49:37 +0530

    Time: 3 ms (71.01%), Space: 11.7 MB (95.20%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1662-check-if-two-string-arrays-are-equivalent/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        cout << "-------------------\n";
+        int i = 0, n1 = word1.size(), idx1  = 0, j = 0, n2 = word2.size(), idx2  = 0;
+        while (i < n1 && j < n2) {
+            if (idx1 == word1[i].size()) {
+                ++i, idx1 = 0;
+            } else if (idx2 == word2[j].size()) {
+                ++j, idx2 = 0;
+            } else if (word1[i][idx1] != word2[j][idx2]) {
+                return false;                
+            } else {
+                ++idx1, ++idx2;
+            };
+        };
+        return ((i == n1 && idx1 == 0) || (i == n1 - 1 && idx1 == word1[i].size())) && 
+            ((j == n2 && idx2 == 0) || (j == n2 - 1 && idx2 == word2[j].size()));
+    };
+};

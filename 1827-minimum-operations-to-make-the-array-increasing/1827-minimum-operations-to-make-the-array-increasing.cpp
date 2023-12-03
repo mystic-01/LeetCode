dc508489472b1bbeef2d5630ab52f04e28d4028b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-12-03 18:35:38 +0530

    Time: 13 ms (36.02%), Space: 16.1 MB (70.83%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.cpp
@@ -3,10 +3,8 @@ public:
     int minOperations(vector<int>& nums) {
         int n = nums.size(), ops = 0;
         for (int i = 1; i < n; ++i) {
-            if (nums[i] <= nums[i - 1]) {
-                ops += (nums[i - 1] - nums[i] + 1);
-                nums[i] = nums[i - 1] + 1;
-            };    
+            ops += max(nums[i - 1] - nums[i] + 1, 0);
+            nums[i] = max(nums[i], nums[i - 1] + 1);
         };
         return ops;
     };

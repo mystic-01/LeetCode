941965dee3fe5c9b67e059db391b277420b66a6e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-12-03 12:47:18 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (42.62%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        queue<int> memory;
-        for (int &x : arr) {
-            if (x == 0) {
-                memory.push(0);
-            };
-            if (memory.size()) {
-                memory.push(x);
-                x = memory.front();
-                memory.pop();
+        int n = arr.size(), start = 0, end = n - 1;
+        while (start < end) {
+            if (arr[start++] == 0) --end;
+        };
+        for (int i = n - 1; i >= 0; --i, --end) {
+            arr[i] = arr[end];
+            if (start != end && arr[end] == 0) {
+                arr[--i] = arr[end];    
             };
         };
     };

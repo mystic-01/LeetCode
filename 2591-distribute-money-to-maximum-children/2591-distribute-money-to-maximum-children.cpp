0a91f04e488680c2040e8811cde3532a4593999a
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-12-06 19:23:55 +0530

    Time: 8 ms (13.43%), Space: 6.6 MB (12.96%) - LeetHub

--- 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.cpp
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int distMoney(int money, int children) {
-        if (money < children) return -1;
-        if (money < 8) return 0;
         if ((money -= children) < 0) return -1;
+        if (money < 7) return 0;
         int valid = min(children, money / 7);
         return max(0, valid + (((money % 7 == 3 && (valid == children || valid == children - 1)) 
                                 || ((money % 7 || valid * 7 < money) && money / 7 >= children)) ? - 1 : 0));        

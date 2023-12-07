b2d1c73be05acfe6e95726de69c9b405d250c113
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-12-07 11:25:14 +0530

    Time: 358 ms (12.29%), Space: 57.4 MB (72.22%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void updateBits(int &x, int *bits, bool &isBadAddition, bool &addOrSubtract) {
+    void updateBits(int &x, int *bits, bool &isBadBit, bool &addOrSubtract) {
         for (int i = 0; i < 32; ++i, x >>= 1) {
             if (x & 1) bits[i] += (addOrSubtract ? 1 : -1);
-            if (bits[i] > 1) isBadAddition = 1;    
+            if (bits[i] > 1) isBadBit = 1;    
         }; 
     };
     
     int longestNiceSubarray(vector<int>& nums) {
-        int bits[32] = {0}, n =  nums.size(), left = 0, maxLen = 1;
+        int bits[32] = {0}, n =  nums.size(), left = 0, maxLen = 1, x = 0;
+        bool isBadBit = 0, add = 1, subtract = 0;
+        
         for (int right = 0; right < n; ++right) {
-            int x = nums[right];
-            bool isBadAddition = 0, add = 1, subtract = 0;
-            
-            updateBits(x, bits, isBadAddition, add);
+            isBadBit = 0, x = nums[right];
+            updateBits(x, bits, isBadBit, add);
 
-            while (left < n && isBadAddition) {
-                isBadAddition = 0, x = nums[left];
-                updateBits(x, bits, isBadAddition, subtract);
+            while (left < n && isBadBit) {
+                isBadBit = 0, x = nums[left];
+                updateBits(x, bits, isBadBit, subtract);
                 ++left;
             };
             maxLen = max(maxLen, right - left + 1);

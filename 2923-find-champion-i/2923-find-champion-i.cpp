2696c65de7707b4756f2e8c839d4961314b3059e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-12-10 20:50:26 +0530

    Time: 63 ms (39.86%), Space: 36.6 MB (95.52%) - LeetHub

--- 2923-find-champion-i/2923-find-champion-i.cpp
+++ 2923-find-champion-i/2923-find-champion-i.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     int findChampion(vector<vector<int>>& grid) {
         int n = grid.size();
+        bool indegree[n];
+        fill(indegree, indegree + n, 0);
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (grid[i][j]) grid[j].push_back(-1);
+                if (grid[i][j] == 1) indegree[j] = 1;
             };
         };
         for (int i = 0; i < n; ++i) {
-            if (grid[i].back() != -1) return i;
+            if (!indegree[i]) return i;
         };
         return -1;
     };

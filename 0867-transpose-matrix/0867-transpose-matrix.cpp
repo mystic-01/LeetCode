16e78534c7af269cdb5bcfef9d515ba5ce63aade
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2023-12-10 13:15:51 +0530

    Time: 11 ms (37.66%), Space: 10.9 MB (45.29%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cpp
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -3,7 +3,6 @@ public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
         int row = matrix.size(), col = matrix[0].size();
         vector<vector<int>> ans(col, vector<int>(row));
-        int x = 0, y = 0;
         for (int j = 0; j < col; ++j) {
             for (int i = 0; i < row; ++i) {
                 ans[j][i] = matrix[i][j];

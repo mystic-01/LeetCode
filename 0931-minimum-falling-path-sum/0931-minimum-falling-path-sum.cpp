29fa18a90e623c9c274523a17b7645bad50cb1ea
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-01-19 22:33:29 +0530

    Time: 6 ms (98.98%), Space: 12.6 MB (5.13%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -4,7 +4,7 @@ public:
         int row = matrix.size(), col = matrix[0].size(), ans = 2147483647;        
         for (int i = 1; i < row; ++i) {
             for (int j = 0; j < col; ++j) {
-                matrix[i][j] += min({j - 1 >= 0 ? matrix[i - 1][j - 1] : 2147483647, matrix[i - 1][j], j + 1 < col ? matrix[i - 1][j + 1] : 2147483647});       
+                matrix[i][j] += min(matrix[i - 1][j], min(j - 1 >= 0 ? matrix[i - 1][j - 1] : 2147483647, j + 1 < col ? matrix[i - 1][j + 1] : 2147483647));       
             };        
         };
         for (int j = 0; j < col; ++j) {

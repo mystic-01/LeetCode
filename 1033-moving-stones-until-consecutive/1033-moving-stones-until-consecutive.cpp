e47b3ad29d4d2e7d7f77bce2748930241325f75b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-01-20 19:56:51 +0530

    Time: 3 ms (49.61%), Space: 7.6 MB (12.40%) - LeetHub

--- 1033-moving-stones-until-consecutive/1033-moving-stones-until-consecutive.cpp
+++ 1033-moving-stones-until-consecutive/1033-moving-stones-until-consecutive.cpp
@@ -8,7 +8,7 @@ public:
         int diff1 = (stones[1] - stones[0] - 1), diff2 = (stones[2] - stones[1] - 1);
         
         if (diff1 == 0 && diff2 == 0) ans[0] = 0;
-        else if (diff1 == 0 || diff2 == 0 || diff1 == 1 || diff2 == 1) ans[0] = 1;
+        else if (diff1 <= 1 || diff2 <= 1) ans[0] = 1;
         else ans[0] = 2;
         ans[1] = diff1 + diff2; 
         

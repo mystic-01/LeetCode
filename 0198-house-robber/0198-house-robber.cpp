3491c8a540095573da92cce2b9fbdaa439386b6b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-01-21 13:15:16 +0530

    Time: 0 ms (100.00%), Space: 9 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n == 1) return nums[0];
-        
-        for (auto i = 0; i < n; i++) {
-            if (i - 3 >= 0) nums[i] = max(nums[i] + nums[i - 3], nums[i] + nums[i - 2]);
-            else if (i - 2 >= 0) nums[i] = max(nums[i] + nums[i - 2], nums[i - 2]);
+        for (int i = 2; i < n; ++i) {
+            nums[i] += max(nums[i - 2], i - 3 >= 0 ? nums[i - 3] : 0);    
         };
-        
-        return max(nums[n - 1], nums[n - 2]);
+        return max(nums[n - 1], n - 2 >= 0 ? nums[n - 2] : 0);
     };
 };

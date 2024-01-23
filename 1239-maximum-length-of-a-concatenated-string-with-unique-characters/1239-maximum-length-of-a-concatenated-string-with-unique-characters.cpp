1d36c77122831169b82af67050da542c829bd434
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-01-23 23:43:25 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (82.63%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int recurse(int idx, int mask, vector<string>& arr) {
+        if (idx == arr.size()) return mask;
+        
+        bool canTakeNewMask = true;
+        int newMask = mask;
+        for (char &ch : arr[idx]) {
+            int val = ch - 'a';
+            if ((newMask >> val) & 1 == 1) canTakeNewMask = false;
+            else newMask = newMask | (1 << val);
+        };
+        
+        int take = canTakeNewMask ? recurse(idx + 1, newMask, arr) : 0;
+        int notTake = recurse(idx + 1, mask, arr);
+        return __builtin_popcount(take) > __builtin_popcount(notTake) ? take : notTake;
+    };
+    
+    int maxLength(vector<string>& arr) {
+        int mask = 0, ansVal = recurse(0, mask, arr), ansBits = 0;
+        while (ansVal) {
+            ansBits += (ansVal % 2), ansVal /= 2;
+        };
+        return ansBits;
+    };
+};

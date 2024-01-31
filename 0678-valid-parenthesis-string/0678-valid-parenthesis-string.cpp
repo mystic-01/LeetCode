94c0b3d258d0ff670b52bf808b751eff9c4e5fae
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-01-31 14:15:12 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (27.07%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,54 +1,24 @@
 class Solution {
 public:
-    // bool recurse
-    void printNums(int &left, int &right, int &aestrik) {
-        cout << left << " " << right << " " << aestrik << "\n";
-    };
-    
     bool checkValidString(string s, bool checkedOnce = false) {
         int left = 0, aestrik = 0, right = 0;
         for (char &ch : s) {
-            switch (ch) {
-                case '(':
-                    ++left;
-                    printNums(left, right, aestrik);
-                    break;
-                case '*':
-                    ++aestrik;
-                    printNums(left, right, aestrik);
-                    break;
-                case ')':
-                    ++right;
-                    if (right > left + aestrik) return false;
-                    printNums(left, right, aestrik);
-                    break;
+            if (ch == '(') ++left;
+            else if (ch == '*') ++aestrik;
+            else {
+                ++right;
+                if (right > left + aestrik) return false;
             };    
         };
-        cout << "----------------\n";
-        printNums(left, right, aestrik);
         
         if (left > right + aestrik) return false;
         int diff = abs(left - right);
         
-        printNums(diff, aestrik, diff);
-        cout << "khatam testcase ----------------\n";
-        
         if (diff != 0 && aestrik < abs(left - right)) return false;
         reverse(begin(s), end(s));
         for (char &ch : s) {
-            switch (ch) {
-                case '(':
-                    ch = ')';
-                    break;
-                case '*':
-                    break;
-                case ')':
-                    ch = '(';
-                    break;
-            };    
+            ch = ch == ')' ? '(' : (ch == '(' ? ')' : '*');   
         };
-
-        
         return checkedOnce || checkValidString(s, true);
     };
 };

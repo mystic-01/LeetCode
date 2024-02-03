c0d78e63024bf01b4e14982fe75060f4f819b887
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-03 12:26:33 +0530

    Time: 3 ms (99.46%), Space: 10.6 MB (28.99%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int recurse(int idx, vector<int>& arr, int &k, int *dp) {
-        if (idx >= arr.size()) return 0;
-        if (dp[idx] != -1) return dp[idx];
-        
-        int sum = 0, ansWithCurrWindow = 0, maxWithCurrWindow = 0;
-        for (int i = 0; i < k && idx + i < arr.size(); ++i) {
-            maxWithCurrWindow = max(arr[idx + i], maxWithCurrWindow);  
-            
-            ansWithCurrWindow = maxWithCurrWindow * (i + 1) + recurse(idx + i + 1, arr, k, dp);
-            
-            sum = max(sum, ansWithCurrWindow);
-        };
-        return dp[idx] = sum;
-    };
-    
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size(), dp[n + 1];
-        fill(dp, dp + n + 1, -1);
-        return recurse(0, arr, k, dp);
+        fill(dp, dp + n + 1, 0);
+
+        for (int idx = n - 1; idx >= 0; --idx) {
+            int currAns = 0, currMax = 0;
+            for (int i = 0; i < k && idx + i < n; ++i) {
+                currMax = max(arr[idx + i], currMax);  
+                currAns = currMax * (i + 1) + (idx + i + 1 >= n ? 0 : dp[idx + i + 1]);
+                dp[idx] = max(dp[idx], currAns);
+            };
+        };
+        return dp[0];
     };
 };

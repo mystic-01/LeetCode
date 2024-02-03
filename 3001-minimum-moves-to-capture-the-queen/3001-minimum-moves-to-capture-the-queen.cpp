313a912ce293de03531bb79b5b52f936c01eeff4
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-03 13:34:55 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (6.56%) - LeetHub

--- 3001-minimum-moves-to-capture-the-queen/3001-minimum-moves-to-capture-the-queen.cpp
+++ 3001-minimum-moves-to-capture-the-queen/3001-minimum-moves-to-capture-the-queen.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
-        
-        if (a == e || b == f) {
-            // Rook captures
-            if (a == e) {
-                int start = min(b, f), end = max(b, f);
-                // If Bishop is not in between
-                if (c != a || d < start || d > end) return 1;
-            } else if (b == f) {
-                int start = min(a, e), end = max(a, e);
-                // If Bishop is not in between
-                if (d != b || c < start || c > end) return 1;
-            };
-        };        
-        if (abs(c - e) == abs(d - f)) {
-            // Bishop captures
+        // Rook captures Row-wise
+        if (a == e) {
+            int start = min(b, f), end = max(b, f);
+            // If Bishop is not in between
+            if (c != a || d < start || d > end) return 1;
+        // Rook captures Col-wise
+        } else if (b == f) {
+            int start = min(a, e), end = max(a, e);
+            // If Bishop is not in between
+            if (d != b || c < start || c > end) return 1;
+        // Bishop captures
+        } else if (abs(c - e) == abs(d - f)) {
             int startRow = min(c, e), endRow = max(c, e);
             int startCol = min(d, f), endCol = max(d, f);
             // If Rook is not in between

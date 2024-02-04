0ed7a0348f1dc9bb1af7010c497922d78a33585e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-04 11:58:38 +0530

    Time: 4 ms (91.16%), Space: 9.2 MB (23.20%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.size(), freq[59] = {0}, needed = t.size(), start = 0, end = INT_MAX;
+        bool chars[59] = {0};
+        
+        for (int i = 0; i < needed; ++i) {
+            chars[t[i] - 'A'] = 1, ++freq[t[i] - 'A'];    
+        };
+        
+        for (int left = 0, i = 0; i < n; ++i) {
+            if (chars[s[i] - 'A']) {
+                if (freq[s[i] - 'A']-- > 0) --needed;
+            };    
+            while (needed == 0) {
+                if (end - start > i - left) {
+                    start = left, end = i;    
+                };
+                if (chars[s[left] - 'A']) {
+                    if (freq[s[left] - 'A']++ >= 0) ++needed;                        
+                };
+                ++left;
+            };
+        };
+        return end == INT_MAX ? "" : s.substr(start, end - start + 1);
+    };
+};

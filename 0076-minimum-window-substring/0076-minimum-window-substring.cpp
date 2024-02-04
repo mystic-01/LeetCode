9c9b7817dee1f34cb24be6c78bc6b23084319097
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-04 12:11:55 +0530

    Time: 9 ms (72.68%), Space: 9.2 MB (22.77%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         int n = s.size(), freq[58] = {0}, needed = t.size(), start = 0, end = INT_MAX;
-        bool chars[58] = {0};
         
         for (int i = 0; i < needed; ++i) {
-            chars[t[i] - 'A'] = 1, ++freq[t[i] - 'A'];    
+            ++freq[t[i] - 'A'];    
         };
         
         for (int left = 0, i = 0; i < n; ++i) {
-            if (chars[s[i] - 'A']) {
-                if (freq[s[i] - 'A']-- > 0) --needed;
-            };    
+            if (freq[s[i] - 'A']-- > 0) --needed;
             while (needed == 0) {
-                if (end - start > i - left) {
-                    start = left, end = i;    
-                };
-                if (chars[s[left] - 'A']) {
-                    if (freq[s[left] - 'A']++ >= 0) ++needed;                        
-                };
+                if (end - start > i - left) start = left, end = i;    
+                if (freq[s[left] - 'A']++ >= 0) ++needed;                        
                 ++left;
             };
         };

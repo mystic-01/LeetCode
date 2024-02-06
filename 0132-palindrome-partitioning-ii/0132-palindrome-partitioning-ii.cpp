94e2261a8b17e23c045ad83a2c8c1e398dc397b9
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-06 14:03:06 +0530

    Time: 1375 ms (41.16%), Space: 7.4 MB (67.31%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int minCut(string s) {
         int n = s.size(), dp[n + 1];
-        fill(dp, dp + n, INT_MAX);
+        fill(dp, dp + n, 1e5);
         dp[n] = 0;
         
         for (int idx = n - 1; idx >= 0; --idx) {

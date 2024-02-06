e6ff441d6150cea5e05d5d1352c6fcee3d052087
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-06 13:56:33 +0530

    Time: 1272 ms (55.24%), Space: 7.4 MB (67.07%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
-    bool isPalindrome(int start, int end, string &s, vector<vector<int>> &dpPartition) {
-        if (dpPartition[start][end] != -1) {
-            return dpPartition[start][end];    
-        };
+    bool isPalindrome(int start, int end, string &s) {
         while (start < end && s[start] == s[end]) {
             ++start, --end;
         };
-        return dpPartition[start][end] = (start >= end);
+        return start >= end;
     };
     
-    int countMinPartitions(int idx, int end, string &s, int *dp, vector<vector<int>> &dpPartition) {
+    int countMinPartitions(int idx, int end, string &s, int *dp) {
         if (idx > end) {
             return 0;
         };
@@ -20,8 +17,8 @@ public:
         
         int count = INT_MAX;
         for (int i = end; i >= idx; --i) {
-            if (isPalindrome(idx, i, s, dpPartition)) {
-                count = min(count, 1 + countMinPartitions(i + 1, end, s, dp, dpPartition));                    
+            if (isPalindrome(idx, i, s)) {
+                count = min(count, 1 + countMinPartitions(i + 1, end, s, dp));                    
             };
         };
         return dp[idx] = count;
@@ -30,13 +27,6 @@ public:
     int minCut(string s) {
         int n = s.size(), dp[n];
         fill(dp, dp + n, -1);
-        vector<vector<int>> dpPartition(n, vector<int>(n, -1));
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                dpPartition[i][j] = -1;
-            };        
-        };
-        return countMinPartitions(0, n - 1, s, dp, dpPartition) - 1;
+        return countMinPartitions(0, n - 1, s, dp) - 1;
     };
 };

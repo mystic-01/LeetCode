03f553619cc8a510ac76de1d993efbfa2af930da
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-06 12:30:47 +0530

    Time: 165 ms (29.29%), Space: 144.1 MB (17.72%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public: 
-    bool isPalindrome(string s) {
+    bool isPalindrome(string &s) {
         int i = 0, j = s.length() - 1;
         while (i < j) {
             if (s[i] != s[j]) return 0;

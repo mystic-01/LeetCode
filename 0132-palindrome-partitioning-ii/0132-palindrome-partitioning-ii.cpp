57002140a57b8d4df359184d6e1032a448acee82
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-06 14:02:02 +0530

    Time: 1393 ms (40.64%), Space: 7.5 MB (65.80%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    bool isPalindrome(int start, int end, string &s) {
-        while (start < end && s[start] == s[end]) {
-            ++start, --end;
-        };
-        return start >= end;
-    };
-    
     int minCut(string s) {
         int n = s.size(), dp[n + 1];
         fill(dp, dp + n, INT_MAX);
@@ -14,12 +7,15 @@ public:
         
         for (int idx = n - 1; idx >= 0; --idx) {
             for (int i = n - 1; i >= idx; --i) {
-                if (isPalindrome(idx, i, s)) {
+                int start = idx, end = i;
+                while (start < end && s[start] == s[end]) {
+                    ++start, --end;
+                };
+                if (start >= end) {
                     dp[idx] = min(dp[idx], 1 + dp[i + 1]);
                 };
             };
         };
-        
         return dp[0] - 1;
     };
 };

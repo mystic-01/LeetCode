8e657a7afd15261e1a6c4e07a96aee6c2b9bb296
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-06 12:48:10 +0530

    Time: 137 ms (33.70%), Space: 95.7 MB (26.95%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public: 
-    bool isPalindrome(string &s, int i, int j) {
+    bool isPalindrome(string &s, int i, int j, int (*dp)[16]) {
+        if (dp[i][j] != -1) {
+            return dp[i][j];    
+        };
         while (i < j) {
-            if (s[i] != s[j]) return false;
+            if (s[i] != s[j]) {
+                return dp[i][j] = 0;
+            };
             i++, j--;
         };
-        return true;
+        return dp[i][j] = 1;
     };  
         
     // pair<int, int> - Ingenious Harshit!!!!
-    void solve(int idx, string &s, int n, vector<vector<string>>& ans, vector<pair<int, int>> &ds) {
+    void solve(int idx, string &s, int n, vector<vector<string>>& ans, vector<pair<int, int>> &ds, int (*dp)[16]) {
         if(idx == n) {
             ans.push_back({});
             for (auto &[first, second] : ds) {
@@ -19,9 +24,9 @@ public:
         };
         
         for (auto i = idx; i < n; i++) {
-            if (isPalindrome(s, idx, i)) {
+            if (isPalindrome(s, idx, i, dp)) {
                 ds.push_back({idx, i});
-                solve(i + 1, s, n, ans, ds);
+                solve(i + 1, s, n, ans, ds, dp);
                 ds.pop_back();
             };            
         };        
@@ -30,8 +35,13 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<pair<int, int>> ds;
-        int n = s.length();
-        solve(0, s, n, ans, ds);
+        int n = s.size(), dp[16][16];
+        for (int i = 0; i < 16; ++i) {
+            for (int j = 0; j < 16; ++j) {
+                dp[i][j] = -1;
+            };        
+        };
+        solve(0, s, n, ans, ds, dp);
         return ans;
     };  
 };      

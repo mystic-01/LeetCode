a63e2c9d1fefc11727fb006e44c23a66dbae89ea
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-07 12:39:14 +0530

    Time: 123 ms (87.01%), Space: 11.9 MB (28.17%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int recurse(int i, int j, vector<int> &nums, vector<vector<int>> &dp) {
-        if (i > j) {
-            return 0;    
-        };
-        if (dp[i][j] != -1) {
-            return dp[i][j];     
-        };
-        int maxCoins = 0;
-        for (int mid = i; mid <= j; ++mid) {
-            int coins = (nums[i - 1] * nums[mid] * nums[j + 1]) + recurse(i, mid - 1, nums, dp) + recurse(mid + 1, j, nums, dp);
-            maxCoins = max(maxCoins, coins);
-        };
-        return dp[i][j] = maxCoins;
-    };
-    
     int maxCoins(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
         
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
-        return recurse(1, n, nums, dp);
+        
+        for (int i = n; i >= 1; --i) {
+            for (int j = 1; j <= n; ++j) {
+                if (i <= j) {
+                    for (int mid = i; mid <= j; ++mid) {
+                        int coins = (nums[i - 1] * nums[mid] * nums[j + 1]) + dp[i][mid - 1] + dp[mid + 1][j];
+                        dp[i][j] = max(dp[i][j], coins);
+                    };
+                };
+            };        
+        };
+        
+        return dp[1][n];
     };
 };

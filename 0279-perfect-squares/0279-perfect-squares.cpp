6f6188d55222b4733a92e06d85e05fcbb0897ce4
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-08 12:29:14 +0530

    Time: 161 ms (37.45%), Space: 157.5 MB (5.32%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -8,12 +8,11 @@ public:
         
         for (int idx = sqSize - 1; idx >= 1; --idx) {
             for (int target = 0; target <= n; ++target) {
-                int take = outOfBound, notTake = outOfBound, newTarget = target - (idx * idx);
+                int newTarget = target - (idx * idx);
                 if (newTarget >= 0) {
-                    take = 1 + dp[idx][newTarget];
+                    dp[idx][target] = 1 + dp[idx][newTarget];
                 };
-                notTake = dp[idx + 1][target];
-                dp[idx][target] = min(take, notTake);                
+                dp[idx][target] = min(dp[idx][target], dp[idx + 1][target]);
             };        
         };
         return dp[1][n];

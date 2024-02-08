2029d6d935f4e8002db2e00b5de11df7d742b750
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-08 13:46:42 +0530

    Time: 112 ms (12.39%), Space: 33.6 MB (7.10%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -8,10 +8,10 @@ public:
         pq.push(1);
         visited.insert(1);   
         
-        auto visitAndInsertInPq = [&pq, &visited](int &val) {
-            pq.push(val);
-            visited.insert(val);               
-        };
+        // auto visitAndInsertInPq = [&pq, &visited](int &val) {
+            // pq.push(val);
+            // visited.insert(val);               
+        // };
         
         while (!pq.empty() && --n) {
             int num = pq.top();
@@ -19,18 +19,21 @@ public:
             
             if (num <= maxInt - num) {
                 int numX2 = num + num;
-                if (!visited.count(numX2)) {    
-                    visitAndInsertInPq(numX2);
+                if (!visited.count(numX2)) {   
+                    pq.push(numX2);
+                    visited.insert(numX2); 
                 };
                 if (numX2 <= maxInt - num) {
                     int numX3 = numX2 + num; 
-                    if (!visited.count(numX3)) {        
-                        visitAndInsertInPq(numX3);
+                    if (!visited.count(numX3)) {    
+                        pq.push(numX3);
+                        visited.insert(numX3); 
                     };
                     if (numX3 <= maxInt - numX2) {
                         int numX5 = numX3 + numX2;
-                        if (!visited.count(numX5)) {        
-                            visitAndInsertInPq(numX5);
+                        if (!visited.count(numX5)) {    
+                            pq.push(numX5);
+                            visited.insert(numX5); 
                         };
                     };
                 };

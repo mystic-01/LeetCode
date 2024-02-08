a28e0e2730a312cf3ef6e76267699d83df763d94
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-08 16:57:21 +0530

    Time: 713 ms (5.94%), Space: 7.7 MB (99.38%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int countPrimes(int n) {
+        int count = 0, nLimit = 5 * 1e6;
+        static vector<bool> nums(nLimit, 0);
         if (n < 2) return 0;
-        int count = 0;
-        static vector<bool> nums(5 * 1e6 + 1, 0);
 
         if (nums[n - 1] == 0) {
-            for (int i = 2; i < n; ++i) {
+            for (int i = 2; i < nLimit; ++i) {
                 if (!nums[i]) {
                     for (int j = 2; j * i < n; ++j) {
                         nums[j * i] = 1;

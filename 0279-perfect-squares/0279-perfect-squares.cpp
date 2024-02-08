881d6dbd6a218d20f074af6121e2a06254032b6f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-08 12:37:22 +0530

    Time: 43 ms (88.64%), Space: 7.3 MB (91.74%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -2,8 +2,8 @@ static const int outOfBound = 1e5;
 class Solution {
 public:
     int numSquares(int n) {
-        int sqSize = sqrt(n) + 1;
-        vector<int> dp(n + 1, outOfBound);
+        int sqSize = sqrt(n) + 1, dp[n + 1];
+        fill(dp, dp + n + 1, outOfBound);
         dp[0] = 0;
         
         for (int idx = sqSize - 1; idx >= 1; --idx) {

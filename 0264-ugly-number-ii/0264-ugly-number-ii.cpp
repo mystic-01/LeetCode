f6da44f4f297aba201754232b7bf4bc0c92a72f5
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-08 14:00:08 +0530

    Time: 122 ms (9.77%), Space: 29.2 MB (23.77%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,44 +1,28 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        int nthUglyNumber = 1, maxInt = 2147483647;
-        
+        int ans = 1;
         priority_queue<int, vector<int>, greater<int>> pq;
         set<int> visited;
         pq.push(1);
-        visited.insert(1);   
         
-        // auto visitAndInsertInPq = [&pq, &visited](int &val) {
-            // pq.push(val);
-            // visited.insert(val);               
-        // };
-        
-        while (!pq.empty() && --n) {
+        while (!pq.empty() && n) {
             int num = pq.top();
             pq.pop();
-            
-            if (num <= maxInt - num) {
-                int numX2 = num + num;
-                if (!visited.count(numX2)) {   
-                    pq.push(numX2);
-                    visited.insert(numX2); 
-                };
-                if (numX2 <= maxInt - num) {
-                    int numX3 = numX2 + num; 
-                    if (!visited.count(numX3)) {    
-                        pq.push(numX3);
-                        visited.insert(numX3); 
-                    };
-                    if (numX3 <= maxInt - numX2) {
-                        int numX5 = numX3 + numX2;
-                        if (!visited.count(numX5)) {    
-                            pq.push(numX5);
-                            visited.insert(numX5); 
+            if (!visited.count(num)) {        
+                visited.insert(num);
+                ans = num, --n;
+                if (num <= INT_MAX - num) {
+                    pq.push(num * 2);
+                    if (num + num <= INT_MAX - num) {
+                        pq.push(num * 3);
+                        if (num + num + num <= INT_MAX - num - num) {
+                            pq.push(num * 5);
                         };
                     };
                 };
-            };
+            };    
         };
-        return pq.top();
+        return ans;
     };
 };

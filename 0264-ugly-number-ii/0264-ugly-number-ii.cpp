053ac79bd15d28c15b759882328749e15a663e5b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-08 13:26:33 +0530

    Time: 123 ms (9.53%), Space: 29.1 MB (23.77%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        long long nthUglyNumber = 1;
+        int nthUglyNumber = 1, maxInt = 2147483647;
         
-        priority_queue<long long, vector<long long>, greater<long long>> pq;
-        pq.push(1LL);
-        set<long long> uglyNumbers;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        pq.push(1);
+        set<int> visited;
         
         while (!pq.empty() && n) {
-            long long top = pq.top();
+            int top = pq.top();
             pq.pop();
-            if (!uglyNumbers.count(top)) {        
+            if (!visited.count(top)) {        
                 nthUglyNumber = top, --n; 
-                uglyNumbers.insert(top);                
-                if (top <= LLONG_MAX - top) {
-                    pq.push(top * 2LL);
-                    if (top + top <= LLONG_MAX - top) {
-                        pq.push(top * 3LL);
-                        if (top + top + top <= LLONG_MAX - top - top) {
-                            pq.push(top * 5LL);                            
+                visited.insert(top);                
+                if (top <= maxInt - top) {
+                    pq.push(top * 2);
+                    if (top + top <= maxInt - top) {
+                        pq.push(top * 3);
+                        if (top + top + top <= maxInt - top - top) {
+                            pq.push(top * 5);                            
                         };
                     };
                 };

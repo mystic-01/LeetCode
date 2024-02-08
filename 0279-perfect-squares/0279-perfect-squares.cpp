732f4d07690c4ecee1138e39102919b5a98385bf
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-08 12:28:25 +0530

    Time: 137 ms (58.82%), Space: 157.5 MB (5.32%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,25 +1,21 @@
 static const int outOfBound = 1e5;
 class Solution {
 public:
-    int recurse(int idx, int target, int &sqSize, vector<vector<int>> &dp) {
-        if (idx == sqSize) {
-            return target == 0 ? 0 : outOfBound;    
-        };    
-        if (dp[idx][target] != -1) {
-            return dp[idx][target];  
-        };
-        int take = outOfBound, notTake = outOfBound, newTarget = target - (idx * idx);
-        if (newTarget >= 0) {
-            take = 1 + recurse(idx, newTarget, sqSize, dp);
-        };
-        notTake = recurse(idx + 1, target, sqSize, dp);
-        
-        return dp[idx][target] = min(take, notTake);
-    };
-    
     int numSquares(int n) {
         int sqSize = sqrt(n) + 1;
-        vector<vector<int>> dp(101, vector<int>(n + 1, -1));
-        return recurse(1, n, sqSize, dp);         
+        vector<vector<int>> dp(sqSize + 1, vector<int>(n + 1, outOfBound));
+        dp[sqSize][0] = 0;
+        
+        for (int idx = sqSize - 1; idx >= 1; --idx) {
+            for (int target = 0; target <= n; ++target) {
+                int take = outOfBound, notTake = outOfBound, newTarget = target - (idx * idx);
+                if (newTarget >= 0) {
+                    take = 1 + dp[idx][newTarget];
+                };
+                notTake = dp[idx + 1][target];
+                dp[idx][target] = min(take, notTake);                
+            };        
+        };
+        return dp[1][n];
     };
 };

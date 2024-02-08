5d1936cb3f25ef57ff01a8bb8c8a3a38d6ee241d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-08 12:35:32 +0530

    Time: 41 ms (90.09%), Space: 10.6 MB (32.42%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -3,19 +3,17 @@ class Solution {
 public:
     int numSquares(int n) {
         int sqSize = sqrt(n) + 1;
-        vector<int> prev(n + 1, outOfBound), dp(n + 1, outOfBound);
-        prev[0] = 0;
+        vector<int> dp(n + 1, outOfBound);
+        dp[0] = 0;
         
         for (int idx = sqSize - 1; idx >= 1; --idx) {
             for (int target = 0; target <= n; ++target) {
                 int newTarget = target - (idx * idx);
                 if (newTarget >= 0) {
-                    dp[target] = 1 + dp[newTarget];
+                    dp[target] = min(dp[target], 1 + dp[newTarget]);
                 };
-                dp[target] = min(dp[target], prev[target]);
             };        
-            prev = dp;
         };
-        return prev[n];
+        return dp[n];
     };
 };

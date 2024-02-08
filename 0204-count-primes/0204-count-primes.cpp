311044f748752add5fd2dfc2652f3937879f9b00
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-08 17:02:00 +0530

    Time: 227 ms (86.96%), Space: 7.6 MB (99.43%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -3,12 +3,13 @@ public:
     int countPrimes(int n) {
         int count = 0, nLimit = 5 * 1e6;
         static vector<bool> nums(nLimit, 0);
+        static int marked = 0;
         if (n < 2) return 0;
 
-        if (nums[n - 1] == 0) {
+        if (!marked++) {
             for (int i = 2; i < nLimit; ++i) {
                 if (!nums[i]) {
-                    for (int j = 2; j * i < n; ++j) {
+                    for (int j = 2; j * i < nLimit; ++j) {
                         nums[j * i] = 1;
                     };
                 };

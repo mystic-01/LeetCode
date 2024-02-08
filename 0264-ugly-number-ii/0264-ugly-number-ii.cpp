8b8936e9ed28dd683b116984a0d9332a0e97b25b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-08 13:44:54 +0530

    Time: 116 ms (11.01%), Space: 33.7 MB (6.91%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -4,26 +4,38 @@ public:
         int nthUglyNumber = 1, maxInt = 2147483647;
         
         priority_queue<int, vector<int>, greater<int>> pq;
-        pq.push(1);
         set<int> visited;
+        pq.push(1);
+        visited.insert(1);   
         
-        while (!pq.empty() && n) {
-            int top = pq.top();
+        auto visitAndInsertInPq = [&pq, &visited](int &val) {
+            pq.push(val);
+            visited.insert(val);               
+        };
+        
+        while (!pq.empty() && --n) {
+            int num = pq.top();
             pq.pop();
-            if (!visited.count(top)) {        
-                nthUglyNumber = top, --n; 
-                visited.insert(top);                
-                if (top <= maxInt - top) {
-                    pq.push(top * 2);
-                    if (top + top <= maxInt - top) {
-                        pq.push(top * 3);
-                        if (top + top + top <= maxInt - top - top) {
-                            pq.push(top * 5);                            
+            
+            if (num <= maxInt - num) {
+                int numX2 = num + num;
+                if (!visited.count(numX2)) {    
+                    visitAndInsertInPq(numX2);
+                };
+                if (numX2 <= maxInt - num) {
+                    int numX3 = numX2 + num; 
+                    if (!visited.count(numX3)) {        
+                        visitAndInsertInPq(numX3);
+                    };
+                    if (numX3 <= maxInt - numX2) {
+                        int numX5 = numX3 + numX2;
+                        if (!visited.count(numX5)) {        
+                            visitAndInsertInPq(numX5);
                         };
                     };
                 };
             };
         };
-        return nthUglyNumber;
+        return pq.top();
     };
 };

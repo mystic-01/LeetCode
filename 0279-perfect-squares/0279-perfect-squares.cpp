fedc5a6ba4c29efa7cdcbb7353f08baaf7e8698f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-08 11:45:55 +0530

    Time: 426 ms (8.84%), Space: 261.1 MB (5.32%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,7 +1,7 @@
 static const int outOfBound = 1e5;
 class Solution {
 public:
-    int recurse(int idx, int target, int &sqSize, int *squares, vector<vector<int>> &dp) {
+    int recurse(int idx, int target, int &sqSize, vector<vector<int>> &dp) {
         if (idx == sqSize) {
             return target == 0 ? 0 : outOfBound;    
         };    
@@ -9,18 +9,15 @@ public:
             return dp[idx][target];  
         };
         int sq = idx * idx;
-        int take = (target >= sq ? 1 + recurse(idx, target - sq, sqSize, squares, dp) : outOfBound);
-        int notTake = recurse(idx + 1, target, sqSize, squares, dp);
+        int take = (target >= sq ? 1 + recurse(idx, target - sq, sqSize, dp) : outOfBound);
+        int notTake = recurse(idx + 1, target, sqSize, dp);
         
         return dp[idx][target] = min(take, notTake);
     };
     
     int numSquares(int n) {
-        int sqSize = sqrt(n) + 1, squares[sqSize];        
-        for (int i = 1; i < sqSize; ++i) {
-            squares[i] = i * i;                
-        };
+        int sqSize = sqrt(n) + 1;
         vector<vector<int>> dp(101, vector<int>(n + 1, -1));
-        return recurse(1, n, sqSize, squares, dp);         
+        return recurse(1, n, sqSize, dp);         
     };
 };

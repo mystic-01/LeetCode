542aa80915a0ae16ac60ed0c427913704f945341
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-10 01:47:18 +0530

    Time: 8 ms (90.21%), Space: 18.9 MB (5.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -6,14 +6,15 @@ public:
         unordered_map<int, int> m;
         vector<int> ans;
      
-        for (auto i : nums)
+        for (int &i : nums) {
             m[i]++;            
-        for (auto i : m)
-            v[i.second].push_back(i.first);
-        for (auto i = n; i > -1 && k > 0; i--) {
-            for (auto j = 0; j < v[i].size(); j++) {
+        };
+        for (auto &it : m) {
+            v[it.second].push_back(it.first);            
+        };
+        for (auto i = n; i >= 0 && k; --i) {
+            for (auto j = 0; j < v[i].size() && k; ++j, --k) {
                 ans.push_back(v[i][j]);
-                if (--k == 0) break;
             };                
         };
         return ans;

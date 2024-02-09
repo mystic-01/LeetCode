edcdb5aa16f587beeb6a400e9faf10cd68d3e2dd
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-09 16:50:58 +0530

    Time: 155 ms (49.28%), Space: 138.4 MB (27.64%) - LeetHub

--- 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.cpp
+++ 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.cpp
@@ -3,12 +3,18 @@ public:
     int minIncrements(int n, vector<int>& nums) {
         int inc = 0;
         
-        
+        auto left = [](const int &i) {
+            return 2 * i + 1;    
+        }; 
+        auto right = [](const int &i) {
+            return 2 * i + 2;    
+        };         
+            
         for (auto i = n - 1; i >= 0; --i) {
             int diffBetweenChildren = 0;
-            if (2 * i + 1 < n && 2 * i + 2 < n) {
-                diffBetweenChildren = abs(nums[2 * i + 1] - nums[2 * i + 2]);                    
-                nums[i] += max(nums[2 * i + 1], nums[2 * i + 2]);                    
+            if (left(i) < n && right(i) < n) {
+                diffBetweenChildren = abs(nums[left(i)] - nums[right(i)]);                    
+                nums[i] += max(nums[left(i)], nums[right(i)]);                    
                 inc += diffBetweenChildren;
             };
         };
@@ -16,13 +22,3 @@ public:
         return inc;
     };
 };
-
-//               764
-//             /    \
-//         1460      2664
-//        /   \        /   \
-//     764    2725     4556         5305
-//    /  \     /  \      /  \      /    \
-// 8829  5064 5929 7660 6321 4830  7055 3761
-                  
-//                   2664 - 1460 + (3 * 5305) - 4556 - 2725 - 764 + (7 * 8829) - 5064 - 5929 - 7660 - 6321 - 4830 - 7055 - 3761

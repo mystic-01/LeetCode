db058241a5baf598fcbc6c7a2f0912d0c4ff1387
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-10 18:29:23 +0530

    Time: 4 ms (76.20%), Space: 7.5 MB (69.49%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n = s.size(), count = 0, dp[n + 1], prev[n + 1];
-        fill(dp, dp + n + 1, 0);
-        fill(prev, prev + n + 1, 0);
+        int n = s.size(), count = 0;
         
-        for (int start = n - 1; start >= 0; --start) {
-            for (int end = start; end < n; ++end) {
-                if (start == end) {
-                    dp[end] = 1;
-                } else if (end == start + 1) {
-                    dp[end] = (s[start] == s[end]);                    
-                } else {
-                    dp[end] = (s[start] == s[end]) ? prev[end - 1] : 0;                                            
-                };
-                count += dp[end];
+        for (int mid = 0; mid < n; ++mid) {
+            int l = mid, r = mid;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                ++count, --l, ++r;    
             };
-            for (int i = 0; i < n + 1; ++i) {
-                prev[i] = dp[i];
-            };
+            l = mid, r = mid + 1;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                ++count, --l, ++r;    
+            };            
         };
 
         return count;           

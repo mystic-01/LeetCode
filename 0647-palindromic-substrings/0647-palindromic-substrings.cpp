0bc6067ac4800c8fc34fd04e06969fb0ee2e9856
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-10 18:15:50 +0530

    Time: 13 ms (38.39%), Space: 7.7 MB (67.60%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -2,25 +2,22 @@ class Solution {
 public:
     int countSubstrings(string s) {
         int n = s.size(), count = 0;
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        
+        vector<int> dp(n + 1), prev(n + 1);
+
         for (int start = n - 1; start >= 0; --start) {
             for (int end = start; end < n; ++end) {
                 if (start == end) {
-                    dp[start][end] = 1;
+                    dp[end] = 1;
                 } else if (end == start + 1) {
-                    dp[start][end] = (s[start] == s[end]);                    
+                    dp[end] = (s[start] == s[end]);                    
                 } else {
-                    dp[start][end] = (s[start] == s[end]) ? dp[start + 1][end - 1] : 0;                                            
+                    dp[end] = (s[start] == s[end]) ? prev[end - 1] : 0;                                            
                 };
-                count += dp[start][end];
+                count += dp[end];
             };
+            prev = dp;
         };
 
         return count;           
     };
 };
-
-
-// aaabaa
-// 6 + 3 + 2 + 0 + 1

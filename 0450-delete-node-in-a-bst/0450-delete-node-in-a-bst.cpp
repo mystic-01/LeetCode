84cbdd0a0aba9f67ee0c07259ec8fbd93809fd08
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-10 23:06:43 +0530

    Time: 33 ms (16.31%), Space: 32 MB (98.96%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -22,64 +22,52 @@ public:
         return !root->left && !root->right;
     };
     
-    bool findAndDeleteKey(TreeNode *root, int &key) {
+    bool deleteKey(TreeNode *root, TreeNode *keyNode, bool isKeyNodeALeftNode) {
+        if (isLeafNode(keyNode)) {
+            if (isKeyNodeALeftNode) {
+                root->left = nullptr;
+            } else {
+                root->right = nullptr;                
+            };
+        } else if (!keyNode->left || !keyNode->right) {
+            if (isKeyNodeALeftNode) {
+                root->left = keyNode->right ? keyNode->right : keyNode->left;
+            } else {
+                root->right = keyNode->right ? keyNode->right : keyNode->left;
+            };
+        } else {
+            TreeNode *leftMost = findLeftMost(keyNode->right);
+            leftMost->left = keyNode->left;
+            if (isKeyNodeALeftNode) {
+                root->left = keyNode->right;
+            } else {
+                root->right = keyNode->right;                
+            };
+        };                                
+        return true;                
+    };
+    
+    bool findKey(TreeNode *root, int &key) {
         if (!root) {
             return false;
         };
-        if (root->left) {
-            if (root->left->val == key) {
-                TreeNode *keyNode = root->left; 
-                if (isLeafNode(keyNode)) {
-                    root->left = nullptr;
-                } else if (!keyNode->left) {
-                    root->left = keyNode->right;
-                } else if (!keyNode->right) {
-                    root->left = keyNode->left;
-                } else {
-                    TreeNode *leftMost = findLeftMost(keyNode->right);
-                    leftMost->left = keyNode->left;
-                    root->left = keyNode->right;
-                };                                
-                return true;
-            } else {
-                if (findAndDeleteKey(root->left, key)) {
-                    return true;                        
-                };
-            };
+        if (root->left && root->left->val == key) {
+            return deleteKey(root, root->left, true);
         };
-        if (root->right) {
-            if (root->right->val == key) {
-                TreeNode *keyNode = root->right; 
-                if (isLeafNode(keyNode)) {
-                    root->right = nullptr;
-                } else if (!keyNode->right) {
-                    root->right = keyNode->left;
-                } else if (!keyNode->left) {
-                    root->right = keyNode->right;
-                } else {
-                    TreeNode *leftMost = findLeftMost(keyNode->right);
-                    leftMost->left = keyNode->left;
-                    root->right = keyNode->right;
-                };                                
-                return true;
-            } else {
-                if (findAndDeleteKey(root->right, key)) {
-                    return true;    
-                };
-            };
+        if (root->right && root->right->val == key) {
+            return deleteKey(root, root->right, false);
         };        
-        return false;
+        return findKey(root->left, key) || findKey(root->right, key);
     };
     
     
     TreeNode* deleteNode(TreeNode* root, int key) {
-        TreeNode* dummy = new TreeNode(-1e9);
-        dummy->left = root;
-        findAndDeleteKey(dummy, key);
+        TreeNode* dummy = new TreeNode(-1e9, root, nullptr);
+        findKey(dummy, key);
         return dummy->left;
         // when found key, 
         // 1) Find leftmostNode in key's right = leftMost
-        // 2) prev->left = key->right
-        // 3) leftMost->left = key->left
+        // 2) leftMost->left = key->left
+        // 3) prev->left = key->right
     };
 };

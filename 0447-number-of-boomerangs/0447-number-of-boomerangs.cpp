5f35f28a832db800cc446f4fd2e1259504d181e0
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-11 15:26:02 +0530

    Time: 316 ms (85.26%), Space: 127.6 MB (52.21%) - LeetHub

--- 0447-number-of-boomerangs/0447-number-of-boomerangs.cpp
+++ 0447-number-of-boomerangs/0447-number-of-boomerangs.cpp
@@ -4,13 +4,11 @@ public:
     int numberOfBoomerangs(vector<vector<int>>& points) {
         int n = points.size(), boomerangs = 0;
         for (int i = 0; i < n; ++i) {
-            map<int, int> m;
+            unordered_map<int, int> m;
             for (int j = 0; j < n; ++j) {
                 if (i != j) {
                     int dist = pow(abs(points[i][0] - points[j][0]), 2) + pow(abs(points[i][1] - points[j][1]), 2);
-                    if (++m[dist] >= 2) {
-                        boomerangs += ((m[dist] - 1) * 2);
-                    }; 
+                    boomerangs += ((++m[dist] - 1) * 2);
                 };
             };        
         };

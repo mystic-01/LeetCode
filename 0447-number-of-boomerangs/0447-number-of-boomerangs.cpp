f9818f0bc622076f9499887709ccb0e5a8c91ba5
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-11 15:11:59 +0530

    Time: 480 ms (34.53%), Space: 149.2 MB (30.95%) - LeetHub

---
+++ 0447-number-of-boomerangs/0447-number-of-boomerangs.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    int numberOfBoomerangs(vector<vector<int>>& points) {
+        int n = points.size(), boomerangs = 0;
+        int combinatorics[n + 1];
+        
+        combinatorics[0] = combinatorics[1] = 0;
+        for (int i = 2; i < n; ++i) {
+            combinatorics[i] = i * (i - 1);
+        };
+        
+        for (int i = 0; i < n; ++i) {
+            map<int, int> m;
+            for (int j = 0; j < n; ++j) {
+                if (i != j) {
+                    int dist = pow(abs(points[i][0] - points[j][0]), 2) + pow(abs(points[i][1] - points[j][1]), 2);
+                    int count = ++m[dist];
+                    if (count >= 2) {
+                        boomerangs -= combinatorics[count - 1];
+                        boomerangs += combinatorics[count];
+                    }; 
+                };
+            };        
+        };
+        return boomerangs;
+    };
+};
+                    // 1 -> (1!/2! * -1!) * 2
+                    // 2 -> (1,2) (2,1) = 1 * 2 = (2!/2! * 0!) * 2
+                    // 3 -> (1,2) (1,3) (2,1) (2,3) (3,1) (3,2) = 3 * 2 = (3!/2! * 1!) * 2
+                    // 4 -> (1,2) (1,3) (1,4) (2,1) (2,3) (2,4) (3,1) (3,2) (3,4) (4,1) (4,2) (4,3) = 6 * 2 = (4!/2! * 2!) * 2
+// 3C2 = 3, 4C2 = 6, 5C2 = 10, 6C2 = 15, 7C2 = 
+// 0 2 4  6  6  4  2 0
+// 0 2 6 12 18 22 24

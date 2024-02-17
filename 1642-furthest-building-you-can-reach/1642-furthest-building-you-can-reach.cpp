5852617422dce2eaffb7ede706ce7fda324c2079
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-17 13:18:26 +0530

    Time: 84 ms (38.71%), Space: 57.2 MB (40.68%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -4,9 +4,8 @@ public:
         priority_queue<int, vector<int>, greater<int>> pq;
         int n = heights.size(), i = 0;
         while (i + 1 < n) {
-            if (heights[i] < heights[i + 1]) {
-                int diff = heights[i + 1] - heights[i];                
-                pq.push(diff);                        
+            if (heights[i] < heights[i + 1]) {                
+                pq.push(heights[i + 1] - heights[i]);                        
                 if (pq.size() > ladders) {
                     if (bricks >= pq.top()) {
                         bricks -= pq.top();

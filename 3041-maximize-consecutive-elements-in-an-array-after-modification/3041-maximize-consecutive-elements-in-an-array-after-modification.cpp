d688930f883386c1dc157c585ec0cc21b27a1196
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-02-20 22:26:30 +0530

    Time: 317 ms (57.18%), Space: 137.7 MB (41.46%) - LeetHub

--- 3041-maximize-consecutive-elements-in-an-array-after-modification/3041-maximize-consecutive-elements-in-an-array-after-modification.cpp
+++ 3041-maximize-consecutive-elements-in-an-array-after-modification/3041-maximize-consecutive-elements-in-an-array-after-modification.cpp
@@ -3,7 +3,7 @@ public:
     int maxSelectedElements(vector<int>& nums) {
         int maxLen = 0;
         sort(begin(nums), end(nums));
-        map<int, int> m;
+        unordered_map<int, int> m;
         for (int &x : nums) {
             m[x + 1] = m[x] + 1;
             m[x] = m[x - 1] + 1;

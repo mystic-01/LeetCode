b56645e977bb6ddb693c71fd1b93ec5baf62bf07
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-06 17:12:39 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (57.96%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size(), left = 0, right = 0, curr = INT_MIN, count = 0;
+        for (; right < n; ++right) {
+            if (curr == INT_MIN || curr != nums[right]) {
+                curr = nums[right];
+                count = 1;
+            } else {
+                ++count;              
+            };
+            if (count <= 2) {
+                nums[left++] = curr;                        
+            };              
+        };
+        return left;
+    };
+};

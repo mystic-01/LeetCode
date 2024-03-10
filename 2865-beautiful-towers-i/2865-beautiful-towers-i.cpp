6622b24e4ff3e7173b530ebfbd4b99e8c3f7b35f
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-10 23:09:20 +0530

    Time: 16 ms (77.89%), Space: 29.1 MB (30.58%) - LeetHub

--- 2865-beautiful-towers-i/2865-beautiful-towers-i.cpp
+++ 2865-beautiful-towers-i/2865-beautiful-towers-i.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    long long maximumSumOfHeights(vector<int>& nums) {
-        int n = nums.size(), limit = 0;
-        long long totalSumOfHeights = 0, currSum = 0;
+    void fillCost(int &n, vector<int> &nums, long long *leftCost) {
+        stack<int> st;
         for (int i = 0; i < n; ++i) {
-            currSum = nums[i];
-            limit = nums[i];
-            for (int j = i - 1; j >= 0; --j) {
-                limit = min(limit, nums[j]);
-                currSum += limit;
-            };
-            limit = nums[i];
-            for (int j = i + 1; j < n; ++j) {
-                limit = min(limit, nums[j]);
-                currSum += limit;
-            };
-            totalSumOfHeights = max(totalSumOfHeights, currSum);
-        };
-        return totalSumOfHeights;
+            leftCost[i] = (i - 1 >= 0) ? leftCost[i - 1] : 0;
+            while (!st.empty() && nums[st.top()] > nums[i]) {
+                int currIdx = st.top();
+                st.pop();
+                int nextIdx = st.empty() ? -1 : st.top();
+                leftCost[i] += ((currIdx - nextIdx) * (long long)(nums[currIdx] - nums[i]));
+            };    
+            st.push(i);
+        };        
+    };
+    
+    long long maximumSumOfHeights(vector<int> &nums) {
+        int n = nums.size();
+        long long sum = accumulate(begin(nums), end(nums), 0LL);
+        long long leftCost[n], rightCost[n];
+        fill(leftCost, leftCost + n, 0);
+        fill(rightCost, rightCost + n, 0);
+
+        fillCost(n, nums, leftCost);
+        
+        reverse(begin(nums), end(nums));
+        fillCost(n, nums, rightCost);    
+        reverse(begin(nums), end(nums));
+
+        reverse(rightCost, rightCost + n);
+
+        long long minCost = LLONG_MAX;
+        for (int i = 0; i < n; ++i) {
+            minCost = min(minCost, leftCost[i] + rightCost[i]);
+        };        
+        return sum - minCost;
     };
 };

0d1b1af1c0744c847293f4fe2d4646db0aa7e51e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-10 23:06:15 +0530

    Time: 117 ms (89.82%), Space: 99.4 MB (57.10%) - LeetHub

---
+++ 2866-beautiful-towers-ii/2866-beautiful-towers-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void fillCost(int &n, vector<int> &nums, vector<long long> &leftCost) {
+        stack<int> st;
+        for (int i = 0; i < n; ++i) {
+            leftCost[i] = (i - 1 >= 0) ? leftCost[i - 1] : 0;
+            while (!st.empty() && nums[st.top()] > nums[i]) {
+                int currIdx = st.top();
+                st.pop();
+                int nextIdx = st.empty() ? -1 : st.top();
+                leftCost[i] += ((currIdx - nextIdx) * (long long)(nums[currIdx] - nums[i]));
+            };    
+            st.push(i);
+        };        
+    };
+    
+    long long maximumSumOfHeights(vector<int> &nums) {
+        int n = nums.size();
+        long long sum = accumulate(begin(nums), end(nums), 0LL);
+        vector<long long> leftCost(n), rightCost(n);
+        fillCost(n, nums, leftCost);
+        
+        reverse(begin(nums), end(nums));
+        fillCost(n, nums, rightCost);    
+        reverse(begin(rightCost), end(rightCost));
+        reverse(begin(nums), end(nums));
+
+        long long minCost = LLONG_MAX;
+        // for (int i = 0; i < n; ++i) {
+        //     cout << nums[i] << " ";
+        // };        
+        // cout << "\n";        
+        // for (int i = 0; i < n; ++i) {
+        //     cout << leftCost[i] << " ";
+        // };    
+        // cout << "\n";
+        // for (int i = 0; i < n; ++i) {
+        //     cout << rightCost[i] << " ";
+        // };
+        // cout << "\n";
+        // cout << sum << "\n";        
+        for (int i = 0; i < n; ++i) {
+            minCost = min(minCost, leftCost[i] + rightCost[i]);
+        };        
+        return sum - minCost;
+    };
+};

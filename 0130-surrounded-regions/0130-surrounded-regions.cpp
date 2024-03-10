5c2b2c13743a81846d9ebc22cca491fb90bb81c2
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-10 23:56:23 +0530

    Time: 58 ms (7.26%), Space: 20.9 MB (8.34%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -7,33 +7,33 @@ public:
     
     void solve(vector<vector<char>>& board) {
         int row = board.size(), col = board[0].size();
-        vector<vector<bool>> visited(row, vector<bool>(col, false));
+        queue<pair<int, int>> q;        
+        set<pair<int, int>> visited;        
         for (int i = 0; i < row; ++i) {
             for (int j = 0; j < col; ++j) {
-                if (board[i][j] == 'O' && !visited[i][j]) {
+                if (board[i][j] == 'O' && !visited.count({i, j})) {
                     bool isQueueTainted = false;
-                    queue<pair<int, int>> q;        
-                    set<pair<int, int>> s;        
+                    set<pair<int, int>> currPairs;        
                     q.push({i, j});
-                    visited[i][j] = true;
+                    visited.insert({i, j});
                     
                     while (!q.empty()) {
                         int x = q.front().first, y = q.front().second;
                         isQueueTainted = isQueueTainted || isBoundaryElement(x, y, row, col);
                         q.pop();
                         if (!isQueueTainted) {
-                            s.insert({x, y});
+                            currPairs.insert({x, y});
                         };
                         for (int k = 0; k < 4; ++k) {
                             int a = x + offsets[k][0], b = y + offsets[k][1];
-                            if (a >= 0 && a < row && b >= 0 && b < col && board[a][b] == 'O' && !visited[a][b]) {      
+                            if (a >= 0 && a < row && b >= 0 && b < col && board[a][b] == 'O' && !visited.count({a, b})) {      
                                 q.push({a, b});
-                                visited[a][b] = true;
+                                visited.insert({a, b});
                             };
                         };        
                     };
                     if (!isQueueTainted) {
-                        for (auto &p : s) {
+                        for (auto &p : currPairs) {
                             board[p.first][p.second] = 'X';    
                         };
                     };                    

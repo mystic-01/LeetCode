5c8a785bbc4f0a567d670467f151b38a718a52a9
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-14 12:20:58 +0530

    Time: 17 ms (91.69%), Space: 31.1 MB (85.39%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int n = nums.size(), sum = 0, count = 0;
-        unordered_map<int, int> m;
-        m[0] = 1;
-        for (int i = 0; i < n; ++i) {
+    
+    int subarrayWithSumLessThanEqualToK(vector<int>& nums, int k) {
+        int n = nums.size(), left = 0, sum = 0, count = 0;
+        if (k < 0) return 0;
+        
+        for (auto i = 0; i < n; i++) {
             sum += nums[i];
-            if (m.find(sum - goal) != m.end()) {
-                count += m[sum - goal];     
-            };
-            ++m[sum];
+            while (sum > k && left < n) sum -= nums[left++];
+            count += (i - left + 1);
         };
         return count;
+    };
+    
+    int numSubarraysWithSum(vector<int>& nums, int k) {
+        return subarrayWithSumLessThanEqualToK(nums, k) - subarrayWithSumLessThanEqualToK(nums, k - 1);
     };
 };

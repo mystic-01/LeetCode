22ce0b69a1b1f3a4b48ba673083913e75cad8d1a
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-14 12:49:16 +0530

    Time: 167 ms (28.76%), Space: 76.2 MB (31.59%) - LeetHub

--- 0870-advantage-shuffle/0870-advantage-shuffle.cpp
+++ 0870-advantage-shuffle/0870-advantage-shuffle.cpp
@@ -3,17 +3,14 @@ public:
     vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         multiset<int> ms(begin(nums1), end(nums1));
-        fill(begin(nums1), end(nums1), -1);
         
         for (int i = 0; i < n; ++i) {
             auto smallestGreater = ms.upper_bound(nums2[i]);
-            if (smallestGreater != ms.end())  {
-                nums1[i] = *smallestGreater;
-            } else {                    
-                nums1[i] = *begin(ms);
+            if (smallestGreater == ms.end())  {
+                smallestGreater = begin(ms);
             };
-            auto toBeErased = ms.find(nums1[i]);
-            ms.erase(toBeErased);
+            nums1[i] = *smallestGreater;
+            ms.erase(smallestGreater);
         };
         return nums1;
     };

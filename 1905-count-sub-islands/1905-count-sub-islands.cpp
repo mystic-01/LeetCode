f48d9a4350b87e76e20dc144b1b4e6858e87bd15
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-16 19:34:48 +0530

    Time: 311 ms (26.81%), Space: 240.4 MB (7.07%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    const int offsets[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        int row = grid1.size(), col = grid1[0].size(), validIslands = 0;
+        
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (grid2[i][j]) {
+                    queue<pair<int, int>> q;
+                    vector<pair<int, int>> visited;
+                    q.push({i, j});
+                    visited.push_back({i, j});
+                    grid2[i][j] = 0;
+                    
+                    while (!q.empty()) {
+                        int x = q.front().first, y = q.front().second;
+                        q.pop();
+                        for (int k = 0; k < 4; ++k) {
+                            int a = x + offsets[k][0], b = y + offsets[k][1];
+                            if (a >= 0 && a < row && b >= 0 && b < col && grid2[a][b]) {
+                                q.push({a, b});
+                                visited.push_back({a, b});
+                                grid2[a][b] = 0;                                    
+                            };
+                        };        
+                    };
+                    
+                    bool isSubIsland = true;
+                    for (auto &[first, second] : visited) {
+                        if (!grid1[first][second]) {
+                            isSubIsland = false;
+                            break;
+                        };    
+                    };
+                    validIslands += isSubIsland;
+                };
+            };            
+        };
+        
+        return validIslands;
+    };
+};

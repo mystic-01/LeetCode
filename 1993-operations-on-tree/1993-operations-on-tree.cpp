9aae253043fc8973cce05211ec050813009d8d8e
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-17 18:37:08 +0530

    Time: 577 ms (16.20%), Space: 433.9 MB (5.63%) - LeetHub

---
+++ 1993-operations-on-tree/1993-operations-on-tree.cpp
@@ -0,0 +1,53 @@
+class LockingTree {
+private:
+    int n = 0;
+    vector<int> parent;
+    vector<int> isLocked;
+public:
+    LockingTree(vector<int> &_parent) {
+        parent = _parent;
+        n = parent.size();
+        isLocked.resize(n, 0);
+    };
+    
+    bool lock(int num, int user) {
+        if (isLocked[num]) {
+            return false;
+        };
+        isLocked[num] = user;
+        return true;
+    };
+    
+    bool unlock(int num, int user) {
+        if (isLocked[num] == user) {
+            isLocked[num] = 0;
+            return true;        
+        };
+        return false;
+    };
+    
+    bool upgrade(int num, int user) {
+        int val = num;
+        while (val != -1) {
+            if (isLocked[val]) {
+                return false;
+            };
+            val = parent[val];
+        };
+        bool unlockedAnyNode = false;
+        vector<int> masterParent = parent;
+        for (int i = 0; i < n; ++i) {
+            while (masterParent[i] != -1 && masterParent[i] != num) {
+                masterParent[i] = masterParent[masterParent[i]];
+            };
+            if (masterParent[i] == num && isLocked[i]) {
+                unlockedAnyNode = true;
+                isLocked[i] = 0;                    
+            };
+        };
+        if (unlockedAnyNode) {
+            isLocked[num] = user;
+        };
+        return unlockedAnyNode;
+    };
+};

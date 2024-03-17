c305aa95d03169e4b93faa5c6749a5ddda1ac045
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-17 17:53:15 +0530

    Time: 108 ms (74.22%), Space: 63.1 MB (86.76%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        int row = land.size(), col = land[0].size();
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (land[i][j]) {
+                    vector<int> curr = {i, j, 0, 0};
+                    for (int k = i; k < row && land[k][j]; ++k) {
+                        for (int l = j; l < col && land[k][l]; ++l) {
+                            land[k][l] = 0;
+                            curr[2] = max(curr[2], k);
+                            curr[3] = max(curr[3], l);
+                        };                    
+                    };
+                    ans.push_back(curr);
+                };
+            };
+        };
+        return ans;
+    };
+};
+
+/*
+
+[[0]
+,[0]
+,[1]
+,[1]
+,[1]
+,[0]
+,[1]
+,[1]]
+
+*/

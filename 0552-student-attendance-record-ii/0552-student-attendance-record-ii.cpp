3f2f3873174b092a50ddb77e469cb93036c8db09
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-24 02:39:36 +0530

    Time: 19 ms (88.55%), Space: 7.1 MB (96.66%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -3,30 +3,36 @@ static const int mod = 1e9 + 7;
 class Solution {
 public:
     int checkRecord(int n) {
-        int dp[n + 1][2][3];
+        int dp[2][3], prev[2][3];
         memset(dp, 0, sizeof(dp));
+        memset(prev, 0, sizeof(prev));
 
         for (int aLeft = 0; aLeft <= 1; ++aLeft) {
             for (int l = 2; l >= 0; --l) {
-                dp[n][aLeft][l] = 1;
+                prev[aLeft][l] = 1;
             };
         };
         
         for (int idx = n - 1; idx >= 0; --idx) {
             for (int aLeft = 0; aLeft <= 1; ++aLeft) {
                 for (int l = 2; l >= 0; --l) {
-                    int take = dp[idx + 1][aLeft][0];
+                    int take = prev[aLeft][0];
                     if (aLeft) {
-                        take = (take + (long long)dp[idx + 1][0][0]) % mod;
+                        take = (take + (long long)prev[0][0]) % mod;
                     };
                     if (l < 2) {
-                        take = (take + (long long)dp[idx + 1][aLeft][l + 1]) % mod;
+                        take = (take + (long long)prev[aLeft][l + 1]) % mod;
                     };
-                    dp[idx][aLeft][l] = take;
+                    dp[aLeft][l] = take;
                 };
             };
+            for (int aLeft = 0; aLeft <= 1; ++aLeft) {
+                for (int l = 2; l >= 0; --l) {
+                    prev[aLeft][l] = dp[aLeft][l];
+                };
+            };            
         };
         
-        return dp[0][1][0];
+        return prev[1][0];
     };
 };

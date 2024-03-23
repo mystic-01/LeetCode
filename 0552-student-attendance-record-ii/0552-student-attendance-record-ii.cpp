a3f4c13fe5c19c7c5fecb9755d7fdcca11198161
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-24 02:36:22 +0530

    Time: 28 ms (85.53%), Space: 9.8 MB (78.06%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -2,31 +2,31 @@ static const int mod = 1e9 + 7;
 
 class Solution {
 public:
-    int recurse(int idx, int aLeft, int l, int &n, int (*dp)[2][3]) {
-        if (idx == n) {
-            return 1;
-        };
-        if (dp[idx][aLeft][l] != -1) {
-            return dp[idx][aLeft][l];    
-        };
-        int takeP = 0, takeA = 0, takeL = 0;
-        takeP = recurse(idx + 1, aLeft, 0, n, dp) % mod;
-        if (aLeft) {
-            takeA = recurse(idx + 1, 0, 0, n, dp) % mod;
-        };
-        if (l < 2) {
-            takeL = recurse(idx + 1, aLeft, l + 1, n, dp) % mod;            
-        };
-        
-        takeP = (takeP + (long long)takeA) % mod;
-        takeP = (takeP + (long long)takeL) % mod; 
-        
-        return dp[idx][aLeft][l] = takeP;
-    };
-    
     int checkRecord(int n) {
-        int dp[n][2][3];
-        memset(dp, -1, sizeof(dp));
-        return recurse(0, 1, 0, n, dp);
+        int dp[n + 1][2][3];
+        memset(dp, 0, sizeof(dp));
+
+        for (int aLeft = 0; aLeft <= 1; ++aLeft) {
+            for (int l = 2; l >= 0; --l) {
+                dp[n][aLeft][l] = 1;
+            };
+        };
+        
+        for (int idx = n - 1; idx >= 0; --idx) {
+            for (int aLeft = 0; aLeft <= 1; ++aLeft) {
+                for (int l = 2; l >= 0; --l) {
+                    int take = dp[idx + 1][aLeft][0];
+                    if (aLeft) {
+                        take = (take + (long long)dp[idx + 1][0][0]) % mod;
+                    };
+                    if (l < 2) {
+                        take = (take + (long long)dp[idx + 1][aLeft][l + 1]) % mod;
+                    };
+                    dp[idx][aLeft][l] = take;
+                };
+            };
+        };
+        
+        return dp[0][1][0];
     };
 };

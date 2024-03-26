3d8403670b67a7fd89d619d3c011df918d709e7a
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-26 21:13:14 +0530

    Time: 120 ms (69.23%), Space: 108.3 MB (79.12%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int val) {
         int n = nums.size(), ans = 0;
-        unordered_map<int, int> m;
+        vector<int> v(val, 0);
         for (int i = 0; i < n; ++i) {
             nums[i] %= val;
             if (nums[i] < 0) {
                 nums[i] += val;            
             };
-            ++m[nums[i]];
+            ++v[nums[i]];
         };
         for (int i = 0; i <= n; ++i) {
-            if (m[i % val]-- == 0) {
+            if (v[i % val]-- == 0) {
                 return i;
             };
         };

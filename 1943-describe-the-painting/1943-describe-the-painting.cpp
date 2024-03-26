8ffb18b26465c2ed1720b880c0796a062f7fecc6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-26 20:37:09 +0530

    Time: 261 ms (80.30%), Space: 154.3 MB (20.45%) - LeetHub

---
+++ 1943-describe-the-painting/1943-describe-the-painting.cpp
@@ -0,0 +1,41 @@
+static const int maxSize = 1e5 + 1;
+class Solution {
+public:
+    vector<vector<long long>> splitPainting(vector<vector<int>>& nums) {
+        int n = nums.size();
+        vector<vector<long long>> ans;
+        vector<long long> line(maxSize, 0);
+        vector<bool> isEnd(maxSize, 0);
+        
+        long long start = 0, end = 0, val = 0; 
+        for (int i = 0; i < n; ++i) {
+            start = nums[i][0], end = nums[i][1], val = nums[i][2];
+            
+            line[start] += val, line[end] -= val, isEnd[end] = true;
+        };
+        start = 1, end = 1, val = line[1];
+        for (int i = 1; i < maxSize; ++i) {
+            line[i] += line[i - 1];
+            if (val > 0) {    
+                if (line[i] == val) {
+                    if (isEnd[i]) {
+                        ans.push_back({start, i, val});                    
+                        start = i, end = i, val = line[i];
+                    };
+                    end = i;                    
+                } else {
+                    ans.push_back({start, end + 1, val});
+                    start = i, end = i, val = line[i];
+                };
+            } else {
+                start = i, end = i, val = line[i];
+            };
+        };
+        return ans;
+    };
+};
+
+// 1  2  3  4  5  6  7  8  9  10
+// 12       -12
+//          12      -12
+// 12 12 12 12 12 12 12  

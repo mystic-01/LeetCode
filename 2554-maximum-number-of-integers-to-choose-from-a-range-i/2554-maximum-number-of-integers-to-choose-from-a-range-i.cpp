ac8527f95e9ca09536b39c10a31fe522ae4f9d79
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-26 20:55:28 +0530

    Time: 153 ms (90.22%), Space: 117.2 MB (87.15%) - LeetHub

--- 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i/2554-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     int maxCount(vector<int>& nums, int n, int maxSum) {
-        unordered_set<int> s(begin(nums), end(nums));
-        int count = 0, sum = 0;
-        for (int i = 1; i <= n; ++i) {
-            if (sum + i <= maxSum && s.count(i) == 0) {
-                ++count, sum += i;    
+        sort(begin(nums), end(nums));
+        int nSize = nums.size(), cd = 1, count = 0, sum = 0;
+        for (int i = 0; i < nSize; ++i) {
+            while (cd < nums[i]) {
+                if (sum + cd <= maxSum && cd <= n) {
+                    sum += cd, ++count, ++cd;    
+                } else {
+                    return count;
+                };                
             };
+            cd = nums[i] + 1;
         };
+        while (cd < INT_MAX) {
+            if (sum + cd <= maxSum && cd <= n) {
+                sum += cd, ++count, ++cd;    
+            } else {
+                return count;
+            };                
+        };        
         return count;
     };
 };

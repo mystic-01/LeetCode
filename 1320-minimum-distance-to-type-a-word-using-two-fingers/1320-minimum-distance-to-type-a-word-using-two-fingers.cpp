01bdf3d73fe1a5c126153ed7e3593c749934df53
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-26 23:51:46 +0530

    Time: 75 ms (34.01%), Space: 8.4 MB (79.19%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -7,26 +7,6 @@ public:
         return abs(dist[ch1 - 'A'].first - dist[ch2 - 'A'].first) + abs(dist[ch1 - 'A'].second - dist[ch2 - 'A'].second);         
     };
     
-    int recurse(int idx, char a, char b, pair<int, int> *dist, string &word, int (*dp)[27][27]) { 
-        if (idx == word.size()) {
-            return 0;    
-        };
-        if (dp[idx][a - 'A'][b - 'A'] != -1) {
-            return dp[idx][a - 'A'][b - 'A'];    
-        };
-        
-        int distOne = outOfBounds, distTwo = outOfBounds; 
-        
-        distOne = getDist(word[idx], a, dist) + recurse(idx + 1, word[idx], b, dist, word, dp);            
-        
-        if (b == invalidChar) {
-            distTwo = 0 + recurse(idx + 1, a, word[idx], dist, word, dp);
-        } else {
-            distTwo = getDist(word[idx], b, dist) + recurse(idx + 1, a, word[idx], dist, word, dp);            
-        };        
-        return dp[idx][a - 'A'][b - 'A'] = min(distOne, distTwo);
-    };
-    
     int minimumDistance(string word) {
         pair<int, int> dist[26];
         int n = word.size(), dp[n + 1][27][27];
@@ -34,6 +14,31 @@ public:
         for (int i = 0; i < 26; ++i) {
             dist[i] = {i / 6, i % 6};
         };
-        return recurse(1, word[0], invalidChar, dist, word, dp);
+        for (char a = invalidChar; a >= 'A'; --a) {
+            for (char b = invalidChar; b >= 'A'; --b) {
+                dp[n][a - 'A'][b - 'A'] = 0;
+            };
+        };
+        
+        for (int idx = n - 1; idx >= 1; --idx) {
+            for (char a = 'A'; a <= 'Z'; ++a) {
+                for (char b = 'A'; b <= invalidChar; ++b) {
+                    int distOne = outOfBounds, distTwo = outOfBounds; 
+                    int wordIdx = word[idx] - 'A', aIdx = a - 'A', bIdx = b - 'A';
+
+                    distOne = getDist(word[idx], a, dist) + dp[idx + 1][wordIdx][bIdx];
+
+                    if (b == invalidChar) {
+                        distTwo = 0 + dp[idx + 1][aIdx][wordIdx];
+                    } else {
+                        distTwo = getDist(word[idx], b, dist) + dp[idx + 1][aIdx][wordIdx];
+                    };        
+                    dp[idx][aIdx][bIdx] = min(distOne, distTwo);
+                };
+            };
+            
+        };
+        
+        return dp[1][word[0] - 'A'][invalidChar - 'A'];
     };
 };

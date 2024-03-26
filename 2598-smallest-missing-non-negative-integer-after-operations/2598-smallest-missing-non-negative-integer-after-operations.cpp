c10c14e75354ae92aef96efbde13ed466d7ead1d
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-26 21:13:45 +0530

    Time: 120 ms (69.23%), Space: 107.3 MB (100.00%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int findSmallestInteger(vector<int>& nums, int val) {
         int n = nums.size(), ans = 0;
-        vector<int> v(val, 0);
+        int v[val];
+        memset(v, 0, sizeof(v));
         for (int i = 0; i < n; ++i) {
             nums[i] %= val;
             if (nums[i] < 0) {

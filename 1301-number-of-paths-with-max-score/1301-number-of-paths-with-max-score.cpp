bfed4e3c1fd0d4bf576333219926116a0f0d08d4
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-27 18:02:06 +0530

    Time: 11 ms (55.44%), Space: 8.9 MB (99.48%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -1,34 +1,8 @@
 static const int outOfBounds = -1e5, mod = 1e9 + 7;
 static const pair<int, int> invalidPair = {-1, -1};
+
 class Solution {
 public:
-    pair<int, int> recurse(int i, int j, vector<string> &board, pair<int, int> (*dp)[101]) {
-        if (i == 0 && j == 0) {
-            return {0, 1};            
-        };        
-        if (dp[i][j] != invalidPair) {
-            return dp[i][j];    
-        };
-        pair<int, int> up = {outOfBounds, 0}, left = {outOfBounds, 0}, upLeft = {outOfBounds, 0};
-        if (i > 0 && board[i - 1][j] != 'X') {
-            up = recurse(i - 1, j, board, dp);
-        };
-        if (j > 0 && board[i][j - 1] != 'X') {
-            left = recurse(i, j - 1, board, dp);
-        };
-        if (i > 0 && j > 0 && board[i - 1][j - 1] != 'X') {
-            upLeft = recurse(i - 1, j - 1, board, dp);
-        };
-        
-        int val = board[i][j] - '0';
-        int maxVal = max({up.first, left.first, upLeft.first});
-        int maxCount = 0;
-        maxCount = (maxCount + (up.first == maxVal ? up.second : 0)) % mod;
-        maxCount = (maxCount + (left.first == maxVal ? left.second : 0)) % mod;
-        maxCount = (maxCount + (upLeft.first == maxVal ? upLeft.second : 0)) % mod;
-        return dp[i][j] = {maxVal + val, maxCount};
-    };
-    
     vector<int> pathsWithMaxScore(vector<string>& board) {
         int row = board.size(), col = board[0].size();
         board[row - 1][col - 1] = '0';
@@ -38,7 +12,34 @@ public:
                 dp[i][j] = {-1, -1};
             };        
         };
-        pair<int, int> ans = recurse(row - 1, col - 1, board, dp);
+        
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = {0, 1};            
+                    continue;
+                };        
+                pair<int, int> up = {outOfBounds, 0}, left = {outOfBounds, 0}, upLeft = {outOfBounds, 0};
+                if (i > 0 && board[i - 1][j] != 'X') {
+                    up = dp[i - 1][j];
+                };
+                if (j > 0 && board[i][j - 1] != 'X') {
+                    left = dp[i][j - 1];
+                };
+                if (i > 0 && j > 0 && board[i - 1][j - 1] != 'X') {
+                    upLeft = dp[i - 1][j - 1];
+                };
+
+                int val = board[i][j] - '0';
+                int maxCount = 0, maxVal = max({up.first, left.first, upLeft.first});
+                maxCount = (maxCount + (up.first == maxVal ? up.second : 0)) % mod;
+                maxCount = (maxCount + (left.first == maxVal ? left.second : 0)) % mod;
+                maxCount = (maxCount + (upLeft.first == maxVal ? upLeft.second : 0)) % mod;
+                dp[i][j] = {maxVal + val, maxCount};
+            };        
+        };
+        
+        pair<int, int> ans = dp[row - 1][col - 1];
         return {max(0, ans.first), ans.second};
     };
 };

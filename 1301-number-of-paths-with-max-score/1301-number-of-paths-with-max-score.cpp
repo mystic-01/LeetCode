7370bffe246c067c4853f07bd80403de15067318
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-27 18:06:58 +0530

    Time: 4 ms (95.34%), Space: 8.9 MB (99.48%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -6,28 +6,26 @@ public:
     vector<int> pathsWithMaxScore(vector<string>& board) {
         int row = board.size(), col = board[0].size();
         board[row - 1][col - 1] = '0';
-        pair<int, int> dp[101][101];
+        pair<int, int> dp[101], prev[101];
         for (int i = 0; i < 101; ++i) {
-            for (int j = 0; j < 101; ++j) {
-                dp[i][j] = {-1, -1};
-            };        
+            prev[i] = {-1, -1};
         };
         
         for (int i = 0; i < row; ++i) {
             for (int j = 0; j < col; ++j) {
                 if (i == 0 && j == 0) {
-                    dp[i][j] = {0, 1};            
+                    dp[j] = {0, 1};            
                     continue;
                 };        
                 pair<int, int> up = {outOfBounds, 0}, left = {outOfBounds, 0}, upLeft = {outOfBounds, 0};
                 if (i > 0 && board[i - 1][j] != 'X') {
-                    up = dp[i - 1][j];
+                    up = prev[j];
                 };
                 if (j > 0 && board[i][j - 1] != 'X') {
-                    left = dp[i][j - 1];
+                    left = dp[j - 1];
                 };
                 if (i > 0 && j > 0 && board[i - 1][j - 1] != 'X') {
-                    upLeft = dp[i - 1][j - 1];
+                    upLeft = prev[j - 1];
                 };
 
                 int val = board[i][j] - '0';
@@ -35,11 +33,14 @@ public:
                 maxCount = (maxCount + (up.first == maxVal ? up.second : 0)) % mod;
                 maxCount = (maxCount + (left.first == maxVal ? left.second : 0)) % mod;
                 maxCount = (maxCount + (upLeft.first == maxVal ? upLeft.second : 0)) % mod;
-                dp[i][j] = {maxVal + val, maxCount};
+                dp[j] = {maxVal + val, maxCount};
             };        
+            for (int j = 0; j < col; ++j) {
+                prev[j] = dp[j];
+            };            
         };
         
-        pair<int, int> ans = dp[row - 1][col - 1];
+        pair<int, int> ans = prev[col - 1];
         return {max(0, ans.first), ans.second};
     };
 };

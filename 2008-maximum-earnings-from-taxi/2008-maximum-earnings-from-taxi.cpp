850c42ae0d8773c48a54e1cfaaf979e53279ddf2
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-29 20:41:20 +0530

    Time: 315 ms (87.35%), Space: 100.2 MB (99.10%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
+    int binarySearch(int &start, int &sz, int &target, vector<vector<int>>& rides) {
+        int lo = start, hi = sz - 1;
+        while (lo <= hi) {
+            int mid = (hi - lo) / 2 + lo;
+            if (rides[mid][0] < target) {
+                lo = mid + 1;
+            } else if (rides[mid][0] > target) {
+                hi = mid - 1;                
+            } else {
+                return mid;
+            };
+        };
+        if (rides[hi][0] < target) {
+            ++hi;
+        };
+        return hi;
+    };
+    
     long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
         sort(begin(rides), end(rides));
-        
-        long long sz = rides.size(), maxProfit = 0;
-        multiset<int> ms;
-        long long maxTillHereMap[n + 1];
-        
+        int sz = rides.size();
+        long long maxProfit = 0, maxTillHereMap[n + 1];
+
         for (int i = sz - 1; i >= 0; --i) {
             int start = rides[i][0], ending = rides[i][1], tip = rides[i][2];
             long long curr = ending - start + tip;
         
-            auto it = ms.lower_bound(ending);
-            if (it != ms.end()) {
-                curr += maxTillHereMap[*it];
+            int idx = binarySearch(i, sz, ending, rides);
+            if (idx != sz) {
+                curr += maxTillHereMap[rides[idx][0]];
             };
             if (curr > maxProfit) {
                 maxProfit = curr;            
-                ms.insert(start);
-                maxTillHereMap[start] = maxProfit;
             };
+            maxTillHereMap[start] = maxProfit;
         };
         return maxProfit;
     };

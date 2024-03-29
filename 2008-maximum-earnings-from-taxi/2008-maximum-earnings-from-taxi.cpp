6651e5744ec77adfc7daa3087c777e7603695940
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-29 19:15:00 +0530

    Time: 477 ms (16.57%), Space: 139.2 MB (23.49%) - LeetHub

--- 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
+++ 2008-maximum-earnings-from-taxi/2008-maximum-earnings-from-taxi.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     long long maxTaxiEarnings(int n, vector<vector<int>>& rides) {
         sort(begin(rides), end(rides));
-        int sz = rides.size();
-        long long maxProfit = 0;
+        
+        long long sz = rides.size(), maxProfit = 0;
         multiset<int> ms;
         unordered_map<int, long long> maxTillHereMap;
-        
-        long long maxSoloProfitYet = 0;
         
         for (int i = sz - 1; i >= 0; --i) {
             int start = rides[i][0], ending = rides[i][1], tip = rides[i][2];
@@ -17,12 +15,10 @@ public:
             if (it != ms.end()) {
                 curr += maxTillHereMap[*it];
             };
-            maxSoloProfitYet = max(maxSoloProfitYet, curr);
-            
-            maxTillHereMap[start] = maxSoloProfitYet;
-            ms.insert(start);
-
             maxProfit = max(maxProfit, curr);
+            
+            maxTillHereMap[start] = maxProfit;
+            ms.insert(start);
         };
         return maxProfit;
     };

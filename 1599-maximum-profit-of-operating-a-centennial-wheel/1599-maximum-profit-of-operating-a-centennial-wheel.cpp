3c9b3e9840d1d9f92407007fdcc7081cf5c0769a
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-03-29 11:51:16 +0530

    Time: 88 ms (44.26%), Space: 83.9 MB (32.79%) - LeetHub

--- 1599-maximum-profit-of-operating-a-centennial-wheel/1599-maximum-profit-of-operating-a-centennial-wheel.cpp
+++ 1599-maximum-profit-of-operating-a-centennial-wheel/1599-maximum-profit-of-operating-a-centennial-wheel.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int minOperationsMaxProfit(vector<int>& nums, int boardingCost, int runningCost) {
-        int n = nums.size(), waiting = 0, onboard = 0;
+        int n = nums.size(), waiting = 0, onboard = 0, cost = 0;
         int rotations = 0, profit = 0, maxRotations = -1, maxProfit = 0;        
-        int lastSlot = 0;
         
         for (int i = 0; i < n; ++i) {
-            int addOnboard = 0, waitingOnboarded = 0;
-            addOnboard = min(4, nums[i]);                
+            int addOnboard = min(4, nums[i]), waitingOnboarded = 0;                
             if (addOnboard < 4 && waiting) {
-                waitingOnboarded = min(4 - (addOnboard), waiting);                    
+                waitingOnboarded = min(4 - addOnboard, waiting);                    
             };
             addOnboard += waitingOnboarded;
-            // cout << waitingOnboarded << "\n";
             waiting += nums[i] - addOnboard;
             onboard += addOnboard;            
-            ++rotations;                
-            profit = (onboard * boardingCost) - (rotations * runningCost);
-            // cout << addOnboard << " " << waiting << " " << profit << " " << onboard << " " << rotations << " - " << lastSlot << "\n";
-            // cout << addOnboard << " " << waiting << " " << profit << " " << maxProfit << " " << onboard << " " << rotations << " - " << lastSlot << "\n";
+            ++rotations, cost += runningCost;                
+            profit = (onboard * boardingCost) - cost;
             if (profit > maxProfit) {
                 maxProfit = profit, maxRotations = rotations;
             };
@@ -27,14 +22,12 @@ public:
             int addOnboard = min(waiting, 4);
             waiting -= addOnboard;
             onboard += addOnboard;            
-            ++rotations;                
-            profit = (onboard * boardingCost) - (rotations * runningCost);
-            // cout << addOnboard << " " << waiting << " " << profit << " " << onboard << " " << rotations << " - " << lastSlot << "\n";
+            ++rotations, cost += runningCost;                
+            profit = (onboard * boardingCost) - cost;
             if (profit > maxProfit) {
                 maxProfit = profit, maxRotations = rotations;
             };         
         };
-        // cout << "\n";
         return maxRotations; 
     };
 };

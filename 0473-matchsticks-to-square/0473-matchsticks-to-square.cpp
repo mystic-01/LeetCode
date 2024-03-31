020aa17f5aed12c8aabef185a5965d80583d7c25
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-01 01:56:25 +0530

    Time: 84 ms (79.10%), Space: 13.9 MB (18.70%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.cpp
@@ -0,0 +1,87 @@
+class Solution {
+public:
+    void recurse(int idx, int target, vector<int> &nums, set<vector<int>> &ans, vector<int> &ds) {
+        if (target == 0) {
+            ans.insert(ds);
+            return;
+        };
+        if (idx == nums.size()) {
+            return;
+        };     
+        if (target - nums[idx] >= 0) {
+            target -= nums[idx];
+            ds.push_back(nums[idx]);
+            recurse(idx + 1, target, nums, ans, ds);
+            ds.pop_back();
+            target += nums[idx];
+        };
+        recurse(idx + 1, target, nums, ans, ds);
+    };
+    
+    bool recurseAgain(int idx, int slotsLeft, unordered_map<int, int> &curr, unordered_map<int, int> &org, vector<vector<int>> &ans) {
+        if (idx == ans.size()) {
+            return slotsLeft == 0;            
+        };
+        
+        bool take = false, notTake = false;
+        bool wasted = false;
+        for (int &x : ans[idx]) {
+            if (++curr[x] > org[x]) {
+                wasted = true;
+            };                        
+        };        
+        if (!wasted) {
+            take = recurseAgain(idx, slotsLeft - 1, curr, org, ans);                
+        };
+        for (int &x : ans[idx]) {
+            --curr[x];
+        };
+        notTake = recurseAgain(idx + 1, slotsLeft, curr, org, ans);
+        return take || notTake;
+    };
+
+    
+    bool makesquare(vector<int>& nums) {
+        int n = nums.size(), totSum = accumulate(begin(nums), end(nums), 0), target = totSum / 4;
+        if (totSum % 4) {
+            return false;            
+        };
+        unordered_map<int, int> org, curr;
+        vector<int> ds;
+        set<vector<int>> ans;
+        vector<vector<int>> ansVec;
+        for (int i = 0; i < n; ++i) {
+            ++org[nums[i]];    
+        };
+        
+        sort(begin(nums), end(nums));
+        recurse(0, target, nums, ans, ds);
+
+        for (auto &vec : ans) {
+            ansVec.push_back(vec);
+        };
+        return recurseAgain(0, 4, curr, org, ansVec);
+    };
+};
+
+/*
+ for (auto &vec : ans) {
+            for (auto &s : vec) {
+                cout << s << " ";
+            };
+            cout << "\n";
+        };
+        cout << "\n";
+        cout << "\n";
+
+[13,11,1,8,6,7,8,8,6,7,8,9,8]
+
+
+[14,2,7,13,6,14,10,13,9,8,18,10,12,3,14]
+[14,11,19,17,7,3,14,9,16,20,15,17,14,18,6]
+[5969561,8742425,2513572,3352059,9084275,2194427,1017540,2324577,6810719,8936380,7868365,2755770,9954463,9912280,4713511]
+[1,2,3,4,5,6,7,8,9,10,5,4,3,2,1]
+[1,1,2,2,2]
+[3,3,3,3,4]
+
+*/

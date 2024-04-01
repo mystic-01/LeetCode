cee620c468b4779fbbb128bbd4ea06c0bfba87c6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-01 20:33:16 +0530

    Time: 1047 ms (46.03%), Space: 17.9 MB (67.55%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int minimizeTheDifference(vector<vector<int>>& nums, int target) {
-        int row = nums.size(), col = nums[0].size();
-        vector<int> dp(4901, -1), prev(4901, -1);
+        int row = nums.size(), col = nums[0].size(), maxSum = 0, maxVal = 0;
+        
+        for (auto &vec : nums) {
+            sort(begin(vec), end(vec), greater<int>());            
+        };
+        for (int i = row - 1; i >= 0; --i) {
+            maxSum += nums[i][0], maxVal = max(maxVal, nums[i][0]);
+        };
+        
+        vector<int> dp(maxSum + maxVal + 1, 0), prev(maxSum + maxVal + 1, 0);
 
-        for (int sum = 4900; sum >= 0; --sum) {
+        for (int sum = maxSum + maxVal; sum >= 0; --sum) {
             prev[sum] = abs(target - sum);
         };
         
         for (int i = row - 1; i >= 0; --i) {
-            for (int sum = 4830; sum >= 0; --sum) {
+            for (int sum = maxSum; sum >= 0; --sum) {
                 int closest = 1e8;
                 for (int k = 0; k < col; ++k) {
                     closest = min(closest, prev[sum + nums[i][k]]);

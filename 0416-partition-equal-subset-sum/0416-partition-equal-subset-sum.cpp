1519f540c8cf6c7f366af7035ff9334055503974
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-01 15:10:06 +0530

    Time: 92 ms (79.04%), Space: 13.8 MB (81.72%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int n = nums.size(), sum = accumulate(begin(nums), end(nums), 0), target = sum / 2;
-        if (sum % 2) {
+        int n = nums.size(), totSum = accumulate(begin(nums), end(nums), 0), target = totSum / 2;
+        if (totSum % 2) {
             return false;            
         };
         sort(begin(nums), end(nums), greater<int>());
-        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, false));
+        vector<int> dp(target + 1, false), prev(target + 1, false);
         
-        dp[n][target] = true;
+        prev[target] = true;
         
         for (int idx = n - 1; idx >= 0; --idx) {
             for (int sum = target; sum >= 0; --sum) {
                 if (sum + nums[idx] <= target) {
-                    if (dp[idx + 1][sum + nums[idx]]) {
-                        dp[idx][sum] = true;
+                    if (prev[sum + nums[idx]]) {
+                        dp[sum] = true;
                     };
                 };
-                dp[idx][sum] = dp[idx][sum] || dp[idx + 1][sum];
-            };            
+                dp[sum] = dp[sum] || prev[sum];
+            };     
+            prev = dp;
         };
-        return dp[0][0];
+        return prev[0];
     };
 };

17c1958fdc596a62cd5c127fd6e8f8307d8b91a0
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-01 13:42:43 +0530

    Time: 2524 ms (5.64%), Space: 11 MB (75.02%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    bool recurse(int idx, int k, int target, vector<int> &nums, int &originalTarget) {
+    bool recurse(int idx, int k, int &t, vector<int> &nums, int &target) {
         if (k == 0) {
             return true;
         };
-        if (target == 0) {
-            return recurse(0, k - 1, originalTarget, nums, originalTarget);            
+        if (t == 0) {
+            t = target;
+            if (recurse(0, k - 1, t, nums, target)) {
+                return true;
+            };            
+            t = 0;
+            return false;
         };
         if (idx == nums.size()) {
             return false;
         };
-        if (target - nums[idx] >= 0) {
+        if (t - nums[idx] >= 0) {
             int temp = nums[idx];
-            target -= temp;
+            t -= temp;
             nums[idx] = INT_MAX;
-            if (recurse(idx + 1, k, target, nums, originalTarget)) {
+            if (recurse(idx + 1, k, t, nums, target)) {
                 return true;
             };
             nums[idx] = temp;
-            target += temp;
+            t += temp;
         };
-        return recurse(idx + 1, k, target, nums, originalTarget);
+        return recurse(idx + 1, k, t, nums, target);
     };
     
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        int sum = accumulate(begin(nums), end(nums), 0), target = sum / k;
+        int sum = accumulate(begin(nums), end(nums), 0), target = sum / k, t = target;
         if (sum % k) {
             return false;
         };
         sort(begin(nums), end(nums), greater<int>());
-        return recurse(0, k, target, nums, target);
+        return recurse(0, k, t, nums, target);
     };
 };

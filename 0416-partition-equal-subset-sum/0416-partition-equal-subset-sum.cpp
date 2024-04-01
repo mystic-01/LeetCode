634695ce02eeb6ec8acf9ab3a1ac582db3247fa6
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-01 15:08:11 +0530

    Time: 128 ms (68.90%), Space: 181.9 MB (10.72%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    bool recurse(int idx, int sum, int &target, vector<int> &nums, vector<vector<int>> &dp) {
-        if (idx == nums.size()) {
-            return sum == target;            
-        };
-        if (dp[idx][sum] != -1) {
-            return dp[idx][sum];
-        };
-        if (sum + nums[idx] <= target) {
-            if (recurse(idx + 1, sum + nums[idx], target, nums, dp)) {
-                return dp[idx][sum] = true;
-            };
-        };
-        return dp[idx][sum] = recurse(idx + 1, sum, target, nums, dp);
-    };
-    
-    
     bool canPartition(vector<int>& nums) {
         int n = nums.size(), sum = accumulate(begin(nums), end(nums), 0), target = sum / 2;
         if (sum % 2) {
             return false;            
         };
         sort(begin(nums), end(nums), greater<int>());
-        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, -1));
-        return recurse(0, 0, target, nums, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, false));
+        
+        dp[n][target] = true;
+        
+        for (int idx = n - 1; idx >= 0; --idx) {
+            for (int sum = target; sum >= 0; --sum) {
+                if (sum + nums[idx] <= target) {
+                    if (dp[idx + 1][sum + nums[idx]]) {
+                        dp[idx][sum] = true;
+                    };
+                };
+                dp[idx][sum] = dp[idx][sum] || dp[idx + 1][sum];
+            };            
+        };
+        return dp[0][0];
     };
 };

21c4ce9427f9753eb1af75bc2cf53d4c3f5e96e3
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-01 20:29:18 +0530

    Time: 1637 ms (8.67%), Space: 19.1 MB (59.25%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int recurse(int i, int sum, int &row, int &col, vector<vector<int>> &nums, int &target, vector<vector<int>> &dp) {
-        if (i == row) {
-            return abs(target - sum);    
-        };     
-        if (dp[i][sum] != -1) {
-            return dp[i][sum];
-        };
-        int closest = 1e8;
-        for (int k = 0; k < col; ++k) {
-            // cout << i << " - " << abs(target - (sum + nums[i][k])) << " " << abs(target - sum) << "\n";
-            closest = min(closest, recurse(i + 1, sum + nums[i][k], row, col, nums, target, dp));
-        };
-        return dp[i][sum] = closest;
-    };
-    
     int minimizeTheDifference(vector<vector<int>>& nums, int target) {
         int row = nums.size(), col = nums[0].size();
-        vector<vector<int>> dp(row + 1, vector<int>(4901, -1));
-        int ans = recurse(0, 0, row, col, nums, target, dp);
-        return ans;
+        vector<int> dp(4901, -1), prev(4901, -1);
+
+        for (int sum = 4900; sum >= 0; --sum) {
+            prev[sum] = abs(target - sum);
+        };
+        
+        for (int i = row - 1; i >= 0; --i) {
+            for (int sum = 4830; sum >= 0; --sum) {
+                int closest = 1e8;
+                for (int k = 0; k < col; ++k) {
+                    closest = min(closest, prev[sum + nums[i][k]]);
+                };
+                dp[sum] = closest;                                                
+            };    
+            prev = dp;
+        };
+        return prev[0];
     };
 };

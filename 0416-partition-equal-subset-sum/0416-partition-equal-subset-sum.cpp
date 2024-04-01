0959fb6367e5cf876189109be8332ed36cfea954
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-01 15:03:26 +0530

    Time: 55 ms (89.48%), Space: 181.8 MB (11.29%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool recurse(int idx, int sum, int &target, vector<int> &nums, vector<vector<int>> &dp) {
+        if (idx == nums.size()) {
+            return sum == target;            
+        };
+        if (dp[idx][sum] != -1) {
+            return dp[idx][sum];
+        };
+        if (sum + nums[idx] <= target) {
+            if (recurse(idx + 1, sum + nums[idx], target, nums, dp)) {
+                return dp[idx][sum] = true;
+            };
+        };
+        return dp[idx][sum] = recurse(idx + 1, sum, target, nums, dp);
+    };
+    
+    
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size(), sum = accumulate(begin(nums), end(nums), 0), target = sum / 2;
+        if (sum % 2) {
+            return false;            
+        };
+        vector<vector<int>> dp(n + 1, vector<int>(sum + 1, -1));
+        return recurse(0, 0, target, nums, dp);
+    };
+};

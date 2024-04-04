9dac6f63322efc1b7dfbf78a66addd31b693e04a
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-04 14:01:58 +0530

    Time: 162 ms (87.71%), Space: 86 MB (98.31%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -2,25 +2,21 @@ class Solution {
 public:
     long long maxPoints(vector<vector<int>>& points) {
         int row = points.size(), col = points[0].size();
-        long long curr[col], prev[col], left[col], right[col];     
+        long long prev[col], left[col], right[col];     
         
         for (int j = col - 1; j >= 0; --j) {
             prev[j] = points[row - 1][j];
         }; 
         
-        for (int i = row - 2; i >= 0; --i) {
-            for (int j = col - 1; j >= 0; --j) {
-                curr[j] = prev[j];
-            };  
-            
+        for (int i = row - 2; i >= 0; --i) { 
             left[0] = prev[0];
             for (int j = 1; j < col; ++j) {
-                left[j] = max(curr[j], left[j - 1] - 1);
+                left[j] = max(prev[j], left[j - 1] - 1);
             }; 
             
             right[col - 1] = prev[col - 1];
             for (int j = col - 2; j >= 0; --j) {
-                right[j] = max(curr[j], right[j + 1] - 1);
+                right[j] = max(prev[j], right[j + 1] - 1);
             };  
             
             for (int j = col - 1; j >= 0; --j) {

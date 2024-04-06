5bdb5ce21436e34515fb03430df7f07b33303308
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-06 14:16:49 +0530

    Time: 106 ms (17.90%), Space: 28.6 MB (56.97%) - LeetHub

---
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n = s.size(), aestriks = 0, open = 0, close = 0,  aestriksTaken = 0;
+        // Seee if we're able to satify all closes
+        for (int i = 0; i < n; ++i) {
+            if (locked[i] == '0') {
+                s[i] = '*';    
+                ++aestriks;    
+            } else if (s[i] == ')') {
+                if (open) {
+                    --open;
+                } else if (aestriks) {
+                    --aestriks, ++aestriksTaken;
+                } else {
+                    return false;
+                };
+            } else {
+                ++open;
+            };
+        };
+//         cout << s << " " << open << " " << aestriks << "\n";
+//         if (open == 0 && aestriks % 2 == 0) {
+//             return true;    
+//         };
+        
+        // Mark opens as visited that correspond to a close
+        for (int i = n - 1; i >= 0; --i) {
+            if (s[i] == ')') {
+                s[i] = 'X', ++close;
+            } else if (s[i] == '(') {
+                if (close) {
+                    s[i] = 'X', --close;
+                };
+            };
+        };        
+        // cout << s << " " << open << " " << aestriks << "\n";
+        
+        // Finally see if you can combine opens and astrixes OR astrixes with astrixes 
+        open = 0, aestriks = 0;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '*') {
+                if (aestriksTaken) {
+                    --aestriksTaken;
+                } else if (open) {
+                    --open;
+                } else {
+                    if (++aestriks == 2) {
+                        aestriks = 0;
+                    };
+                };    
+            } else if (s[i] == '(') {
+                ++open;
+            };
+        };        
+        return open == 0 && aestriks == 0;
+    };
+};

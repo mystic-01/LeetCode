55e0f8a1aa53edb110be98a527b64b0a8b74784b
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-08 18:24:58 +0530

    Time: 43 ms (45.76%), Space: 23.6 MB (25.76%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -8,20 +8,16 @@ public:
     };
     
     int maxPoints(vector<vector<int>>& points) {
-        int n = points.size(), maxPoints = n == 1 ? 1 : 2;
-        for (int i = 0; i < n; ++i) {
+        int n = points.size(), maxPoints = 0;
+        for (int i = 0, curr = 0; i < n; ++i, curr = 0) {
             unordered_map<double, int> m;
             for (int j = 0; j < n; ++j) {
                 if (j == i) {
                     continue;
                 };
                 double slope = getSlope(points[j], points[i]);
-                ++m[slope];
+                curr = max(curr, ++m[slope]);    
             };  
-            int curr = 0;
-            for (auto &it : m) {
-                curr = max(curr, it.second);    
-            };
             maxPoints = max(maxPoints, curr + 1);
         };
         return maxPoints;

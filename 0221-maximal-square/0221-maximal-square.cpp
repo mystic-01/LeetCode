dcfa962c14427d28c195a93aa11dfaf68e338c82
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-14 01:25:08 +0530

    Time: 58 ms (78.47%), Space: 22.2 MB (96.37%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int getLargestSqArea(int *nums, int &n) {
-        int i = 0, maxArea = 0;
-        stack<int> st;
-        while (i <= n) {
-            while (!st.empty() && (i == n || nums[i] < nums[st.top()])) {
-                int height = nums[st.top()];
-                st.pop();
-                int left = st.size() ? st.top() : -1;
-                int area = pow(min(i - left - 1, height), 2);
-                maxArea = max(maxArea, area);
-            };  
-            st.push(i++);
-        };
-        return maxArea;
-    };
-    
     int maximalSquare(vector<vector<char>>& matrix) {
-        int row = matrix.size(), col = matrix[0].size(), maxArea = 0, nums[col];
-        memset(nums, 0, sizeof(nums));
-
-        for (int i = 0; i < row; ++i) {
-            for (int j = 0; j < col; ++j) {
-                nums[j] = matrix[i][j] == '0' ? 0 : nums[j] + 1;
-            };        
-            int area = getLargestSqArea(nums, col);
-            maxArea = max(maxArea, area);
+        int row = matrix.size(), col = matrix[0].size(), maxVal = 0, prev[col], dp[col];
+        memset(dp, 0, sizeof(dp));
+        memset(prev, 0, sizeof(prev));
+        for (int j = 0; j < col; ++j) {
+            prev[j] = matrix[0][j] == '0' ? 0 : 1;
+            maxVal = max(maxVal, prev[j]);
         };
-        return maxArea;
+        
+        for (int i = 1; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                if (matrix[i][j] == '0') {
+                    dp[j] = 0;
+                } else {
+                    if (j != 0) {
+                        // cout << prev[j - 1] << " " << prev[j] << " " << dp[j - 1] << "\n";
+                        dp[j] = 1 + min({prev[j - 1], prev[j], dp[j - 1]});
+                    } else {
+                        dp[j] = 1;
+                    };
+                };
+                maxVal = max(maxVal, dp[j]);
+            };        
+            for (int j = 0; j < col; ++j) {
+                prev[j] = dp[j];
+            };
+        };
+        return maxVal * maxVal;
     };
 };

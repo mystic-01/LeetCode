a9f4ebad59dfe77247a3bfb256c7c467fc2354bf
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-14 18:25:55 +0530

    Time: 1827 ms (49.06%), Space: 589.3 MB (13.43%) - LeetHub

--- 3077-maximum-strength-of-k-disjoint-subarrays/3077-maximum-strength-of-k-disjoint-subarrays.cpp
+++ 3077-maximum-strength-of-k-disjoint-subarrays/3077-maximum-strength-of-k-disjoint-subarrays.cpp
@@ -1,39 +1,28 @@
 static const long long oob = -1e18;
+
 class Solution {
 public:
-    long long recurse(int idx, long long k, int isStart, vector<int> &nums, vector<vector<vector<long long>>> &dp) {
-        if (k < 0) {
-            return oob;            
-        };
-        if (idx == nums.size()) {
-            if (k == 0) {
-                return 0;
-            } else {
-                return oob;
-            };
-        };
-
-        if (dp[idx][k][isStart] != oob) {
-            return dp[idx][k][isStart];            
-        };
-
-        int sign = k % 2 ? 1 : -1;
-        long long take = oob, notTake = oob, val = sign * nums[idx] * k;
-        
-        // take = val + recurse(idx + 1, k, 0, nums, dp);
-        // startNew = val + recurse(idx + 1, k - 1, 1, nums, dp);
-        take = val + recurse(idx + 1, k, 0, nums, dp);
-        take = max(take, val + recurse(idx + 1, k - 1, 1, nums, dp));
-        
-        if (isStart) {
-            notTake = recurse(idx + 1, k, 1, nums, dp);
-        };
-        return dp[idx][k][isStart] = max(take, notTake);
-    };
-    
-    long long maximumStrength(vector<int>& nums, int k) {
+    long long maximumStrength(vector<int>& nums, int kMax) {
         int n = nums.size();
-        vector<vector<vector<long long>>> dp(n, vector<vector<long long>>(k + 1, vector<long long>(2, oob)));
-        return recurse(0, k, 1, nums, dp);
+        vector<vector<vector<long long>>> dp(n + 1, vector<vector<long long>>(kMax + 1, vector<long long>(2, oob)));
+        dp[n][0][0] = dp[n][0][1] = 0;
+        
+        for (int idx = n - 1; idx >= 0; --idx) {
+            for (long long k = 0; k <= kMax; ++k) {
+                for (int isStart = 0; isStart <= 1; ++isStart) {
+                    int sign = k % 2 ? 1 : -1;
+                    long long take = oob, notTake = oob, val = sign * nums[idx] * k;
+
+                    take = val + dp[idx + 1][k][0];
+                    take = max(take, val + (k - 1 >= 0 ? dp[idx + 1][k - 1][1] : oob));
+
+                    if (isStart) {
+                        notTake = dp[idx + 1][k][1];
+                    };
+                    dp[idx][k][isStart] = max(take, notTake);
+                };
+            };            
+        };
+        return dp[0][kMax][1];
     };
 };

41bfe5b678179386d157d4a112e87c2921db85bd
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-14 18:29:57 +0530

    Time: 106 ms (83.97%), Space: 34.3 MB (97.67%) - LeetHub

--- 3077-maximum-strength-of-k-disjoint-subarrays/3077-maximum-strength-of-k-disjoint-subarrays.cpp
+++ 3077-maximum-strength-of-k-disjoint-subarrays/3077-maximum-strength-of-k-disjoint-subarrays.cpp
@@ -3,7 +3,13 @@ class Solution {
 public:
     long long maximumStrength(vector<int>& nums, int kMax) {
         int n = nums.size();
-        vector<vector<long long>> dp(kMax + 1, vector<long long>(2, oob)), prev(kMax + 1, vector<long long>(2, oob));
+        long long dp[kMax + 1][2], prev[kMax + 1][2];
+        for (long long k = 0; k <= kMax; ++k) {
+            for (int isStart = 0; isStart <= 1; ++isStart) {
+                prev[k][isStart] = dp[k][isStart] = oob;
+            };
+        };
+        
         prev[0][0] = prev[0][1] = 0;
         
         for (int idx = n - 1; idx >= 0; --idx) {
@@ -20,8 +26,12 @@ public:
                     };
                     dp[k][isStart] = max(take, notTake);
                 };
-            };        
-            prev = dp;
+            };
+            for (long long k = 0; k <= kMax; ++k) {
+                for (int isStart = 0; isStart <= 1; ++isStart) {
+                    prev[k][isStart] = dp[k][isStart];
+                };
+            };
         };
         return prev[kMax][1];
     };

44037cf044e4ab5202a294b161f6e9b425bb3320
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-14 17:59:13 +0530

    Time: 2252 ms (6.85%), Space: 589.6 MB (12.57%) - LeetHub

---
+++ 3077-maximum-strength-of-k-disjoint-subarrays/3077-maximum-strength-of-k-disjoint-subarrays.cpp
@@ -0,0 +1,42 @@
+static const long long oob = -1e18;
+class Solution {
+public:
+    long long recurse(int idx, long long k, int isStart, vector<int> &nums, vector<vector<vector<long long>>> &dp) {
+        if (k < 0) {
+            return oob;            
+        };
+        if (idx == nums.size()) {
+            if (k == 0) {
+                return 0;
+            } else {
+                return oob;
+            };
+        };
+
+        if (dp[idx][k][isStart] != oob) {
+            return dp[idx][k][isStart];            
+        };
+
+        int sign = k % 2 ? 1 : -1;
+        long long take = oob, notTake = oob, startNew = oob, val = sign * nums[idx] * k;
+        
+        // take = val + recurse(idx + 1, k, 0, nums, dp);
+        // startNew = val + recurse(idx + 1, k - 1, 1, nums, dp);
+        take = val + recurse(idx + 1, k, 0, nums, dp);
+        take = max(take, val + recurse(idx + 1, k - 1, 1, nums, dp));
+        
+        if (isStart) {
+            notTake = recurse(idx + 1, k, 1, nums, dp);
+        } else {
+            notTake = recurse(idx + 1, k - 1, 1, nums, dp);            
+        };
+        
+        return dp[idx][k][isStart] = max(max(take, notTake), startNew);
+    };
+    
+    long long maximumStrength(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<vector<vector<long long>>> dp(n, vector<vector<long long>>(k + 1, vector<long long>(2, oob)));
+        return recurse(0, k, 1, nums, dp);
+    };
+};

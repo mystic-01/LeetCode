8c133c814d2ba4e17fbfa088a66c82bf1160138c
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-14 09:56:42 +0530

    Time: 3185 ms (14.29%), Space: 448.4 MB (28.57%) - LeetHub

--- 3117-minimum-sum-of-values-by-dividing-array/3117-minimum-sum-of-values-by-dividing-array.cpp
+++ 3117-minimum-sum-of-values-by-dividing-array/3117-minimum-sum-of-values-by-dividing-array.cpp
@@ -1,30 +1,39 @@
+static const int oob = 1e9 + 1;
+
 class Solution {
 public:
-vector<vector<unordered_map<int,int>>> dp;
-int helper(vector<int>& nums, vector<int>& andValues, int cur, int andCur, int And){
-    if(cur>=nums.size()){
-        if(andCur>=andValues.size()) return 0;
-        else return 1e8;
-    }
-    else if(andCur>=andValues.size()) return 1e8;
+    int getAns(int i, int k, int mIdx, vector<int> &nums, vector<int> &andValues, int &n, int &m, vector<vector<unordered_map<int, int>>> &dp) {
+        if (i == n) {
+            if (mIdx == m) return 0;
+            else return oob;
+        };
+        if (mIdx >= m) {
+            return oob;
+        };
+        if (dp[i][mIdx].find(k + 1) != dp[i][mIdx].end()) {
+            return dp[i][mIdx][k + 1];
+        };
+
+        int take = oob;
+        int startNew = oob;
+        int andVal = k == -1 ? nums[i] : (k & nums[i]);
+        if (n - i - 1 >= m - mIdx - 1) {
+            take = getAns(i + 1, andVal, mIdx, nums, andValues, n, m, dp); 
+        };
+        if (andVal == andValues[mIdx]) {
+            startNew = nums[i] + getAns(i + 1, -1, mIdx + 1, nums, andValues, n, m, dp);
+        };
+
+        return dp[i][mIdx][k + 1] = min(take, startNew);
+    };
     
-    if(dp[cur][andCur].count(And)) return dp[cur][andCur][And];
-    
-    int inclu=1e8;
-    if((And&nums[cur])==andValues[andCur]){
-        inclu = nums[cur] + helper(nums, andValues, cur+1, andCur+1, ((1<<20)-1));
-    }
-    int exclu = helper(nums, andValues, cur+1, andCur, (And&nums[cur]));
-    
-    return dp[cur][andCur][And]=min(inclu, exclu);
-}
-int minimumValueSum(vector<int>& nums, vector<int>& andValues) {
-    int n=nums.size();
-    dp.resize(n+2, vector<unordered_map<int,int>> (11));
-    
-    int ans = helper(nums, andValues, 0, 0, ((1<<19)-1));
-    
-    if(ans==1e8) return -1;
-    return ans;
-}
+    int minimumValueSum(vector<int>& nums, vector<int>& andValues) {
+        int n = nums.size(), m = andValues.size();
+        vector<vector<unordered_map<int, int>>> dp(n, vector<unordered_map<int, int>>(m + 1));
+        int ans = getAns(0, -1, 0, nums, andValues, n, m, dp);           
+        return ans >= oob ? -1 : ans;
+    };
 };
+
+
+

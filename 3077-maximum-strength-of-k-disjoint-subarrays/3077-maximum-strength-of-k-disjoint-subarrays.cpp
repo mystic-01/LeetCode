2407d6b4bd1e859008cd0df362302e42e4a6a268
mystic-01 <80163619+mystic-01@users.noreply.github.com>
2024-04-14 18:27:37 +0530

    Time: 355 ms (68.90%), Space: 38 MB (94.84%) - LeetHub

--- 3077-maximum-strength-of-k-disjoint-subarrays/3077-maximum-strength-of-k-disjoint-subarrays.cpp
+++ 3077-maximum-strength-of-k-disjoint-subarrays/3077-maximum-strength-of-k-disjoint-subarrays.cpp
@@ -1,11 +1,10 @@
 static const long long oob = -1e18;
-
 class Solution {
 public:
     long long maximumStrength(vector<int>& nums, int kMax) {
         int n = nums.size();
-        vector<vector<vector<long long>>> dp(n + 1, vector<vector<long long>>(kMax + 1, vector<long long>(2, oob)));
-        dp[n][0][0] = dp[n][0][1] = 0;
+        vector<vector<long long>> dp(kMax + 1, vector<long long>(2, oob)), prev(kMax + 1, vector<long long>(2, oob));
+        prev[0][0] = prev[0][1] = 0;
         
         for (int idx = n - 1; idx >= 0; --idx) {
             for (long long k = 0; k <= kMax; ++k) {
@@ -13,16 +12,17 @@ public:
                     int sign = k % 2 ? 1 : -1;
                     long long take = oob, notTake = oob, val = sign * nums[idx] * k;
 
-                    take = val + dp[idx + 1][k][0];
-                    take = max(take, val + (k - 1 >= 0 ? dp[idx + 1][k - 1][1] : oob));
+                    take = val + prev[k][0];
+                    take = max(take, val + (k - 1 >= 0 ? prev[k - 1][1] : oob));
 
                     if (isStart) {
-                        notTake = dp[idx + 1][k][1];
+                        notTake = prev[k][1];
                     };
-                    dp[idx][k][isStart] = max(take, notTake);
+                    dp[k][isStart] = max(take, notTake);
                 };
-            };            
+            };        
+            prev = dp;
         };
-        return dp[0][kMax][1];
+        return prev[kMax][1];
     };
 };
